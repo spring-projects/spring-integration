44c7a44380691a82250163c6ab2c3703bb7aaf7a
Mark Fisher <markfisher@vmware.com>
2007-11-21 19:31:04 +0000

    Defined base MessageHandler interface (INT-5).

---
+++ spring-eai-core/src/main/java/org/springframework/integration/handler/MessageHandler.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.handler;
+
+import org.springframework.integration.message.Message;
+
+/**
+ * Generic message handler interface. Typical implementations will translate
+ * between the generic Messages of the integration framework and the domain
+ * objects that are passed-to and returned-from business components.
+ * 
+ * @author Mark Fisher
+ */
+public interface MessageHandler {
+
+	Message handle(Message message);
+
+}
