c7c8564295fd1c573982fb0f8bb6ea933c96534c
Mark Fisher <markfisher@vmware.com>
2008-02-25 22:51:35 +0000

    Classpath is now dynamically generated from all JARs in the "lib" and "dist" directories (INT-79).

--- build-spring-integration/resources/samples/cafeDemo.sh
+++ build-spring-integration/resources/samples/cafeDemo.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-java -cp ../dist/spring-integration-core-1.0.0.m1.jar:\
-../dist/spring-integration-samples-1.0.0.m1.jar:\
-../lib/aopalliance-1.0.jar:\
-../lib/commons-logging-1.1.jar:\
-../lib/spring-aop-2.5.0.jar:\
-../lib/spring-beans-2.5.0.jar:\
-../lib/spring-context-2.5.0.jar:\
-../lib/spring-context-support-2.5.0.jar:\
-../lib/spring-core-2.5.0.jar\
- org.springframework.integration.samples.cafe.CafeDemo $*
+JARS=`find ../dist ../lib -iname *.jar`
+CP=`echo $JARS | sed 's/ /:/g'`
+
+java -cp $CP org.springframework.integration.samples.cafe.CafeDemo $*
