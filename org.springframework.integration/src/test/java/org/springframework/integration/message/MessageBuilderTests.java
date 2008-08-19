42508cc231d55d8835d98036722e88d516e1f103
Mark Fisher <markfisher@vmware.com>
2008-08-19 21:50:32 +0000

    Added some tests for MessageBuilder.

--- org.springframework.integration/src/test/java/org/springframework/integration/message/MessageBuilderTests.java
+++ org.springframework.integration/src/test/java/org/springframework/integration/message/MessageBuilderTests.java
@@ -63,6 +63,44 @@ public class MessageBuilderTests {
 	}
 
 	@Test
+	public void copyHeadersIfAbsent() {
+		Message<String> message1 = MessageBuilder.fromPayload("test1")
+				.setHeader("foo", "bar").build();
+		Message<String> message2 = MessageBuilder.fromPayload("test2")
+				.setHeader("foo", 123)
+				.copyHeadersIfAbsent(message1.getHeaders())
+				.build();
+		assertEquals("test2", message2.getPayload());
+		assertEquals(123, message2.getHeaders().get("foo"));
+	}
+
+	@Test
+	public void createFromMessage() {
+		Message<String> message1 = MessageBuilder.fromPayload("test")
+				.setHeader("foo", "bar").build();
+		Message<String> message2 = MessageBuilder.fromMessage(message1).build();
+		assertEquals("test", message2.getPayload());
+		assertEquals("bar", message2.getHeaders().get("foo"));
+	}
+
+	@Test
+	public void testPriority() {
+		Message<Integer> importantMessage = MessageBuilder.fromPayload(1)
+			.setPriority(MessagePriority.HIGHEST).build();
+		assertEquals(MessagePriority.HIGHEST, importantMessage.getHeaders().getPriority());
+	}
+
+	@Test
+	public void testNonDestructiveSet() {
+		Message<Integer> message1 = MessageBuilder.fromPayload(1)
+			.setPriority(MessagePriority.HIGHEST).build();
+		Message<Integer> message2 = MessageBuilder.fromMessage(message1)
+			.setHeaderIfAbsent(MessageHeaders.PRIORITY, MessagePriority.LOW)
+			.build();
+		assertEquals(MessagePriority.HIGHEST, message2.getHeaders().getPriority());
+	}
+
+	@Test
 	public void testExpirationDate() {
 		Date past = new Date(System.currentTimeMillis() - (60 * 1000));
 		Message<Integer> expiredMessage = MessageBuilder.fromPayload(1)
