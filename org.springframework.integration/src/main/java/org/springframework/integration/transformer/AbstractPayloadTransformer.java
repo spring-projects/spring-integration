860709910d4dcd486555bd8dd98823a33f27a9e1
Mark Fisher <markfisher@vmware.com>
2008-09-03 04:44:56 +0000

    Added AbstractPayloadTransformer and AbstractHeaderTransformer base classes.

---
+++ org.springframework.integration/src/main/java/org/springframework/integration/transformer/AbstractPayloadTransformer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2002-2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.transformer;
+
+import org.springframework.integration.message.Message;
+import org.springframework.integration.message.MessageBuilder;
+import org.springframework.integration.message.MessagingException;
+
+/**
+ * A base class for {@link Transformer} implementations that modify
+ * the payload of a {@link Message}.
+ * 
+ * @author Mark Fisher
+ */
+public abstract class AbstractPayloadTransformer<T, U> implements Transformer {
+
+	public final Message<?> transform(Message<?> message) {
+		try {
+			@SuppressWarnings("unchecked")
+	        U result = this.transformPayload((T) message.getPayload());
+	        return MessageBuilder.fromPayload(result).copyHeaders(message.getHeaders()).build();
+        } catch (Exception e) {
+        	throw new MessagingException(message, "failed to transform message payload", e);
+        }
+	}
+
+	protected abstract U transformPayload(T payload) throws Exception;
+
+}
