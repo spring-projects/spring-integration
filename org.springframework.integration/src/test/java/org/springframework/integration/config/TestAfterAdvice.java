fc6dc7488abf7c25f106461e4334f2b41ad90158
Mark Fisher <markfisher@vmware.com>
2008-10-09 19:12:23 +0000

    Added tests for advice chain with @Poller on a channel-polling consumer (@ServiceActivator).

---
+++ org.springframework.integration/src/test/java/org/springframework/integration/config/TestAfterAdvice.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2002-2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.config;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.CountDownLatch;
+
+import org.springframework.aop.AfterReturningAdvice;
+
+/**
+ * @author Mark Fisher
+ */
+public class TestAfterAdvice implements AfterReturningAdvice {
+
+	private final CountDownLatch latch;
+
+	private volatile long latchCount;
+
+
+	public TestAfterAdvice(CountDownLatch latch) {
+		this.latch = latch;
+	}
+
+
+	public long getLatchCount() {
+		return this.latchCount;
+	}
+
+	public void afterReturning(Object returnValue, Method method,
+			Object[] args, Object target) throws Throwable {
+		this.latchCount = this.latch.getCount();
+		this.latch.countDown();
+	}
+
+}
