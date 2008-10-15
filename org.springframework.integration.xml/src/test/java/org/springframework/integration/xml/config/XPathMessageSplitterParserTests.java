3b7ff7c1887c1e7b98d83681173cebba1b7b7905
Mark Fisher <markfisher@vmware.com>
2008-10-15 03:47:34 +0000

    Removed unnecessary sleep (inputChannel is a DirectChannel).

--- org.springframework.integration.xml/src/test/java/org/springframework/integration/xml/config/XPathMessageSplitterParserTests.java
+++ org.springframework.integration.xml/src/test/java/org/springframework/integration/xml/config/XPathMessageSplitterParserTests.java
@@ -19,6 +19,8 @@ package org.springframework.integration.xml.config;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.w3c.dom.Document;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
@@ -28,38 +30,32 @@ import org.springframework.integration.endpoint.SubscribingConsumerEndpoint;
 import org.springframework.integration.message.GenericMessage;
 import org.springframework.integration.xml.util.XmlTestUtil;
 import org.springframework.test.context.ContextConfiguration;
-import org.w3c.dom.Document;
 
 /**
  * @author Jonas Partner
  */
 @ContextConfiguration
 public class XPathMessageSplitterParserTests {
-	
+
 	String channelDefinitions = "<si:channel id='test-input' /><si:channel id='test-output'><si:queue capacity='10'/></si:channel>";
 
 	@Autowired @Qualifier("test-input")
 	MessageChannel inputChannel;
-	
+
 	@Autowired @Qualifier("test-output")
 	QueueChannel outputChannel;
-	
+
 	@Test
 	public void testSimpleStringExpression() throws Exception {
 		Document doc = XmlTestUtil.getDocumentForString("<names><name>Bob</name><name>John</name></names>");
 		GenericMessage<Document> docMessage = new GenericMessage<Document>(doc);
-
-		TestXmlApplicationContext ctx = TestXmlApplicationContextHelper.getTestAppContext(  channelDefinitions + "<si-xml:xpath-splitter id='splitter' input-channel='test-input' output-channel='test-output'><si-xml:xpath-expression expression='//name'/></si-xml:xpath-splitter>");
-		
+		TestXmlApplicationContext ctx = TestXmlApplicationContextHelper.getTestAppContext(
+				channelDefinitions + "<si-xml:xpath-splitter id='splitter' input-channel='test-input' output-channel='test-output'><si-xml:xpath-expression expression='//name'/></si-xml:xpath-splitter>");
 		SubscribingConsumerEndpoint sce = (SubscribingConsumerEndpoint)ctx.getBean("splitter");
 		sce.start();
-		
 		ctx.getAutowireCapableBeanFactory().autowireBeanProperties(this, AutowireCapableBeanFactory.AUTOWIRE_BY_TYPE, false);
-		
 		inputChannel.send(docMessage);
-		Thread.sleep(5000);
 		assertEquals("Wrong number of split messages ", 2, outputChannel.getMesssageCount());
-	
 	}
 
 }
