4735c700b1bfe7ab81102a9f89109f333f5a8f34
Jonas Partner <jonas.partner@gmail.com>
2008-11-22 21:49:24 +0000

    sample for INT-432  documentation to follow

---
+++ org.springframework.integration.samples/src/main/java/org/springframework/integration/samples/xml/BookOrderProcessingSample.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2002-2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.samples.xml;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.integration.core.MessageChannel;
+import org.springframework.integration.message.GenericMessage;
+import org.w3c.dom.Document;
+
+/**
+ * 
+ * @author Jonas Partner
+ * 
+ */
+public class BookOrderProcessingSample {
+
+	public static void main(String[] args) throws Exception {
+		try {
+			AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+					"orderProcessingSample.xml", BookOrderProcessingSample.class);
+			MessageChannel messageChannel = (MessageChannel) applicationContext.getBean("ordersChannel");
+			GenericMessage<Document> orderMessage = createXmlMessageFromResource("org/springframework/integration/samples/xml/order.xml");
+			messageChannel.send(orderMessage);
+			applicationContext.close();
+		}
+		catch (Throwable t){
+			t.printStackTrace();
+		}
+		finally {
+			System.exit(0);
+		}
+	}
+
+	private static GenericMessage<Document> createXmlMessageFromResource(String path) throws Exception {
+		Resource orderRes = new ClassPathResource(path);
+
+		DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+		builderFactory.setNamespaceAware(true);
+		DocumentBuilder builder = builderFactory.newDocumentBuilder();
+
+		Document orderDoc = builder.parse(orderRes.getInputStream());
+		return new GenericMessage<Document>(orderDoc);
+	}
+
+}
