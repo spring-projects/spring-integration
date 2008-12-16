1ddc8f6c23cfc6d00d090eb1f7c9ed4ddb4528de
Mark Fisher <markfisher@vmware.com>
2008-12-16 19:36:04 +0000

    Added LoggingChannelAdapterParser (INT-524).

---
+++ org.springframework.integration/src/main/java/org/springframework/integration/config/xml/LoggingChannelAdapterParser.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2002-2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.config.xml;
+
+import org.w3c.dom.Element;
+
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.ParserContext;
+
+/**
+ * Parser for the 'logging-channel-adapter' element.
+ * 
+ * @author Mark Fisher
+ * @since 1.0.1
+ */
+public class LoggingChannelAdapterParser extends AbstractOutboundChannelAdapterParser {
+
+	@Override
+	protected AbstractBeanDefinition parseConsumer(Element element, ParserContext parserContext) {
+		BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(
+				IntegrationNamespaceUtils.BASE_PACKAGE + ".handler.LoggingHandler");
+		builder.addConstructorArgValue(element.getAttribute("level"));
+		return builder.getBeanDefinition();
+	}
+
+}
