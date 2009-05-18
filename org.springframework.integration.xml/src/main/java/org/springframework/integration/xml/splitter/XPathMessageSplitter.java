3a2988e913ffc82d854e85afbc783b7f566f7faf
Mark Fisher <markfisher@vmware.com>
2009-05-18 22:01:27 +0000

    INT-652

--- org.springframework.integration.xml/src/main/java/org/springframework/integration/xml/splitter/XPathMessageSplitter.java
+++ org.springframework.integration.xml/src/main/java/org/springframework/integration/xml/splitter/XPathMessageSplitter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -104,6 +104,12 @@ public class XPathMessageSplitter extends AbstractMessageSplitter {
 			else if (payload instanceof String) {
 				payload = xmlPayloadConverter.convertToDocument(payload);
 				result = splitStringPayload(message);
+			}
+			else {
+				throw new IllegalArgumentException(
+						"Unsupported payload type [" + payload.getClass().getName()
+						+ "]. The XPathMessageSplitter only accepts [" + Node.class.getName()
+						+ "] or [java.lang.String] typed payloads.");
 			}
 			return result;
 		}
--- org.springframework.integration.xml/src/test/java/org/springframework/integration/xml/splitter/XPathMessageSplitterTests.java
+++ org.springframework.integration.xml/src/test/java/org/springframework/integration/xml/splitter/XPathMessageSplitterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -93,6 +93,11 @@ public class XPathMessageSplitterTests {
 		for (Message<?> message : docMessages) {
 			assertTrue("unexpected payload type " + message.getPayload().getClass().getName(), message.getPayload() instanceof String);
 		}
+	}
+
+	@Test(expected = MessagingException.class)
+	public void invalidPayloadType() {
+		splitter.handleMessage(new GenericMessage<Integer>(123));
 	}
 
 }
