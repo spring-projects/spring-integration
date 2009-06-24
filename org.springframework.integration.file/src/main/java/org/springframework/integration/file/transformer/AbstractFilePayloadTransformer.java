cb7fd4e9b5696d277bdb1cd9e1ee0ff48090ad0c
Mark Fisher <markfisher@vmware.com>
2009-06-24 23:52:28 +0000

    INT-686

--- org.springframework.integration.file/src/main/java/org/springframework/integration/file/transformer/AbstractFilePayloadTransformer.java
+++ org.springframework.integration.file/src/main/java/org/springframework/integration/file/transformer/AbstractFilePayloadTransformer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,6 +58,7 @@ public abstract class AbstractFilePayloadTransformer<T> implements Transformer {
 	        T result = this.transformFile(file);
 	        Message<?> transformedMessage = MessageBuilder.withPayload(result)
 	        		.copyHeaders(message.getHeaders())
+	        		.setHeaderIfAbsent(FileHeaders.ORIGINAL_FILE, file)
 	        		.setHeaderIfAbsent(FileHeaders.FILENAME, file.getName())
 	        		.build();
 			if (this.deleteFiles) {
