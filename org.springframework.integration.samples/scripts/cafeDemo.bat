530aab15926827e731aa48d7579be15b0aed3460
Mark Fisher <markfisher@vmware.com>
2009-07-02 02:40:43 +0000

    removed scripts now that the samples are easy to build and run

--- org.springframework.integration.samples/scripts/cafeDemo.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-setlocal ENABLEDELAYEDEXPANSION
-
-for /r ..\dist %%j IN (*.jar) do set CP=!CP!;%%j
-for /r ..\lib %%j IN (*.jar) do set CP=!CP!;%%j
-
-@java -cp %CP% org.springframework.integration.samples.cafe.xml.CafeDemo %1
--- org.springframework.integration.samples/scripts/cafeDemo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-JARS=`find ../dist ../lib -iname *.jar`
-CP=`echo $JARS | sed 's/ /:/g'`
-
-java -cp $CP org.springframework.integration.samples.cafe.xml.CafeDemo $*
