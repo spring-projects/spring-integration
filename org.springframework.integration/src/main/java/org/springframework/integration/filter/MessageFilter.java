3cffe9e7d7adcfc2dea80449a420f4a4b04717b9
Mark Fisher <markfisher@vmware.com>
2009-07-04 18:57:02 +0000

    added javadoc comment about the discard channel

--- org.springframework.integration/src/main/java/org/springframework/integration/filter/MessageFilter.java
+++ org.springframework.integration/src/main/java/org/springframework/integration/filter/MessageFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,7 +31,8 @@ import org.springframework.util.Assert;
  * will be passed to this filter's output channel. Otherwise the message will
  * either be silently dropped (the default) or will trigger the throwing of a
  * {@link MessageRejectedException} depending on the value of its
- * {@link #throwExceptionOnRejection} property.
+ * {@link #throwExceptionOnRejection} property. If a discard channel is
+ * provided, the rejected Messages will be sent to that channel.
  * 
  * @author Mark Fisher
  */
