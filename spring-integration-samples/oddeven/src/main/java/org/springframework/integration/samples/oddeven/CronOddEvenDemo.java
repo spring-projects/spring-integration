bf4aa1f180b89e45d41e5f5a0809808289c05c66
Mark Fisher <markfisher@vmware.com>
2009-07-09 00:25:36 +0000

    INT-706 moved config files to the same directory as the corresponding demo classes and added descriptions

--- spring-integration-samples/oddeven/src/main/java/org/springframework/integration/samples/oddeven/CronOddEvenDemo.java
+++ spring-integration-samples/oddeven/src/main/java/org/springframework/integration/samples/oddeven/CronOddEvenDemo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,12 @@ package org.springframework.integration.samples.oddeven;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
+ * Demonstrates a method-invoking inbound Channel Adapter with a
+ * Cron-based polling trigger followed by a simple method-invoking
+ * router.
+ * <p>
+ * See the 'cronOddEvenDemo.xml' configuration file for more detail.
+ * 
  * @author Mark Fisher
  */
 public class CronOddEvenDemo {
