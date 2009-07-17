f4cb1b54646aa43a8f50cbd9075b64eaa850b84b
Mark Fisher <markfisher@vmware.com>
2009-07-17 02:24:05 +0000

    reorganized osgi samples

--- spring-integration-samples/osgi-inbound/src/org/springframework/integration/samples/osgi/inbound/InboundGateway.java
+++ spring-integration-samples/osgi-inbound/src/org/springframework/integration/samples/osgi/inbound/InboundGateway.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.integration.samples.inbound;
+package org.springframework.integration.samples.osgi.inbound;
 
 import java.io.File;
 
