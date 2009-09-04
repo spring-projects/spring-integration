5876e99475f4f37f147d5491a422107e60da3648
Mark Fisher <markfisher@vmware.com>
2009-09-04 16:13:53 +0000

    INT-645

---
+++ org.springframework.integration/src/test/java/org/springframework/integration/handler/MockHandlerTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2002-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.handler;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.core.Message;
+import org.springframework.integration.core.MessageChannel;
+import org.springframework.integration.message.MessageBuilder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Iwein Fuld
+ * @author Mark Fisher
+ */
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class MockHandlerTest {
+
+	@Autowired
+	@Qualifier("input")
+	private MessageChannel input;
+
+	@Autowired
+	private TestInterface mock;
+
+
+	@Test
+	public void configOk() throws Exception {
+		QueueChannel output = new QueueChannel();
+		Mockito.when(mock.test("foo")).thenReturn("bar");
+		input.send(MessageBuilder.withPayload("foo").setReplyChannel(output).build());
+		Message<?> result = output.receive(0);
+		assertEquals("bar", result.getPayload());
+	}
+
+
+	public static interface TestInterface {
+		String test(String input);
+	}
+
+}
