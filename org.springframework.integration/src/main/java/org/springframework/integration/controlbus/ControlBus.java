0f5b6dfd20a9dff273c196c03ec8773319c3e137
Oleg Zhurakousky <ozhurakousky@vmware.com>
2009-11-17 13:58:06 +0000

    INT-881, Decoupled SI Services configuration from service interaction with the Control Bus (forgot to commit the dependent change, fixing broken build)

--- org.springframework.integration/src/main/java/org/springframework/integration/controlbus/ControlBus.java
+++ org.springframework.integration/src/main/java/org/springframework/integration/controlbus/ControlBus.java
@@ -26,5 +26,5 @@ import org.springframework.integration.channel.SubscribableChannel;
  * @since 2.0
  */
 public interface ControlBus extends SubscribableChannel {
-	public String getBusName();
+	public boolean isBusAvailable();
 }
