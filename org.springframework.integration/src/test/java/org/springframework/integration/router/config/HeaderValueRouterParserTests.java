36f6ec07d6da7ba83cd568024f83af88e22171d5
Mark Fisher <markfisher@vmware.com>
2009-12-06 23:58:07 +0000

    formatting

--- org.springframework.integration/src/test/java/org/springframework/integration/router/config/HeaderValueRouterParserTests.java
+++ org.springframework.integration/src/test/java/org/springframework/integration/router/config/HeaderValueRouterParserTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.integration.channel.PollableChannel;
@@ -34,58 +35,61 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 public class HeaderValueRouterParserTests {
+
 	@Autowired
 	private ConfigurableApplicationContext context;
+
 	@Autowired
 	private TestServiceA testServiceA;
+
 	@Autowired
 	private TestServiceB testServiceB;
+
 
 	@Test
 	public void testHeaderValuesAsChannels() {
 		context.start();
-		MessageBuilder channel1MessageBuilder = MessageBuilder.withPayload("");
+		MessageBuilder<?> channel1MessageBuilder = MessageBuilder.withPayload("");
 		channel1MessageBuilder.setHeader("testHeader", "channel1");
-		Message message1 = channel1MessageBuilder.build();
-		MessageBuilder channel2MessageBuilder = MessageBuilder.withPayload("");
+		Message<?> message1 = channel1MessageBuilder.build();
+		MessageBuilder<?> channel2MessageBuilder = MessageBuilder.withPayload("");
 		channel2MessageBuilder.setHeader("testHeader", "channel2");
-		Message message2 = channel2MessageBuilder.build();
+		Message<?> message2 = channel2MessageBuilder.build();
 		testServiceA.foo(message1);
 		testServiceA.foo(message2);
-		PollableChannel chanel1 = (PollableChannel) context.getBean("channel1");
-		PollableChannel chanel2 = (PollableChannel) context.getBean("channel2");
-		message1 = chanel1.receive();
+		PollableChannel channel1 = (PollableChannel) context.getBean("channel1");
+		PollableChannel channel2 = (PollableChannel) context.getBean("channel2");
+		message1 = channel1.receive();
 		assertTrue(message1.getHeaders().get("testHeader").equals("channel1"));
-		message2 = chanel2.receive();
+		message2 = channel2.receive();
 		assertTrue(message2.getHeaders().get("testHeader").equals("channel2"));
 	}
 	@Test
 	public void testHeaderValuesWithMapResolver() {
 		context.start();
-		MessageBuilder channel1MessageBuilder = MessageBuilder.withPayload("");
+		MessageBuilder<?> channel1MessageBuilder = MessageBuilder.withPayload("");
 		channel1MessageBuilder.setHeader("testHeader", "1");
-		Message message1 = channel1MessageBuilder.build();
-		MessageBuilder channel2MessageBuilder = MessageBuilder.withPayload("");
+		Message<?> message1 = channel1MessageBuilder.build();
+		MessageBuilder<?> channel2MessageBuilder = MessageBuilder.withPayload("");
 		channel2MessageBuilder.setHeader("testHeader", "2");
-		Message message2 = channel2MessageBuilder.build();
+		Message<?> message2 = channel2MessageBuilder.build();
 		testServiceB.foo(message1);
 		testServiceB.foo(message2);
-		PollableChannel chanel1 = (PollableChannel) context.getBean("channel1");
-		PollableChannel chanel2 = (PollableChannel) context.getBean("channel2");
-		message1 = chanel1.receive();
+		PollableChannel channel1 = (PollableChannel) context.getBean("channel1");
+		PollableChannel channel2 = (PollableChannel) context.getBean("channel2");
+		message1 = channel1.receive();
 		assertTrue(message1.getHeaders().get("testHeader").equals("1"));
-		message2 = chanel2.receive();
+		message2 = channel2.receive();
 		assertTrue(message2.getHeaders().get("testHeader").equals("2"));
 	}
-	
-	
-	
-	public static interface TestServiceA{
-		public void foo(Message message);
+
+
+	public static interface TestServiceA {
+		public void foo(Message<?> message);
 	}
-	public static interface TestServiceB{
-		public void foo(Message message);
+
+	public static interface TestServiceB {
+		public void foo(Message<?> message);
 	}
-	
 
 }
