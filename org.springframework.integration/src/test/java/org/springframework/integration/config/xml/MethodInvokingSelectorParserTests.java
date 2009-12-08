5961c8f3a821f368f7a281d401ee64e61d29b2d8
Mark Fisher <markfisher@vmware.com>
2009-12-08 15:03:40 +0000

    removed unintended com.sun import

--- org.springframework.integration/src/test/java/org/springframework/integration/config/xml/MethodInvokingSelectorParserTests.java
+++ org.springframework.integration/src/test/java/org/springframework/integration/config/xml/MethodInvokingSelectorParserTests.java
@@ -1,27 +1,48 @@
+/*
+ * Copyright 2002-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.integration.config.xml;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.DirectFieldAccessor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.core.Message;
 import org.springframework.integration.filter.MethodInvokingSelector;
 import org.springframework.integration.selector.MessageSelector;
 import org.springframework.integration.selector.MessageSelectorChain;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
-
+/**
+ * @author Iwein Fuld
+ */
 @ContextConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MethodInvokingSelectorParserTests {
+
 	@Autowired
-	MessageSelectorChain chain;
+	private MessageSelectorChain chain;
+
 
 	@SuppressWarnings("unchecked")
 	@Test
@@ -31,9 +52,11 @@ public class MethodInvokingSelectorParserTests {
 		assertThat(selectors.get(0), is(MethodInvokingSelector.class));
 	}
 
-	public static class SomeClass {
-		public boolean accept(Message m) {
+
+	public static class TestFilter {
+		public boolean accept(Message<?> m) {
 			return true;
 		}
 	}
+
 }
