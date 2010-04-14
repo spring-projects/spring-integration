5ec9820ded4e5cda979f8c1d8799ffdb131b9681
Mark Fisher <markfisher@vmware.com>
2010-04-14 00:41:00 +0000

    removed unintentional sun.tools import

--- org.springframework.integration.xml/src/main/java/org/springframework/integration/xml/transformer/DefaultTransformerConfigurer.java
+++ org.springframework.integration.xml/src/main/java/org/springframework/integration/xml/transformer/DefaultTransformerConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,8 +22,6 @@ import javax.xml.transform.Transformer;
 
 import org.springframework.integration.core.Message;
 import org.springframework.integration.core.MessageHeaders;
-
-import sun.tools.tree.OrExpression;
 
 /**
  * {@link TransformerConfigurer} instance which looks for headers and uses them
