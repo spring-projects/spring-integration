dee7f85e4a6d166e342dd77f2be0a3ad840d60b1
Mark Fisher <markfisher@vmware.com>
2010-04-14 01:00:13 +0000

    using File.separator in test instead of hardcoded slash

--- org.springframework.integration.file/src/test/java/org/springframework/integration/file/FileOutboundGatewayIntegrationTests.java
+++ org.springframework.integration.file/src/test/java/org/springframework/integration/file/FileOutboundGatewayIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -81,7 +81,7 @@ public class FileOutboundGatewayIntegrationTests {
 
 	@BeforeClass
 	public static void setupClass() {
-		workDir = new File(System.getProperty("java.io.tmpdir") + "/anyDir");
+		workDir = new File(System.getProperty("java.io.tmpdir") + File.separator + "anyDir");
 		workDir.mkdir();
 		workDir.deleteOnExit();
 	}
