c9c3da0fe7d193712a80daa5eac6973cc7a6f95a
Mark Fisher <markfisher@vmware.com>
2010-04-26 20:20:04 +0000

    formatting

--- org.springframework.integration/src/main/java/org/springframework/integration/channel/AbstractPollableChannel.java
+++ org.springframework.integration/src/main/java/org/springframework/integration/channel/AbstractPollableChannel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -66,6 +66,5 @@ public abstract class AbstractPollableChannel extends AbstractMessageChannel imp
 	 * available or the blocking thread is interrupted.
 	 */
 	protected abstract Message<?> doReceive(long timeout);
-
 
 }
