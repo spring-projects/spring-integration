57eddef9d9500d43d0371f009a1298da0095b6fc
Mark Fisher <markfisher@vmware.com>
2010-05-24 14:50:39 +0000

    renamed MultiMethodGatewayConfig to MultiMethodGatewayConfigTests

--- org.springframework.integration/src/test/java/org/springframework/integration/gateway/MultiMethodGatewayConfigTests.java
+++ org.springframework.integration/src/test/java/org/springframework/integration/gateway/MultiMethodGatewayConfigTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,12 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.integration.gateway;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -30,40 +32,47 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @ContextConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
-public class MultiMethodGatewayConfig {
-	
+public class MultiMethodGatewayConfigTests {
+
 	@Autowired
 	private ApplicationContext applicationContext;
+
+
 	@Test
-	public void validateGatewayMethods(){
+	public void validateGatewayMethods() {
 		TestGateway gateway = (TestGateway) applicationContext.getBean("myGateway");
-		Assert.assertEquals(gateway.echo("oleg"), 
-						"org.springframework.integration.gateway.MultiMethodGatewayConfig$TestBeanA:oleg");
+		String parentClassName = "org.springframework.integration.gateway.MultiMethodGatewayConfigTests"; 
+		Assert.assertEquals(gateway.echo("oleg"),
+				parentClassName + "$TestBeanA:oleg");
 		Assert.assertEquals(gateway.echoUpperCase("oleg"), 
-						"org.springframework.integration.gateway.MultiMethodGatewayConfig$TestBeanB:oleg");
+				parentClassName + "$TestBeanB:oleg");
 		Assert.assertEquals(gateway.echoViaDefault("oleg"), 
-						"org.springframework.integration.gateway.MultiMethodGatewayConfig$TestBeanC:oleg");
+				parentClassName + "$TestBeanC:oleg");
 	}
-	
-	public static class TestBeanA{
-		public String echo(String str){
+
+
+	public static class TestBeanA {
+		public String echo(String str) {
 			return this.getClass().getName() + ":" + str;
 		}
 	}
-	public static class TestBeanB{
-		public String echo(String str){
+
+	public static class TestBeanB {
+		public String echo(String str) {
 			return this.getClass().getName() + ":" + str;
 		}
 	}
-	public static class TestBeanC{
-		public String echo(String str){
+
+	public static class TestBeanC {
+		public String echo(String str) {
 			return this.getClass().getName() + ":" + str;
 		}
 	}
-	
-	public static interface TestGateway{
+
+	public static interface TestGateway {
 		public String echo(String str);
 		public String echoViaDefault(String str);
 		public String echoUpperCase(String str);
 	}
+
 }
