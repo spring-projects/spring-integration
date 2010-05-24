9cf953c677ae708ba5ab572550a0a82f88fae66d
Mark Fisher <markfisher@vmware.com>
2010-05-24 14:18:48 +0000

    renamed AggregationResendTest to AggregationResendTests

--- org.springframework.integration/src/test/java/org/springframework/integration/aggregator/scenarios/AggregationResendTests.java
+++ org.springframework.integration/src/test/java/org/springframework/integration/aggregator/scenarios/AggregationResendTests.java
@@ -38,9 +38,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  *
  * @author Iwein Fuld
  */
-@ContextConfiguration(locations = {"aggregation-resend-config.xml"})
+@ContextConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
-public class AggregationResendTest {
+public class AggregationResendTests {
 
 	@Autowired
 	DirectChannel input_for_aggregator_with_explicit_timeout;
