de9b62bd3e2920b82fc5b662f3886eb93366877e
Chris Beams <cbeams@vmware.com>
2010-06-02 00:26:42 +0000

    build-distribution.sh script can now be safely run from within any directory, including its own

--- src/main/scripts/build-distribution.sh
+++ src/main/scripts/build-distribution.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-mvn -Dmaven.test.skip=true clean package javadoc:aggregate docbkx:generate-pdf docbkx:generate-html assembly:assembly
+cd `dirname $0`
+mvn -f ../../../pom.xml -Dmaven.test.skip=true clean package javadoc:aggregate docbkx:generate-pdf docbkx:generate-html assembly:assembly
+cd -
