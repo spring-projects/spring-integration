9724b0c832afaa48bfaf5902a113d1c45f6f802b
Mark Fisher <markfisher@vmware.com>
2010-06-14 23:35:27 +0000

    INT-1154 The <header-enricher> element now supports the 'send-timeout' attribute.

--- spring-integration-core/src/main/java/org/springframework/integration/config/xml/AbstractTransformerParser.java
+++ spring-integration-core/src/main/java/org/springframework/integration/config/xml/AbstractTransformerParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,6 +37,7 @@ public abstract class AbstractTransformerParser extends AbstractConsumerEndpoint
 		String transformerBeanName = BeanDefinitionReaderUtils.registerWithGeneratedName(
 				transformerBuilder.getBeanDefinition(), parserContext.getRegistry());
 		builder.addConstructorArgReference(transformerBeanName);
+		IntegrationNamespaceUtils.setValueIfAttributeDefined(builder, element, "send-timeout");
 		return builder;
 	}
 
