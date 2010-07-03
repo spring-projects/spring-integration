08928f9a8042ba016155abd41c72733ffd599349
Mark Fisher <markfisher@vmware.com>
2010-07-03 20:39:49 +0000

    added a 0 timeout for receive calls in test

--- spring-integration-core/src/test/java/org/springframework/integration/router/config/RecipientListRouterParserTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/router/config/RecipientListRouterParserTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,8 +58,8 @@ public class RecipientListRouterParserTests {
 		channel.send(message);
 		PollableChannel chanel1 = (PollableChannel) context.getBean("channel1");
 		PollableChannel chanel2 = (PollableChannel) context.getBean("channel2");
-		assertTrue(chanel1.receive().getPayload().equals(1));
-		assertTrue(chanel2.receive().getPayload().equals(1));
+		assertTrue(chanel1.receive(0).getPayload().equals(1));
+		assertTrue(chanel2.receive(0).getPayload().equals(1));
 	}
 
 	@Test
