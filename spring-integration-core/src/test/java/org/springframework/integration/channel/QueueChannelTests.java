8189f99d1e56c6b95acce1ec97458a0b41ec5710
Mark Fisher <markfisher@vmware.com>
2010-07-19 14:31:15 +0000

    increased latch await timeout in test

--- spring-integration-core/src/test/java/org/springframework/integration/channel/QueueChannelTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/channel/QueueChannelTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,7 +57,7 @@ public class QueueChannelTests {
 		}).start();
 		assertFalse(messageReceived.get());
 		channel.send(new GenericMessage<String>("testing"));
-		latch.await(25, TimeUnit.MILLISECONDS);
+		latch.await(1000, TimeUnit.MILLISECONDS);
 		assertTrue(messageReceived.get());
 	}
 
