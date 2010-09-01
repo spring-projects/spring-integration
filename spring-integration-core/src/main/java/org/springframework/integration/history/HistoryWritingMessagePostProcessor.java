a73810dbf962644dfc20a4fc21b20f41a21a37f5
Mark Fisher <markfisher@vmware.com>
2010-09-01 20:34:39 +0000

    INT-1263 added HistoryWritingMessagePostProcessor

---
+++ spring-integration-core/src/main/java/org/springframework/integration/history/HistoryWritingMessagePostProcessor.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.history;
+
+import org.springframework.integration.Message;
+import org.springframework.integration.core.MessagePostProcessor;
+import org.springframework.util.Assert;
+
+/**
+ * @author Mark Fisher
+ * @since 2.0
+ */
+public class HistoryWritingMessagePostProcessor implements MessagePostProcessor {
+
+	private volatile TrackableComponent trackableComponent;
+
+	private volatile boolean shouldTrack;
+
+
+	public HistoryWritingMessagePostProcessor() {
+	}
+
+	public HistoryWritingMessagePostProcessor(TrackableComponent trackableComponent) {
+		Assert.notNull(trackableComponent, "trackableComponent must not be null");
+		this.trackableComponent = trackableComponent;
+	}
+
+
+	public void setTrackableComponent(TrackableComponent trackableComponent) {
+		this.trackableComponent = trackableComponent;
+	}
+
+	public void setShouldTrack(boolean shouldTrack) {
+		this.shouldTrack = shouldTrack;
+	}
+
+	public Message<?> postProcessMessage(Message<?> message) {
+		if (this.shouldTrack && this.trackableComponent != null) {
+			return MessageHistory.write(message, this.trackableComponent);
+		}
+		return message;
+	}
+	
+}
