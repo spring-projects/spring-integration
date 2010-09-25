ac49187a64f032d225100fc4927f5ec37d385b05
Oleg Zhurakousky <ozhurakousky@vmware.com>
2010-09-24 21:14:31 -0400

    INT-1444, added tests to validate namespace attributes for FTP and FTPS

---
+++ spring-integration-ftp/src/test/java/org/springframework/integration/ftp/FtpParserTests.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.ftp;
+
+import static junit.framework.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Oleg Zhurakousky
+ *
+ */
+public class FtpParserTests {
+	@Before
+	public void prepare(){
+		new File("target/foo").delete();
+	}
+
+	@Test
+	public void testLocalFilesAutoCreationTrue() throws Exception{
+		assertTrue(!new File("target/foo").exists());
+		new ClassPathXmlApplicationContext("FtpParserTests-inbound.xml", this.getClass());
+		assertTrue(new File("target/foo").exists());
+		assertTrue(!new File("target/bar").exists());
+	}
+	@Test(expected=BeanCreationException.class)
+	public void testLocalFilesAutoCreationFalse() throws Exception{
+		assertTrue(!new File("target/bar").exists());
+		new ClassPathXmlApplicationContext("FtpParserTests-inbound-fail.xml", this.getClass());
+	}
+	
+	@After
+	public void cleanUp() throws Exception{
+		new File("target/foo").delete();
+	}
+}
