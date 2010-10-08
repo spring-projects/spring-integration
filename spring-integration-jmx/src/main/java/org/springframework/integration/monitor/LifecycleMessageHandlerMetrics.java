7cfd0f4a55436845aaf6eca4469f80afcc80bbc6
Dave Syer <dsyer@vmware.com>
2010-10-08 08:15:55 -0700

    INT-1503: *Monitor->*Metrics

---
+++ spring-integration-jmx/src/main/java/org/springframework/integration/monitor/LifecycleMessageHandlerMetrics.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.monitor;
+
+import org.springframework.context.Lifecycle;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
+
+/**
+ * A {@link MessageHandlerMetrics} that exposes in addition the {@link Lifecycle} interface. The lifecycle methods can
+ * be used to stop and start polling endpoints, for instance, in a live system.
+ * 
+ * @author Dave Syer
+ * 
+ * @since 2.0
+ * 
+ */
+@ManagedResource
+public class LifecycleMessageHandlerMetrics implements MessageHandlerMetrics, Lifecycle {
+
+	private final Lifecycle lifecycle;
+
+	private final MessageHandlerMetrics delegate;
+
+	public LifecycleMessageHandlerMetrics(Lifecycle lifecycle, MessageHandlerMetrics delegate) {
+		this.lifecycle = lifecycle;
+		this.delegate = delegate;
+	}
+
+	@ManagedAttribute
+	public boolean isRunning() {
+		return lifecycle.isRunning();
+	}
+
+	@ManagedOperation
+	public void start() {
+		lifecycle.start();
+	}
+
+	@ManagedOperation
+	public void stop() {
+		lifecycle.stop();
+	}
+
+	public int getErrorCount() {
+		return delegate.getErrorCount();
+	}
+
+	public int getHandleCount() {
+		return delegate.getHandleCount();
+	}
+
+	public double getMaxDuration() {
+		return delegate.getMaxDuration();
+	}
+
+	public double getMeanDuration() {
+		return delegate.getMeanDuration();
+	}
+
+	public double getMinDuration() {
+		return delegate.getMinDuration();
+	}
+
+	public double getStandardDeviationDuration() {
+		return delegate.getStandardDeviationDuration();
+	}
+
+	public Statistics getDuration() {
+		return delegate.getDuration();
+	}
+
+	public String getName() {
+		return delegate.getName();
+	}
+
+	public String getSource() {
+		return delegate.getSource();
+	}
+
+	public int getActiveCount() {
+		return delegate.getActiveCount();
+	}
+
+}
---
+++ spring-integration-jmx/src/main/java/org/springframework/integration/monitor/PollableChannelMetrics.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.monitor;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.integration.MessageChannel;
+import org.springframework.jmx.export.annotation.ManagedMetric;
+import org.springframework.jmx.support.MetricType;
+
+/**
+ * @author Dave Syer
+ * 
+ * @since 2.0
+ * 
+ */
+public class PollableChannelMetrics extends DirectChannelMetrics {
+
+	private final AtomicInteger receiveCount = new AtomicInteger();
+
+	private final AtomicInteger receiveErrorCount = new AtomicInteger();
+
+	/**
+	 * @param name
+	 */
+	public PollableChannelMetrics(String name) {
+		super(name);
+	}
+
+	@Override
+	protected Object doInvoke(MethodInvocation invocation, String method, MessageChannel channel) throws Throwable {
+		if ("receive".equals(method)) {
+			return monitorReceive(invocation, channel);
+		}
+		return super.doInvoke(invocation, method, channel);
+	}
+
+	private Object monitorReceive(MethodInvocation invocation, MessageChannel channel) throws Throwable {
+		if (logger.isTraceEnabled()) {
+			logger.trace("Recording receive on channel(" + channel + ") ");
+		}
+		try {
+			Object object = invocation.proceed();
+			if (object!=null) {
+				receiveCount.incrementAndGet();
+			}
+			return object;
+
+		}
+		catch (Throwable e) {
+			receiveErrorCount.incrementAndGet();
+			throw e;
+		}
+	}
+
+	@ManagedMetric(metricType = MetricType.COUNTER, displayName = "MessageChannel Receives")
+	public int getReceiveCount() {
+		return receiveCount.get();
+	}
+
+	@ManagedMetric(metricType = MetricType.COUNTER, displayName = "MessageChannel Receive Errors")
+	public int getReceiveErrorCount() {
+		return receiveErrorCount.get();
+	}
+
+	@Override
+	public String toString() {
+		return String.format("MessageChannelMonitor: [name=%s, sends=%d, receives=%d]", getName(), getSendCount(),
+				receiveCount.get());
+	}
+
+}
