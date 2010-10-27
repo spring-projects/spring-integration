4d3cf9cad4658711319c8bec87606574c0e042c8
Mark Fisher <markfisher@vmware.com>
2010-10-27 13:10:35 -0400

    polishing

--- spring-integration-core/src/main/java/org/springframework/integration/context/ConverterRegistrar.java
+++ spring-integration-core/src/main/java/org/springframework/integration/context/ConverterRegistrar.java
@@ -13,39 +13,53 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.integration.context;
 
 import java.util.Set;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.support.ConversionServiceFactory;
 import org.springframework.core.convert.support.GenericConversionService;
 import org.springframework.util.Assert;
 
 /**
+ * Utility class that keeps track of a set of Converters in order to register
+ * them with the "integrationConversionService" upon initialization.
+ * 
  * @author Oleg Zhurakousky
+ * @author Mark Fisher
  * @since 2.0
  */
 class ConverterRegistrar implements InitializingBean, BeanFactoryAware {
+
 	private final Set<Converter<?, ?>> converters;
+
 	private BeanFactory beanFactory;
-	
-	public ConverterRegistrar(Set<Converter<?, ?>> converters){
+
+
+	public ConverterRegistrar(Set<Converter<?, ?>> converters) {
 		this.converters = converters;
 	}
 
-	public void afterPropertiesSet() throws Exception {
-		GenericConversionService conversionService = beanFactory.getBean(IntegrationContextUtils.INTEGRATION_CONVERSION_SERVICE_BEAN_NAME, GenericConversionService.class);
-		Assert.notNull(conversionService, "can not locate '" + IntegrationContextUtils.INTEGRATION_CONVERSION_SERVICE_BEAN_NAME + "' ");
-		ConversionServiceFactory.registerConverters(converters, conversionService);
-	}
 
-	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+	public void setBeanFactory(BeanFactory beanFactory) {
 		this.beanFactory = beanFactory;
 	}
-	
+
+	public void afterPropertiesSet() throws Exception {
+		Assert.notNull(beanFactory, "BeanFactory is required");
+		ConversionService conversionService = IntegrationContextUtils.getConversionService(beanFactory);
+		if (conversionService instanceof GenericConversionService) {
+			ConversionServiceFactory.registerConverters(converters, (GenericConversionService) conversionService);			
+		}
+		else {
+			Assert.notNull(conversionService, "Failed to locate '" + IntegrationContextUtils.INTEGRATION_CONVERSION_SERVICE_BEAN_NAME + "'");
+		}
+	}
+
 }
