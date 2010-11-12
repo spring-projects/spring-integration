da48d96237ababd6f65ee92a294db59830d85d3f
Mark Fisher <markfisher@vmware.com>
2010-11-12 08:41:49 -0500

    INT-1562 renamed outbound channel adapter parsers

---
+++ spring-integration-ftp/src/main/java/org/springframework/integration/ftp/config/FtpOutboundChannelAdapterParser.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.ftp.config;
+
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.integration.config.xml.AbstractOutboundChannelAdapterParser;
+import org.springframework.integration.config.xml.IntegrationNamespaceUtils;
+
+import org.w3c.dom.Element;
+
+/**
+ * Logic for parsing the ftp:outbound-channel-adapter
+ *
+ * @author Josh Long
+ */
+public class FtpOutboundChannelAdapterParser extends AbstractOutboundChannelAdapterParser {
+
+	@Override
+	protected AbstractBeanDefinition parseConsumer(Element element, ParserContext parserContext) {
+		BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(
+				FtpSendingMessageHandlerFactoryBean.class.getName());
+		IntegrationNamespaceUtils.setValueIfAttributeDefined(builder,element,"charset");
+		IntegrationNamespaceUtils.setReferenceIfAttributeDefined(builder,element,"filename-generator", "fileNameGenerator");
+		FtpNamespaceParserSupport.configureCoreFtpClient(builder, element, parserContext);
+		return builder.getBeanDefinition();
+	}
+
+}
