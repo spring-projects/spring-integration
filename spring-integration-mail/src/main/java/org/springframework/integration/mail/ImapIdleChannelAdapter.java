aab914676e5280b45b6bf9813480a0b5e882c9b4
Oleg Zhurakousky <ozhurakousky@vmware.com>
2011-01-10 09:33:52 -0500

    INT-1701 updated reference documentation regarding changes to IMAP IDLE auto-reconnect, changed copyright versions

--- spring-integration-mail/src/main/java/org/springframework/integration/mail/ImapIdleChannelAdapter.java
+++ spring-integration-mail/src/main/java/org/springframework/integration/mail/ImapIdleChannelAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,6 +41,7 @@ import org.springframework.util.Assert;
  * 
  * @author Arjen Poutsma
  * @author Mark Fisher
+ * @author Oleg Zhurakousky
  */
 public class ImapIdleChannelAdapter extends MessageProducerSupport {
 
@@ -136,6 +137,7 @@ public class ImapIdleChannelAdapter extends MessageProducerSupport {
 					}
 				}
 				catch (MessagingException e) {
+					e.printStackTrace();
 					if (shouldReconnectAutomatically){
 						if (e instanceof FolderClosedException || 
 							e instanceof StoreClosedException || 
