015c059cd94848038fc8cef13059d0fc9b4074cf
Gary Russell <grussell@vmware.com>
2011-03-25 12:13:51 -0400

    INT-1850 Update Copyrights

--- spring-integration-core/src/main/java/org/springframework/integration/handler/MessageHandlerChain.java
+++ spring-integration-core/src/main/java/org/springframework/integration/handler/MessageHandlerChain.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,6 +62,7 @@ import org.springframework.util.Assert;
  * 
  * @author Mark Fisher
  * @author Iwein Fuld
+ * @author Gary Russell
  */
 public class MessageHandlerChain extends AbstractMessageHandler implements MessageProducer, Ordered {
 
--- spring-integration-core/src/test/java/org/springframework/integration/handler/MessageHandlerChainTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/handler/MessageHandlerChainTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +35,7 @@ import org.springframework.integration.support.MessageBuilder;
 /**
  * @author Mark Fisher
  * @author Iwein Fuld
+ * @author Gary Russell
  */
 public class MessageHandlerChainTests {
 
