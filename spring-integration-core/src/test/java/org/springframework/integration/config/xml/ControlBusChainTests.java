222cf7acbbda879c8d410bb3fc515daba4d5988b
Mark Fisher <markfisher@vmware.com>
2011-04-22 09:10:21 -0400

    INT-1874 the control-bus element is now allowed in a chain

---
+++ spring-integration-core/src/test/java/org/springframework/integration/config/xml/ControlBusChainTests.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.config.xml;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.Date;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.Message;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.core.PollableChannel;
+import org.springframework.integration.message.GenericMessage;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Mark Fisher
+ */
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ControlBusChainTests {
+
+	@Autowired
+	private MessageChannel input;
+
+	@Autowired
+	private PollableChannel output;
+
+	@Test
+	public void testDefaultEvaluationContext() {
+		Message<?> message = MessageBuilder.withPayload("@service.convert('aardvark')+headers.foo").setHeader("foo", "bar").build();
+		this.input.send(message);
+		assertEquals("catbar", output.receive(0).getPayload());
+		assertNull(output.receive(0));
+	}
+
+
+	public static class Service {
+
+		@ManagedOperation
+		public String convert(String input) {
+			return "cat";
+		}
+	}
+	
+	public static class AdapterService {
+		public Message<String> receive() {
+			return new GenericMessage<String>(new Date().toString());
+		}
+	}
+
+}
