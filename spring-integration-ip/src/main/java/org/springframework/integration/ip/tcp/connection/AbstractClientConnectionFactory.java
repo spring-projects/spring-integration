31e90fcca308777e9bca7d908545d000e563d940
Gary Russell <grussell@vmware.com>
2011-04-26 21:26:47 -0400

    INT-1854 Remove unused imports; fix copyright dates

--- spring-integration-ip/src/main/java/org/springframework/integration/ip/tcp/connection/AbstractClientConnectionFactory.java
+++ spring-integration-ip/src/main/java/org/springframework/integration/ip/tcp/connection/AbstractClientConnectionFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@ package org.springframework.integration.ip.tcp.connection;
 
 import java.net.Socket;
 import java.net.SocketException;
-import java.util.concurrent.Executors;
 
 import org.springframework.util.Assert;
 
