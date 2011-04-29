7cd30025fcb088badbb2157d92dbadb8f4e67f34
Oleg Zhurakousky <ozhurakousky@vmware.com>
2011-04-29 17:20:03 -0400

    INT-1859 added tests for namespace support for ErrorMessageExceptionTypeRouter

---
+++ spring-integration-core/src/test/java/org/springframework/integration/config/xml/ErrorMessageExceptionTypeRouterParserTests.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.config.xml;
+
+import static junit.framework.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.message.ErrorMessage;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Oleg Zhurakousky
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class ErrorMessageExceptionTypeRouterParserTests {
+	
+	@Autowired
+	private MessageChannel inputChannel;
+	
+	@Autowired
+	private QueueChannel defaultChannel;
+	
+	@Autowired
+	private QueueChannel illegalChannel;
+	
+	@Autowired
+	private QueueChannel npeChannel;
+
+	@Test
+	public void validateExceptionTypeRouterConfig(){
+		
+		inputChannel.send(new ErrorMessage(new NullPointerException()));
+		assertTrue(npeChannel.receive(1000).getPayload() instanceof NullPointerException);
+		
+		inputChannel.send(new ErrorMessage(new IllegalArgumentException()));
+		assertTrue(illegalChannel.receive(1000).getPayload()  instanceof IllegalArgumentException);
+		
+		inputChannel.send(new ErrorMessage(new RuntimeException()));
+		assertTrue(defaultChannel.receive(1000).getPayload()  instanceof RuntimeException);
+	}
+}
