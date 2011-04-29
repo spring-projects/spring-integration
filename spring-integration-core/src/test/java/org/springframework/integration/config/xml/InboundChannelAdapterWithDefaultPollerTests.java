fa67ae75a6545f4826215eed7ac6b75b125e36d6
Mark Fisher <markfisher@vmware.com>
2011-04-29 15:54:48 -0400

    added test for inbound-channel-adapter using the default poller

---
+++ spring-integration-core/src/test/java/org/springframework/integration/config/xml/InboundChannelAdapterWithDefaultPollerTests.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.config.xml;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.DirectFieldAccessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.endpoint.SourcePollingChannelAdapter;
+import org.springframework.integration.test.util.TestUtils;
+import org.springframework.scheduling.Trigger;
+import org.springframework.scheduling.support.PeriodicTrigger;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Mark Fisher
+ */
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class InboundChannelAdapterWithDefaultPollerTests {
+
+	@Autowired
+	private SourcePollingChannelAdapter adapter;
+
+
+	@Test
+	public void verifyDefaultPollerInUse() {
+		Trigger trigger = TestUtils.getPropertyValue(adapter, "trigger", Trigger.class);
+		assertEquals(PeriodicTrigger.class, trigger.getClass());
+		DirectFieldAccessor triggerAccessor = new DirectFieldAccessor(trigger);
+		assertEquals(12345L, triggerAccessor.getPropertyValue("period"));
+		assertEquals(Boolean.TRUE, triggerAccessor.getPropertyValue("fixedRate"));
+	}
+
+}
