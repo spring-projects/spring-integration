34bb27f767de03598b35b6012e1ff32cde1528f1
Oleg Zhurakousky <ozhurakousky@vmware.com>
2011-05-16 12:25:49 -0400

    INT-1899 fixed auto-creation of channel logic not to create errorChannel as DirectChannel

--- spring-integration-core/src/main/java/org/springframework/integration/config/xml/AbstractConsumerEndpointParser.java
+++ spring-integration-core/src/main/java/org/springframework/integration/config/xml/AbstractConsumerEndpointParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,6 +29,7 @@ import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
 import org.springframework.beans.factory.xml.AbstractBeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.integration.MessageHeaders;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.xml.DomUtils;
 
@@ -101,10 +102,12 @@ public abstract class AbstractConsumerEndpointParser extends AbstractBeanDefinit
 		}
 		if (!channelExists) {
 			// create a default DirectChannel instance
-			BeanDefinitionBuilder channelDef = BeanDefinitionBuilder.genericBeanDefinition(
-					IntegrationNamespaceUtils.BASE_PACKAGE + ".channel.DirectChannel");
-			BeanDefinitionHolder holder = new BeanDefinitionHolder(channelDef.getBeanDefinition(), inputChannelName);
-			BeanDefinitionReaderUtils.registerBeanDefinition(holder, parserContext.getRegistry());
+			if (!MessageHeaders.ERROR_CHANNEL.equals(inputChannelName)){
+				BeanDefinitionBuilder channelDef = BeanDefinitionBuilder.genericBeanDefinition(
+						IntegrationNamespaceUtils.BASE_PACKAGE + ".channel.DirectChannel");
+				BeanDefinitionHolder holder = new BeanDefinitionHolder(channelDef.getBeanDefinition(), inputChannelName);
+				BeanDefinitionReaderUtils.registerBeanDefinition(holder, parserContext.getRegistry());
+			}	
 		}
 		builder.addPropertyValue("inputChannelName", inputChannelName);
 		List<Element> pollerElementList = DomUtils.getChildElementsByTagName(element, "poller");
---
+++ spring-integration-core/src/test/java/org/springframework/integration/config/xml/ErrorChannelAutoCreationTests.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.config.xml;
+
+import static junit.framework.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.channel.PublishSubscribeChannel;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Oleg Zhurakousky
+ *
+ */
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ErrorChannelAutoCreationTests {
+	
+	@Autowired
+	private MessageChannel errorChannel;
+	
+	// see INT-1899
+	@Test
+	public void testErrorChannelIsPubSub(){
+		assertTrue(errorChannel instanceof PublishSubscribeChannel);
+	}
+	
+}
