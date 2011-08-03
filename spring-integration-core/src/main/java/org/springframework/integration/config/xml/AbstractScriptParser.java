9f415cc6abb9d18eddfa739de4d14a9f21197632
David Turanski <dturanski@gmail.com>
2011-08-03 15:15:19 +0530

    INT-1576,INT-1579,INT-2025: added spring-integration-scripting and fixed HeaderEnricher issue

---
+++ spring-integration-core/src/main/java/org/springframework/integration/config/xml/AbstractScriptParser.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2002-2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.springframework.integration.config.xml;
+
+import java.util.List;
+
+import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
+import org.springframework.beans.factory.support.ManagedMap;
+import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.beans.factory.xml.XmlReaderContext;
+import org.springframework.scripting.support.StaticScriptSource;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.util.xml.DomUtils;
+import org.w3c.dom.Element;
+
+/**
+ * @author David Turanski
+ *
+ */
+public abstract class AbstractScriptParser extends AbstractSingleBeanDefinitionParser {
+	protected static final String LOCATION_ATTRIBUTE = "location";
+
+	protected static final String REFRESH_CHECK_DELAY_ATTRIBUTE = "refresh-check-delay";
+	
+	protected boolean shouldGenerateIdAsFallback() {
+		return true;
+	}
+	
+	protected abstract String getBeanClassName(Element element);
+	
+	protected abstract String getScriptSourceClassName();
+	
+	@Override
+	protected void doParse(Element element, ParserContext parserContext, BeanDefinitionBuilder builder) {
+		String scriptLocation = element.getAttribute(LOCATION_ATTRIBUTE);
+		String scriptText = DomUtils.getTextValue(element);
+		if (!(StringUtils.hasText(scriptLocation) ^ StringUtils.hasText(scriptText))) {
+			parserContext.getReaderContext().error("Either the 'location' attribute or inline script text must be provided, but not both.", element);
+			return;
+		}
+
+		List<Element> variableElements = DomUtils.getChildElementsByTagName(element, "variable");
+		String scriptVariableGeneratorName = element.getAttribute("script-variable-generator");
+
+		if (StringUtils.hasText(scriptText) && (variableElements.size() > 0 || StringUtils.hasText(scriptVariableGeneratorName))) {
+			parserContext.getReaderContext().error("Variable bindings or custom ScriptVariableGenerator are not allowed when using an inline groovy script. " +
+					"Specify location of the script via 'location' attribute instead", element);
+			return;
+		}
+		
+		if (StringUtils.hasText(scriptVariableGeneratorName) && variableElements.size() > 0) {
+			parserContext.getReaderContext().error("'script-variable-generator' and 'variable' sub-elements are mutually exclusive.", element);
+			return;
+		}
+		
+		if (StringUtils.hasText(scriptLocation)) {
+			builder.addConstructorArgValue(this.resolveScriptLocation(element, parserContext.getReaderContext(), scriptLocation));
+		}
+		else {
+			if (getScriptSourceClassName() != null){
+				builder.addConstructorArgValue(new StaticScriptSource(scriptText, getScriptSourceClassName()));
+			} else {
+				builder.addConstructorArgValue(new StaticScriptSource(scriptText));
+			}
+		}
+		if (!StringUtils.hasText(scriptVariableGeneratorName)) {
+			BeanDefinitionBuilder scriptVariableGeneratorBuilder = 
+					BeanDefinitionBuilder.genericBeanDefinition("org.springframework.integration.scripting.DefaultScriptVariableGenerator");
+			ManagedMap<String, Object> variableMap = new ManagedMap<String, Object>();
+			for (Element childElement : variableElements) {
+				String variableName = childElement.getAttribute("name");
+				String variableValue = childElement.getAttribute("value");
+				String variableRef = childElement.getAttribute("ref");
+				if (!(StringUtils.hasText(variableValue) ^ StringUtils.hasText(variableRef))) {
+					parserContext.getReaderContext().error("Exactly one of the 'ref' attribute or 'value' attribute, " +
+							" is required for element " +
+							IntegrationNamespaceUtils.createElementDescription(element) + ".", element);
+				}
+				if (StringUtils.hasText(variableValue)) {
+					variableMap.put(variableName, variableValue);
+				}
+				else {
+					variableMap.put(variableName, new RuntimeBeanReference(variableRef));
+				}
+			}
+			if (!CollectionUtils.isEmpty(variableMap)) {
+				scriptVariableGeneratorBuilder.addConstructorArgValue(variableMap);
+			}
+			scriptVariableGeneratorName = BeanDefinitionReaderUtils.registerWithGeneratedName(
+					scriptVariableGeneratorBuilder.getBeanDefinition(), parserContext.getRegistry());
+		}
+		builder.addConstructorArgReference(scriptVariableGeneratorName);
+		postProcess(builder, element, parserContext);
+	}
+	
+	/**
+	 * Subclasses may override this no-op method to provide additional configuration.
+	 */
+	protected void postProcess(BeanDefinitionBuilder builder, Element element, ParserContext parserContext) {
+	}
+
+	private Object resolveScriptLocation(Element element, XmlReaderContext readerContext, String scriptLocation) {
+		String refreshDelayText = element.getAttribute(REFRESH_CHECK_DELAY_ATTRIBUTE);			
+		String beanClassName = "org.springframework.integration.scripting.RefreshableResourceScriptSource";
+		BeanDefinitionBuilder resourceScriptSourceBuilder =
+				BeanDefinitionBuilder.genericBeanDefinition(beanClassName);
+		resourceScriptSourceBuilder.addConstructorArgValue(scriptLocation);
+		if (StringUtils.hasText(refreshDelayText)) {
+			resourceScriptSourceBuilder.addConstructorArgValue(refreshDelayText);
+		}
+		else {
+			resourceScriptSourceBuilder.addConstructorArgValue(-1L);				
+		}
+		return resourceScriptSourceBuilder.getBeanDefinition();
+	}
+
+	
+}
