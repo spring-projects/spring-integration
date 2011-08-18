6d40df291f1c81c4bcefe0c48156405f89a2ac81
David Turanski <dturanski@gmail.com>
2011-08-18 13:08:21 -0400

    INT-2062 - Fixed GemfireMessageGroupStoreTest (changed Set to List to avoid equals() collisions affecting size())

--- spring-integration-gemfire/src/test/java/org/springframework/integration/gemfire/store/messagegroupstore/GemfireMessageGroupStoreTest.java
+++ spring-integration-gemfire/src/test/java/org/springframework/integration/gemfire/store/messagegroupstore/GemfireMessageGroupStoreTest.java
@@ -1,5 +1,10 @@
 package org.springframework.integration.gemfire.store.messagegroupstore;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collection;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,50 +13,48 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-
 /**
- * Tests the Gemfire {@link org.springframework.integration.store.MessageGroupStore} implementation,
- * {@link org.springframework.integration.gemfire.store.GemfireMessageGroupStore}.
+ * Tests the Gemfire
+ * {@link org.springframework.integration.store.MessageGroupStore}
+ * implementation,
+ * {@link org.springframework.integration.gemfire.store.GemfireMessageGroupStore}
+ * .
  * <p/>
- * It tests the {@link org.springframework.integration.store.MessageGroupStore} by sending 10 batches of letters (all of the same width),
- * and then counting on the other end that indeed all 10 batches arrived and that all letters expected are there.
- * *
- *
+ * It tests the {@link org.springframework.integration.store.MessageGroupStore}
+ * by sending 10 batches of letters (all of the same width), and then counting
+ * on the other end that indeed all 10 batches arrived and that all letters
+ * expected are there. *
+ * 
  * @author Josh Long
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = {GemfireMessageGroupStoreTestConfiguration.class})
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { GemfireMessageGroupStoreTestConfiguration.class })
 public class GemfireMessageGroupStoreTest {
 
-	@Autowired private GemfireMessageGroupStoreTestConfiguration.FakeMessageConsumer consumer;
+	@Autowired
+	private GemfireMessageGroupStoreTestConfiguration.FakeMessageConsumer consumer;
+	@Autowired
+	private GemfireMessageGroupStoreTestConfiguration.FakeMessageProducer producer;
 
 	private List<String> letters = GemfireMessageGroupStoreTestConfiguration.LIST_OF_STRINGS;
+
 	private int maxSize = 10;
-	private long totalTimeSleeping = 10 * 1000; // give it 10s to send the messages
 
 	@Test
-	public void testGemfireMessageGroupStore() throws Throwable {
-		long counter = 0;
-		int delay = 1000;
-		Set<Collection<Object>> batches = consumer.getBatches();
-
-		while (batches.size() < maxSize && counter < totalTimeSleeping) {
-			counter += delay;
-			Thread.sleep(delay);
-		}
-
-		Assert.assertTrue(batches.size() == maxSize);
-		for (Collection<Object> collection : batches) {
-			Assert.assertTrue(letters.size() == collection.size());
-			for (String c : this.letters) {
-				Assert.assertTrue(collection.contains(c));
+	public void testGemfireMessageGroupStore() throws Exception {
+			producer.afterPropertiesSet();
+			producer.start();
+			List<Collection<Object>> batches = consumer.getBatches();
+			assertEquals(maxSize, batches.size());
+			for (Collection<Object> collection : batches) {
+				Assert.assertTrue(letters.size() == collection.size());
+				for (String c : this.letters) {
+					Assert.assertTrue(collection.contains(c));
+				}
+				for (Object o : collection) {
+					Assert.assertTrue(o instanceof String);
+				}
 			}
-			for (Object o : collection) {
-				Assert.assertTrue(o instanceof String);
-			}
-		}
+			producer.stop();
 	}
 }
