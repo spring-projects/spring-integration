027489b3b7651fb0185d5ef9b2c5c56407875564
Oleg Zhurakousky <ozhurakousky@vmware.com>
2011-09-01 15:59:24 -0400

    INT-1677 added UriPathHandlerMapping to aid ini support of 'path' attribute

--- spring-integration-http/src/main/java/org/springframework/integration/http/inbound/HttpRequestHandlingEndpointSupport.java
+++ spring-integration-http/src/main/java/org/springframework/integration/http/inbound/HttpRequestHandlingEndpointSupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -158,6 +158,10 @@ abstract class HttpRequestHandlingEndpointSupport extends MessagingGatewaySuppor
 	
 	public void setPath(String path) {
 		this.path = path;
+	}
+	
+	String getPath() {
+		return path;
 	}
 
 	public void setPayloadExpression(Expression payloadExpression) {
