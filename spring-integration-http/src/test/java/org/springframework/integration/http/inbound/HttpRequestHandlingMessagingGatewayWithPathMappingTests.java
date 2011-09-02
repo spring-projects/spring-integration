4aa89103bf13da2badb51537746850dd46f3b127
Oleg Zhurakousky <ozhurakousky@vmware.com>
2011-09-02 12:17:21 -0400

    INT-1677 changed uriVariables to pathVariables, added parameterMap and parameters as well as HttpEntity to the EvauationContext

--- spring-integration-http/src/test/java/org/springframework/integration/http/inbound/HttpRequestHandlingMessagingGatewayWithPathMappingTests.java
+++ spring-integration-http/src/test/java/org/springframework/integration/http/inbound/HttpRequestHandlingMessagingGatewayWithPathMappingTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -121,7 +121,7 @@ public class HttpRequestHandlingMessagingGatewayWithPathMappingTests {
 		HttpRequestHandlingMessagingGateway gateway = new HttpRequestHandlingMessagingGateway(true);
 		gateway.setPath("/fname/{f}/lname/{l}");
 		gateway.setRequestChannel(echoChannel);
-		gateway.setPayloadExpression(PARSER.parseExpression("#uriVariables"));
+		gateway.setPayloadExpression(PARSER.parseExpression("#pathVariables"));
 			
 		Object result =  gateway.doHandleRequest(request, response);
 		assertEquals("bill", ((Map<String, Object>)result).get("f"));
