56d9cb9bb5d10afebd73d3cb568fb30aa11fa589
Mark Fisher <markfisher@vmware.com>
2011-09-02 12:27:46 -0400

    INT-2104 on maint

--- spring-integration-mail/src/main/java/org/springframework/integration/mail/ImapIdleChannelAdapter.java
+++ spring-integration-mail/src/main/java/org/springframework/integration/mail/ImapIdleChannelAdapter.java
@@ -111,12 +111,10 @@ public class ImapIdleChannelAdapter extends MessageProducerSupport {
 					public void run() {
 						try {
 							idleTask.run();
-							if (mailReceiver.getFolder().isOpen()){
-								if (logger.isDebugEnabled()){
-									logger.debug("Task completed successfully. Re-scheduling it again right away");
-								}
-								scheduler.schedule(this, new Date());
-							}					
+							if (logger.isDebugEnabled()){
+								logger.debug("Task completed successfully. Re-scheduling it again right away");
+							}
+							scheduler.schedule(this, new Date());					
 						}
 						catch (IllegalStateException e) { //run again after a delay
 							logger.warn("Failed to execute IDLE task. Will atempt to resubmit in " + reconnectDelay + " milliseconds", e);
