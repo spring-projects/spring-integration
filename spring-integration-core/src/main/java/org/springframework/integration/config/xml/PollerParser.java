2fd8f894634745c5f8dc84017c35aba5f5d39e5a
Mark Fisher <markfisher@vmware.com>
2011-09-27 16:52:00 -0400

    PollerParser correctly prevents multiple triggers

--- spring-integration-core/src/main/java/org/springframework/integration/config/xml/PollerParser.java
+++ spring-integration-core/src/main/java/org/springframework/integration/config/xml/PollerParser.java
@@ -126,7 +126,7 @@ public class PollerParser extends AbstractBeanDefinitionParser {
 			}
 			triggerBeanNames.add(triggerAttribute);
 		}
-		else if (StringUtils.hasText(fixedRateAttribute)) {
+		if (StringUtils.hasText(fixedRateAttribute)) {
 			BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(PERIODIC_TRIGGER_CLASSNAME);
 			builder.addConstructorArgValue(fixedRateAttribute);
 			if (StringUtils.hasText(timeUnit)) {
@@ -137,7 +137,7 @@ public class PollerParser extends AbstractBeanDefinitionParser {
 					builder.getBeanDefinition(), parserContext.getRegistry());
 			triggerBeanNames.add(triggerBeanName);
 		}
-		else if (StringUtils.hasText(fixedDelayAttribute)) {
+		if (StringUtils.hasText(fixedDelayAttribute)) {
 			BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(PERIODIC_TRIGGER_CLASSNAME);
 			builder.addConstructorArgValue(fixedDelayAttribute);
 			if (StringUtils.hasText(timeUnit)) {
@@ -148,7 +148,7 @@ public class PollerParser extends AbstractBeanDefinitionParser {
 					builder.getBeanDefinition(), parserContext.getRegistry());
 			triggerBeanNames.add(triggerBeanName);
 		}
-		else if (StringUtils.hasText(cronAttribute)) {
+		if (StringUtils.hasText(cronAttribute)) {
 			if (StringUtils.hasText(timeUnit)) {
 				parserContext.getReaderContext().error("The 'time-unit' attribute cannot be used with a 'cron' trigger.", pollerElement);
 			}
