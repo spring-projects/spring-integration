c13626b3232a571dd76b826aa623f125225e8eeb
David Turanski <dturanski@gmail.com>
2011-09-29 20:38:37 -0400

    INT-2160 - Corrected concurrency issues with JRuby

--- spring-integration-scripting/src/main/java/org/springframework/integration/scripting/jsr223/DefaultScriptExecutor.java
+++ spring-integration-scripting/src/main/java/org/springframework/integration/scripting/jsr223/DefaultScriptExecutor.java
@@ -16,166 +16,85 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
-import javax.script.*;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.integration.scripting.ScriptExecutor;
 import org.springframework.scripting.ScriptSource;
-import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
 
 /**
- * Executes Jsr223 scripts
+ * Executes JSR223 scripts
  * 
  * @author David Turanski
  * @since 2.1
  */
 public class DefaultScriptExecutor implements ScriptExecutor {
+
+	static {
+		if (ClassUtils.isPresent("org.jruby.embed.jsr223.JRubyEngine", System.class.getClassLoader())) {
+			System.setProperty("org.jruby.embed.localvariable.behavior", "transient");
+			System.setProperty("org.jruby.embed.localcontext.scope", "threadsafe");
+		}
+	}
+
 	private static Log log = LogFactory.getLog(DefaultScriptExecutor.class);
 
-	// private ScriptEngine scriptEngine;
-	private final String language;
-
-	private final ScriptEngineManager manager = new ScriptEngineManager();
-	
-	private final ExecutorService pool = Executors.newSingleThreadExecutor();
-	
+	private final ScriptEngine scriptEngine;
 
 	/**
-	 * Set the engine name (language)
+	 * Set the language name (JSR233 alias)
 	 * @param language
 	 */
 	public DefaultScriptExecutor(String language) {
-		this.language = language;
-		// ScriptEngineManager manager = new ScriptEngineManager();
-		// if (log.isDebugEnabled()){
-		// for (ScriptEngineFactory factory: manager.getEngineFactories()) {
-		// log.debug(factory.getNames());
-		// }
-		// }
-		//
-		// scriptEngine = manager.getEngineByName(language);
-		// Assert.notNull(scriptEngine,"JVM cannot create a script engine for name ["
-		// + language + "]");
-		//
+		ScriptEngineManager manager = new ScriptEngineManager();
+
+		scriptEngine = manager.getEngineByName(language);
+		if (log.isDebugEnabled()) {
+			log.debug("using script engine : " + scriptEngine.getFactory().getEngineName());
+		}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.dturanski.test.jruby.ScriptExecutor#executeScript(org.springframework
-	 * .scripting.ScriptSource)
-	 */
 	public Object executeScript(ScriptSource scriptSource) {
 		return this.executeScript(scriptSource, null);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.dturanski.test.jruby.ScriptExecutor#bindVariable(java.lang.String,
-	 * java.lang.Object)
-	 */
-	// private void bindVariable(String variableName, Object value) {
-	// scriptEngine.put(variableName, value);
-	// }
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.springframework.integration.jsr233.ScriptExecutor#executeScript(org
-	 * .springframework.scripting.ScriptSource, java.util.Map)
-	 */
 	public synchronized Object executeScript(ScriptSource scriptSource, Map<String, Object> variables) {
 		Object obj = null;
-		
 		try {
-			
-			ScriptRunner scriptRunner = new ScriptRunner(scriptSource, variables);
-			Future<Object> future = pool.submit(scriptRunner);
+
+			if (variables != null) {
+				for (Entry<String, Object> entry : variables.entrySet()) {
+					scriptEngine.put(entry.getKey(), entry.getValue());
+				}
+			}
+
+			String script = scriptSource.getScriptAsString();
+
 			Date start = new Date();
-		
-			
-			obj = future.get(10,TimeUnit.SECONDS);
-			long time = new Date().getTime() - start.getTime();
-			log.info("script executed in " + time + " ms");
+			if (log.isDebugEnabled()) {
+				log.debug("executing script: " + script + " on instance " + scriptEngine.hashCode());
+			}
+
+			obj = scriptEngine.eval(script);
+
+			if (log.isDebugEnabled()) {
+				log.debug("script executed in " + (new Date().getTime() - start.getTime()) + " ms");
+			}
+
 		}
-		catch(Throwable t) {
-			log.error(t.getMessage(),t);
-			throw new RuntimeException(t);
-		} finally {
-			
+		catch (IOException e) {
+			throw new RuntimeException(e);
 		}
+		catch (ScriptException e) {
+			throw new RuntimeException(e);
+		}
+
 		return obj;
-	}
-	
-	class ScriptRunner implements Callable<Object> {
-        private final String script;
-        private final Map<String,Object> variables;
-		public ScriptRunner(ScriptSource scriptSource, Map<String, Object> variables) throws IOException{
-			this.variables = variables;
-			Assert.notNull(scriptSource, "scriptSource must not be null");
-        	this.script = scriptSource.getScriptAsString();
-        }
-		/* (non-Javadoc)
-		 * @see java.util.concurrent.Callable#call()
-		 */
-		@Override
-		public Object call() throws Exception {
-			
-
-			Object obj = null;
-			try {
-				ScriptEngine scriptEngine = manager.getEngineByName(language);
-				//scriptEngine.getBindings(ScriptContext.GLOBAL_SCOPE).clear();
-
-				//Bindings bindings = scriptEngine.getBindings(ScriptContext.ENGINE_SCOPE);
-
-				if (variables != null) {
-					for (Entry<String, Object> entry : variables.entrySet()) {
-						log.info(entry.getKey() + "=" + entry.getValue().toString());
-						scriptEngine.put(entry.getKey(), entry.getValue());
-					}
-				}
-
-				
-				if (log.isDebugEnabled()) {
-					Thread.currentThread().setName(Thread.currentThread().getName().replace("scheduler", "script"));
-					log.debug(Long.toHexString(Thread.currentThread().getId()) + " executing script: " + script);
-				}
-				obj = scriptEngine.eval(script);
-
-				if (log.isDebugEnabled()) {
-					log.debug(Long.toHexString(Thread.currentThread().getId()) + " returned from eval: ");
-				}
-
-			}
-			catch (Throwable t) {
-				t.printStackTrace();
-				throw new RuntimeException(t);
-			}
-
-			// catch (ScriptException e) {
-			// throw new RuntimeException(e);
-			// }
-			// catch (IOException e) {
-			// throw new RuntimeException(e);
-			// }
-
-			return obj;
-		}
-		
 	}
 
 }
