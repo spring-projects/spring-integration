d4404d217f599b44270f96996eda63bf40275daa
Artem Bilan <clericsmail@gmail.com>
2011-11-23 00:27:10 +0200

    INT-1906: merge conflicts

--- spring-integration-core/src/main/java/org/springframework/integration/config/xml/IntegrationNamespaceHandler.java
+++ spring-integration-core/src/main/java/org/springframework/integration/config/xml/IntegrationNamespaceHandler.java
@@ -52,7 +52,10 @@ public class IntegrationNamespaceHandler extends AbstractIntegrationNamespaceHan
 		registerBeanDefinitionParser("payload-deserializing-transformer", new PayloadDeserializingTransformerParser());
 		registerBeanDefinitionParser("claim-check-in", new ClaimCheckInParser());
 		registerBeanDefinitionParser("claim-check-out", new ClaimCheckOutParser());
-		registerBeanDefinitionParser("inbound-channel-adapter", new DefaultInboundChannelAdapterParser());		registerBeanDefinitionParser("resource-inbound-channel-adapter", new ResourceInboundChannelAdapterParser());		registerBeanDefinitionParser("outbound-channel-adapter", new DefaultOutboundChannelAdapterParser());		registerBeanDefinitionParser("logging-channel-adapter", new LoggingChannelAdapterParser());
+		registerBeanDefinitionParser("inbound-channel-adapter", new DefaultInboundChannelAdapterParser());
+		registerBeanDefinitionParser("resource-inbound-channel-adapter", new ResourceInboundChannelAdapterParser());
+		registerBeanDefinitionParser("outbound-channel-adapter", new DefaultOutboundChannelAdapterParser());
+		registerBeanDefinitionParser("logging-channel-adapter", new LoggingChannelAdapterParser());
 		registerBeanDefinitionParser("gateway", new GatewayParser());
 		registerBeanDefinitionParser("delayer", new DelayerParser());
 		registerBeanDefinitionParser("bridge", new BridgeParser());
