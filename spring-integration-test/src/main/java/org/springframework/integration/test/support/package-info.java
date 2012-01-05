ae502ac58ae5ce532a36f14ab9b83e2c9009fde9
Gunnar Hillert <ghillert@vmware.com>
2012-01-05 15:35:29 -0500

    INT-2382 - add package-info.java files - Test For reference: https://jira.springsource.org/browse/INT-2382

---
+++ spring-integration-test/src/main/java/org/springframework/integration/test/support/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides several test support classes including for testing Spring Integration 
+ * request-response message scenarios.
+ */
+package org.springframework.integration.test.support;
