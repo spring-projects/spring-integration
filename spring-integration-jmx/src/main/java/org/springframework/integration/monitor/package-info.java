89cdef7a902d3c3da0a297c0539c550bf363580e
Gary Russell <grussell@vmware.com>
2012-01-05 15:03:00 -0500

    INT-2373 package-info JMX
    INT-2374 package-info Mail
    
    INT-2375 package-info MongoDB
    
    INT-2376 package-info Redis
    
    INT-2377 package-info RMI
    
    INT-2379 package-info Security
    
    INT-2381 package-info Stream

---
+++ spring-integration-jmx/src/main/java/org/springframework/integration/monitor/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides classes related to Spring Integration managed resources.
+ */
+package org.springframework.integration.monitor;
---
+++ spring-integration-stream/src/main/java/org/springframework/integration/stream/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Base package for stream support.
+ */
+package org.springframework.integration.stream;
