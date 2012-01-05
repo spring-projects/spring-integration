c5d55a54692572c387a80b511d58d6a914a4d9b3
Mark Fisher <markfisher@vmware.com>
2012-01-05 18:16:41 -0500

    removed warnings

--- spring-integration-ftp/src/test/java/org/springframework/integration/ftp/outbound/FtpSendingMessageHandlerTests.java
+++ spring-integration-ftp/src/test/java/org/springframework/integration/ftp/outbound/FtpSendingMessageHandlerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2011 the original author or authors.
+ * Copyright 2002-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -101,7 +101,7 @@ public class FtpSendingMessageHandlerTests {
 		File targetDir = new File("remote-target-dir");
 		assertTrue("target directory does not exist: " + targetDir.getName(), targetDir.exists());
 
-		FileTransferringMessageHandler handler = new FileTransferringMessageHandler(sessionFactory);
+		FileTransferringMessageHandler<FTPFile> handler = new FileTransferringMessageHandler<FTPFile>(sessionFactory);
 		handler.setRemoteDirectoryExpression(new LiteralExpression(targetDir.getName()));
 		handler.setFileNameGenerator(new FileNameGenerator() {
 			public String generateFileName(Message<?> message) {
--- spring-integration-sftp/src/test/java/org/springframework/integration/sftp/outbound/SftpSendingMessageHandlerTests.java
+++ spring-integration-sftp/src/test/java/org/springframework/integration/sftp/outbound/SftpSendingMessageHandlerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2011 the original author or authors.
+ * Copyright 2002-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,8 +55,8 @@ public class SftpSendingMessageHandlerTests {
 		File targetDir = new File("remote-target-dir");
 		assertTrue("target directory does not exist: " + targetDir.getName(), targetDir.exists());
 
-		SessionFactory sessionFactory = new TestSftpSessionFactory();
-		FileTransferringMessageHandler handler = new FileTransferringMessageHandler(sessionFactory);
+		SessionFactory<LsEntry> sessionFactory = new TestSftpSessionFactory();
+		FileTransferringMessageHandler<LsEntry> handler = new FileTransferringMessageHandler<LsEntry>(sessionFactory);
 		handler.setRemoteDirectoryExpression(new LiteralExpression(targetDir.getName()));
 		DefaultFileNameGenerator fGenerator = new DefaultFileNameGenerator();
 		fGenerator.setExpression("payload + '.test'");
