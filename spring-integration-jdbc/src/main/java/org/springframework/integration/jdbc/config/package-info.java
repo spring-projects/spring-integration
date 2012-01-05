7cdb3e09c899bfe6b6f74f5a14cede926177e639
Gunnar Hillert <ghillert@vmware.com>
2012-01-05 12:34:50 -0500

    INT-2370 - add package-info.java files - JDBC Reference: https://jira.springsource.org/browse/INT-2370

---
+++ spring-integration-jdbc/src/main/java/org/springframework/integration/jdbc/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains parser classes for the JDBC namespace support.
+ */
+package org.springframework.integration.jdbc.config;
---
+++ spring-integration-jdbc/src/main/java/org/springframework/integration/jdbc/storedproc/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides Stored Procedure/Function supporting classes. 
+ */
+package org.springframework.integration.jdbc.storedproc;
