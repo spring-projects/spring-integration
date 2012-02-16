37c7e3c2c6c36bf31e0d985ff0e51d4436683a0b
Artem Bilan <clericsmail@gmail.com>
2012-02-16 15:33:44 +0200

    INT-2439 PollerParser adviceChain.add(customBean)
    Fix PollerParser#configureAdviceChain for adding 'customBeanDefinition' into 'adviceChain' List.
    PollerParserTests: adding <tx:advice> as customElement into poller's <advice-chain>.

--- spring-integration-core/src/main/java/org/springframework/integration/config/xml/PollerParser.java
+++ spring-integration-core/src/main/java/org/springframework/integration/config/xml/PollerParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,6 +45,7 @@ import org.springframework.util.xml.DomUtils;
  * @author Mark Fisher
  * @author Marius Bogoevici
  * @author Oleg Zhurakousky
+ * @author Artem Bilan
  */
 public class PollerParser extends AbstractBeanDefinitionParser {
 
@@ -273,6 +274,7 @@ public class PollerParser extends AbstractBeanDefinitionParser {
 							parserContext.getReaderContext().error(
 									"failed to parse custom element '" + localName + "'", childElement);
 						}
+						adviceChain.add(customBeanDefinition);
 					}
 				}
 			}
