22694974842e4eceb65f30cabef63f60bd4b79b1
James Carr <james.r.carr@gmail.com>
2012-04-22 15:19:51 -0500

    modified to ObjectToJsonTransformer to set the content-type on request

--- spring-integration-core/src/main/java/org/springframework/integration/json/ObjectToJsonTransformer.java
+++ spring-integration-core/src/main/java/org/springframework/integration/json/ObjectToJsonTransformer.java
@@ -17,21 +17,26 @@
 package org.springframework.integration.json;
 
 import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.codehaus.jackson.map.ObjectMapper;
-
-import org.springframework.integration.transformer.AbstractPayloadTransformer;
+import org.springframework.integration.Message;
+import org.springframework.integration.message.GenericMessage;
+import org.springframework.integration.transformer.AbstractTransformer;
 import org.springframework.util.Assert;
 
 /**
  * Transformer implementation that converts a payload instance into a JSON string representation.
  *
  * @author Mark Fisher
+ * @author James Carr <james.r.carr@gmail.com>
  * @since 2.0
  */
-public class ObjectToJsonTransformer extends AbstractPayloadTransformer<Object, String> {
-
+public class ObjectToJsonTransformer extends AbstractTransformer {
+	private static final String JSON_CONTENT_TYPE = "application/json";
 	private final ObjectMapper objectMapper;
+	private boolean contentTypeShouldBePopulated = false;
 
 
 	public ObjectToJsonTransformer(ObjectMapper objectMapper) {
@@ -48,6 +53,21 @@ public class ObjectToJsonTransformer extends AbstractPayloadTransformer<Object, 
 		StringWriter writer = new StringWriter();
 		this.objectMapper.writeValue(writer, payload);
 		return writer.toString();
+	}
+
+	public void setContentType(boolean contentTypeShouldBePopulated) {
+		this.contentTypeShouldBePopulated = contentTypeShouldBePopulated;
+		
+	}
+
+	@Override
+	protected Object doTransform(Message<?> message) throws Exception {
+		final String marshalledPayload = transformPayload(message.getPayload());
+		final Map<String, Object> headers = new HashMap<String, Object>(message.getHeaders());
+		if(contentTypeShouldBePopulated){
+			headers.put("content-type", JSON_CONTENT_TYPE);
+		}
+		return new GenericMessage<String>(marshalledPayload, headers);
 	}
 
 }
