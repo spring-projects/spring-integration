87f487007df88c2b414a63456863da20d2dfe20d
Gary Russell <grussell@vmware.com>
2012-05-07 09:15:01 -0400

    INT-1871 Remove Schema Versions
    Schemas update to 2.2 after the PR and the new
    test config had 2.1 versioned schemas.
    
    INT-1871 polishing, removed unused import

--- spring-integration-ip/src/test/java/org/springframework/integration/ip/tcp/connection/CachingClientConnectionFactoryTests.java
+++ spring-integration-ip/src/test/java/org/springframework/integration/ip/tcp/connection/CachingClientConnectionFactoryTests.java
@@ -23,7 +23,6 @@ import static org.junit.Assert.fail;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
