f8782f3dc92e252359816b2151c5ba0e694447e9
Gary Russell <grussell@vmware.com>
2012-07-26 22:24:33 -0400

    INT-2711 syslog Transformer
    Initial Implementation; UDP test.
    
    syslog - Add TCP Test
    
    Add single byte terminating deserializer and a
    subclass that terminates on LF (syslog uses
    LF terminator).
    
    Move to Core
    
    Allow for transport of undecoded packet over, say, AMQP.
    
    INT-2711 Syslog Transformer
    
    PR Comments; Polishing.
    
    Remove List option; support to-map version only.

---
+++ spring-integration-core/src/test/java/org/springframework/integration/config/xml/SyslogTransformerParserTests.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.config.xml;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.core.PollableChannel;
+import org.springframework.integration.message.GenericMessage;
+import org.springframework.integration.transformer.SyslogToMapTransformer;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Gary Russell
+ * @since 2.2
+ *
+ */
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SyslogTransformerParserTests {
+
+	@Autowired
+	private MessageChannel toMapChannel;
+
+	@Autowired
+	private PollableChannel out;
+
+	@Test
+	public void testMap() {
+		toMapChannel.send(new GenericMessage<String>("<157>JUL 26 22:08:35 WEBERN TESTING[70729]: TEST SYSLOG MESSAGE"));
+		Map<?, ?> map = (Map<?, ?>) out.receive(1000).getPayload();
+		assertNotNull(map);
+		assertEquals(6, map.size());
+		System.out.println(map);
+		assertEquals(19, map.get(SyslogToMapTransformer.FACILITY));
+		assertEquals(5, map.get(SyslogToMapTransformer.SEVERITY));
+		assertTrue(map.get(SyslogToMapTransformer.TIMESAMP) instanceof Date);
+		assertEquals("WEBERN", map.get(SyslogToMapTransformer.HOST));
+		assertEquals("TESTING[70729]", map.get(SyslogToMapTransformer.TAG));
+		assertEquals("TEST SYSLOG MESSAGE", map.get(SyslogToMapTransformer.MESSAGE));
+	}
+}
---
+++ spring-integration-core/src/test/java/org/springframework/integration/transformer/SysLogTransformerTests.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.transformer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.junit.Test;
+
+/**
+ * @author Gary Russell
+ * @since 2.2
+ *
+ */
+public class SysLogTransformerTests {
+
+	@Test
+	public void testMap() throws Exception {
+		SyslogToMapTransformer t = new SyslogToMapTransformer();
+		Map<String, ?> transformed = t.transformPayload(
+				"<158>JUL 26 22:08:35 WEBERN TESTING[70729]: TEST SYSLOG MESSAGE".getBytes());
+		assertEquals(6, transformed.size());
+//		System.out.println(transformed);
+		assertEquals(19, transformed.get(SyslogToMapTransformer.FACILITY));
+		assertEquals(6, transformed.get(SyslogToMapTransformer.SEVERITY));
+		assertTrue(transformed.get(SyslogToMapTransformer.TIMESAMP) instanceof Date);
+		assertEquals("WEBERN", transformed.get(SyslogToMapTransformer.HOST));
+		assertEquals("TESTING[70729]", transformed.get(SyslogToMapTransformer.TAG));
+		assertEquals("TEST SYSLOG MESSAGE", transformed.get(SyslogToMapTransformer.MESSAGE));
+	}
+
+	@Test
+	public void testBadPattern() throws Exception {
+		SyslogToMapTransformer t = new SyslogToMapTransformer();
+		String syslog = "&158>JUL 26 22:08:35 WEBERN TESTING[70729]: TEST SYSLOG MESSAGE";
+		Map<String, ?> transformed = t.transformPayload(
+				syslog.getBytes());
+		assertEquals(1, transformed.size());
+		assertEquals(syslog, transformed.get(SyslogToMapTransformer.UNDECODED));
+	}
+
+	@Test
+	public void testBadFacilitySeverity() throws Exception {
+		SyslogToMapTransformer t = new SyslogToMapTransformer();
+		String syslog = "<X58>JUL 26 22:08:35 WEBERN TESTING[70729]: TEST SYSLOG MESSAGE";
+		Map<String, ?> transformed = t.transformPayload(
+				syslog.getBytes());
+		assertEquals(1, transformed.size());
+		assertEquals(syslog, transformed.get(SyslogToMapTransformer.UNDECODED));
+	}
+}
