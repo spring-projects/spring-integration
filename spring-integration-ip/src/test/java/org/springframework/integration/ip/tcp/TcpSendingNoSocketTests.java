535f4d0b802601f7b74921696d6e1b7eed53c836
Gary Russell <grussell@vmware.com>
2012-08-07 22:23:30 -0400

    INT-2704 Exception Processing in TCP Adapter
    When using an outbound adapter with a server connection factory
    (an inbound adapter 'owns' the connections), the outbound adapter
    simply logged exceptions.
    
    There are use cases where flows need to know the exception occurs.
    
    Change the adapter to throw a MessagingException.
    
    Update the 2.1 to 2.2. Migration guide explaining that the
    ExpressionEvaluatingRequestHandlerAdvice can be used to restore
    2.1 behavior, and trap the exception.

---
+++ spring-integration-ip/src/test/java/org/springframework/integration/ip/tcp/TcpSendingNoSocketTests.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.ip.tcp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.MessageHandlingException;
+import org.springframework.integration.message.GenericMessage;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Gary Russell
+ * @since 2.2
+ *
+ */
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TcpSendingNoSocketTests {
+
+	@Autowired
+	private MessageChannel shouldFail;
+
+	@Autowired
+	private MessageChannel advised;
+
+	@Test
+	public void exceptionExpected() {
+		try {
+			shouldFail.send(new GenericMessage<String>("foo"));
+			fail("Exception expected");
+		}
+		catch (MessageHandlingException e) {
+			assertEquals("Unable to find outbound socket", e.getMessage());
+		}
+	}
+
+	@Test
+	public void exceptionTrapped() {
+		advised.send(new GenericMessage<String>("foo"));
+	}
+}
