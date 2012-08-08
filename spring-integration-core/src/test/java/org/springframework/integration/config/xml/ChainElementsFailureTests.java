21856675b0e8770cafb125d83a9ace855941b173
Gunnar Hillert <ghillert@vmware.com>
2012-08-08 11:05:19 -0400

    INT-2392 - ChainElementsFailureTests reads wrong 'xmlheader' property For reference: https://jira.springsource.org/browse/INT-2392

--- spring-integration-core/src/test/java/org/springframework/integration/config/xml/ChainElementsFailureTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/config/xml/ChainElementsFailureTests.java
@@ -1,6 +1,6 @@
 package org.springframework.integration.config.xml;
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,6 @@ package org.springframework.integration.config.xml;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 
 import java.io.ByteArrayInputStream;
 import java.util.Properties;
@@ -31,83 +30,84 @@ import org.springframework.core.io.InputStreamResource;
 
 /**
  * @author Oleg Zhurakousky
+ * @author Gunnar Hillert
  *
  */
 public class ChainElementsFailureTests {
-	
+
 	@Test(expected=XmlBeanDefinitionStoreException.class)
 	public void chainServiceActivator() throws Exception {
 		this.bootStrap("service-activator");
 	}
-	
+
 	@Test(expected=XmlBeanDefinitionStoreException.class)
 	public void chainAggregator() throws Exception {
 		this.bootStrap("aggregator");
 	}
-	
+
 	@Test(expected=XmlBeanDefinitionStoreException.class)
 	public void chainChain() throws Exception {
 		this.bootStrap("chain");
 	}
-	
+
 	@Test(expected=XmlBeanDefinitionStoreException.class)
 	public void chainDelayer() throws Exception {
 		this.bootStrap("delayer");
 	}
-	
+
 	@Test(expected=XmlBeanDefinitionStoreException.class)
 	public void chainFilter() throws Exception {
 		this.bootStrap("filter");
 	}
-	
+
 	@Test(expected=XmlBeanDefinitionStoreException.class)
 	public void chainGateway() throws Exception {
 		this.bootStrap("gateway");
 	}
-	
+
 	@Test(expected=XmlBeanDefinitionStoreException.class)
 	public void chainHeaderEnricher() throws Exception {
 		this.bootStrap("header-enricher");
 	}
-	
+
 	@Test(expected=XmlBeanDefinitionStoreException.class)
 	public void chainHeaderFilter() throws Exception {
 		this.bootStrap("header-filter");
 	}
-	
+
 	@Test(expected=XmlBeanDefinitionStoreException.class)
 	public void chainHeaderValueRouter() throws Exception {
 		this.bootStrap("header-value-router");
 	}
-	
+
 	@Test(expected=XmlBeanDefinitionStoreException.class)
 	public void chainTransformer() throws Exception {
 		this.bootStrap("transformer");
 	}
-	
+
 	@Test(expected=XmlBeanDefinitionStoreException.class)
 	public void chainRouter() throws Exception {
 		this.bootStrap("router");
 	}
-	
+
 	@Test(expected=XmlBeanDefinitionStoreException.class)
 	public void chainSplitter() throws Exception {
 		this.bootStrap("splitter");
 	}
-	
+
 	@Test(expected=XmlBeanDefinitionStoreException.class)
 	public void chainResequencer() throws Exception {
 		this.bootStrap("resequencer");
 	}
 
-	
+
 	private ApplicationContext bootStrap(String configProperty) throws Exception {
 		PropertiesFactoryBean pfb = new PropertiesFactoryBean();
 		pfb.setLocation(new ClassPathResource("org/springframework/integration/config/xml/chain-elements-config.properties"));
 		pfb.afterPropertiesSet();
 		Properties prop = pfb.getObject();
 		StringBuffer buffer = new StringBuffer();
-		buffer.append(prop.getProperty("xmlheader")).append(prop.getProperty(configProperty)).append(prop.getProperty("xmlfooter"));
+		buffer.append(prop.getProperty("xmlheaders")).append(prop.getProperty(configProperty)).append(prop.getProperty("xmlfooter"));
 		ByteArrayInputStream stream = new ByteArrayInputStream(buffer.toString().getBytes());
 		GenericApplicationContext ac = new GenericApplicationContext();
 		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(ac);
@@ -116,7 +116,7 @@ public class ChainElementsFailureTests {
 		ac.refresh();
 		return ac;
 	}
-	
+
 	public static class Sampleservice {
 		public String echo(String value){
 			return value;
