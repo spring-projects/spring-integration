f5849b3fffa0f7e2ace9ea52907d6f06556e04d8
Andrew Cowlin <andrew.cowlin@gmail.com>
2013-01-17 22:01:23 +0000

    INT-2853 - Use platform default character encoding

--- spring-integration-core/src/main/java/org/springframework/integration/transformer/ObjectToStringTransformer.java
+++ spring-integration-core/src/main/java/org/springframework/integration/transformer/ObjectToStringTransformer.java
@@ -28,7 +28,7 @@ public class ObjectToStringTransformer extends AbstractPayloadTransformer<Object
 	@Override
 	protected String transformPayload(Object payload) throws Exception {
 		if(payload instanceof byte[]) {
-			return new String((byte[]) payload,"UTF-8");
+			return new String((byte[]) payload);
 		}
 		return payload.toString();
 	}
