12d268baa12e1472a2b787551d259ef3cfa9ea8c
Artem Bilan <clericsmail@gmail.com>
2013-01-23 15:39:09 +0200

    INT-2893: fix chain -> router -> bean or script
    * add XSD element for nested router type
    * add tests on the matter
    * polishing for RouterParserTests
    
    JIRA: https://jira.springsource.org/browse/INT-2893
    
    INT-2893: Polishing
    
    INT-2893: after rebasing to 3.0
    
    XSD changes reverted from 2.2 and moved to 3.0

--- spring-integration-core/src/test/java/org/springframework/integration/router/config/TestRouter.java
+++ spring-integration-core/src/test/java/org/springframework/integration/router/config/TestRouter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,9 @@ public class TestRouter {
 		}
 		if (input == 2) {
 			return "output2";
+		}
+		if (input == 3) {
+			return "channelDoesNotExist";
 		}
 		return null;
 	}
