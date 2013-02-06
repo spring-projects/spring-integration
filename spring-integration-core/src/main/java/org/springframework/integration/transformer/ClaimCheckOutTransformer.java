a20d9a5039a707b47787d1860a9f3973e4f235df
Nick Spacek <nick@spacek.ca>
2013-02-06 16:06:07 -0400

    Add @author and update copyright

--- spring-integration-core/src/main/java/org/springframework/integration/transformer/ClaimCheckOutTransformer.java
+++ spring-integration-core/src/main/java/org/springframework/integration/transformer/ClaimCheckOutTransformer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2011 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,6 +30,7 @@ import org.springframework.util.Assert;
  * 
  * @author Mark Fisher
  * @author Oleg Zhurakousky
+ * @author Nick Spacek
  * @since 2.0
  */
 public class ClaimCheckOutTransformer extends AbstractTransformer {
