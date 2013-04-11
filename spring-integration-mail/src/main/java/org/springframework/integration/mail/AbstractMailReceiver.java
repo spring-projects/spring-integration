1fceb0e8f4691a1e24113c309b5eeefe2c135a23
Gary Russell <grussell@vmware.com>
2013-04-11 13:05:17 -0400

    INT-2991 Filtered Messages Marked SEEN
    Missing else clause caused filtered email messages to
    still be marked as SEEN. Also marked un-filtered messages
    twice if a filter was present.
    
    Add else; add tests.

--- spring-integration-mail/src/main/java/org/springframework/integration/mail/AbstractMailReceiver.java
+++ spring-integration-mail/src/main/java/org/springframework/integration/mail/AbstractMailReceiver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2011 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -322,7 +322,9 @@ public abstract class AbstractMailReceiver extends IntegrationObjectSupport impl
 					}
 				}
 			}
-			filteredMessages.add(message);
+			else {
+				filteredMessages.add(message);
+			}
 		}
 		return filteredMessages.toArray(new Message[filteredMessages.size()]);
 	}
