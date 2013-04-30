e5919799ceea83a48c0e135cdf9d7d2f59f209f7
David Turanski <dturanski_at_vmware_dot_com>
2013-04-30 07:48:27 -0400

    XD-33 - Implemented Local Channel Registry
    modified per Artem's comments and added pub-sub to support inbound taps
    
    Modified tap to use provided wiretap on inbound channels. Tap on outbound not supported
    
    Changes to support conditional shared channel creation and the ability to share a registered channel for inbound() and outbound()

---
+++ spring-integration-core/src/main/java/org/springframework/integration/channel/registry/ChannelRegistry.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.springframework.integration.channel.registry;
+
+import org.springframework.integration.MessageChannel;
+
+/**
+ * A strategy interface used to bind a {@link MessageChannel} to a logical name. The name
+ * is intended to identify a logical consumer or producer of messages. This may be a
+ * queue, a channel adapter, another message channel, a Spring bean, etc.
+ *
+ * @author Mark Fisher
+ * @author David Turanski
+ * @since 3.0
+ */
+public interface ChannelRegistry {
+
+	/**
+	 * Register a message consumer
+	 * @param name the logical identity of the message source
+	 * @param channel the channel bound as a consumer
+	 */
+	void inbound(String name, MessageChannel channel);
+
+	/**
+	 * Register a message producer
+	 * @param name the logical identity of the message target
+	 * @param channel the channel bound as a producer
+	 */
+	void outbound(String name, MessageChannel channel);
+
+	/**
+	 * Create a tap on an already registered inbound channel
+	 * @param the registered name
+	 * @param channel the channel that will receive messages from the tap
+	 */
+	void tap(String name, MessageChannel channel);
+
+}
---
+++ spring-integration-core/src/test/java/org/springframework/integration/channel/registry/LocalChannelRegistryTests.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.springframework.integration.channel.registry;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.integration.Message;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.MessagingException;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageHandler;
+import org.springframework.integration.core.SubscribableChannel;
+import org.springframework.integration.message.GenericMessage;
+
+/**
+ * @author David Turanski
+ * @author Mark Fisher
+ * @since 3.0
+ */
+public class LocalChannelRegistryTests {
+
+	private LocalChannelRegistry registry = new LocalChannelRegistry();
+
+	private ApplicationContext context = new GenericApplicationContext();
+
+	@Before
+	public void setUp() {
+		registry.setApplicationContext(context);
+	}
+
+	@Test
+	public void testInbound() {
+		DirectChannel channel = new DirectChannel();
+		registry.inbound("inbound", channel);
+		assertTrue(context.containsBean("inbound"));
+
+		final AtomicBoolean messageReceived = new AtomicBoolean();
+		channel.subscribe(new MessageHandler() {
+			@Override
+			public void handleMessage(Message<?> message) throws MessagingException {
+				messageReceived.set(true);
+				assertEquals("hello", message.getPayload());
+			}
+		});
+		SubscribableChannel registeredChannel = context.getBean("inbound", SubscribableChannel.class);
+		registeredChannel.send(new GenericMessage<String>("hello"));
+		assertTrue(messageReceived.get());
+	}
+
+	@Test
+	public void testOutbound() {
+		DirectChannel channel = new DirectChannel();
+		registry.outbound("outbound", channel);
+		assertTrue(context.containsBean("outbound"));
+
+		final AtomicBoolean messageReceived = new AtomicBoolean();
+		SubscribableChannel registeredChannel = context.getBean("outbound", SubscribableChannel.class);
+		registeredChannel.subscribe(new MessageHandler() {
+			@Override
+			public void handleMessage(Message<?> message) throws MessagingException {
+				messageReceived.set(true);
+				assertEquals("hello", message.getPayload());
+			}
+		});
+		channel.send(new GenericMessage<String>("hello"));
+		assertTrue(messageReceived.get());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testOutboundTapShouldFail() {
+		DirectChannel channel = new DirectChannel();
+		registry.outbound("outbound", channel);
+		DirectChannel tapChannel = new DirectChannel();
+		registry.tap("outbound", tapChannel);
+	}
+
+	@Test
+	public void testInboundTap() {
+		DirectChannel channel = new DirectChannel();
+		registry.inbound("inbound", channel);
+		DirectChannel tapChannel = new DirectChannel();
+		registry.tap("inbound", tapChannel);
+		final AtomicBoolean originalMessageReceived = new AtomicBoolean();
+		final AtomicBoolean tapMessageReceived = new AtomicBoolean();
+		tapChannel.subscribe(new MessageHandler() {
+			@Override
+			public void handleMessage(Message<?> message) throws MessagingException {
+				tapMessageReceived.set(true);
+				assertEquals("hello", message.getPayload());
+			}
+		});
+		channel.subscribe(new MessageHandler() {
+			@Override
+			public void handleMessage(Message<?> message) throws MessagingException {
+				originalMessageReceived.set(true);
+				assertEquals("hello", message.getPayload());
+			}
+		});
+		MessageChannel registeredChannel = context.getBean("inbound", MessageChannel.class);
+		registeredChannel.send(new GenericMessage<String>("hello"));
+		assertTrue(originalMessageReceived.get());
+		assertTrue(tapMessageReceived.get());
+	}
+
+	@Test
+	public void testFlowThroughRegisteredChannelFromOutboundToInbound() {
+		DirectChannel outbound = new DirectChannel();
+		DirectChannel inbound = new DirectChannel();
+		registry.outbound("foo", outbound);
+		registry.inbound("foo", inbound);
+		final AtomicBoolean messageReceived = new AtomicBoolean();
+		inbound.subscribe(new MessageHandler() {
+			@Override
+			public void handleMessage(Message<?> message) throws MessagingException {
+				messageReceived.set(true);
+				assertEquals("hello", message.getPayload());
+			}
+		});
+		outbound.send(new GenericMessage<String>("hello"));
+		assertTrue(messageReceived.get());
+	}
+
+	@Test
+	public void testFlowThroughRegisteredChannelFromOutboundToInboundWithTap() {
+		DirectChannel outbound = new DirectChannel();
+		DirectChannel inbound = new DirectChannel();
+		DirectChannel tap = new DirectChannel();
+		registry.outbound("foo", outbound);
+		registry.inbound("foo", inbound);
+		registry.tap("foo", tap);
+		final AtomicBoolean originalMessageReceived = new AtomicBoolean();
+		final AtomicBoolean tapMessageReceived = new AtomicBoolean();
+		inbound.subscribe(new MessageHandler() {
+			@Override
+			public void handleMessage(Message<?> message) throws MessagingException {
+				originalMessageReceived.set(true);
+				assertEquals("hello", message.getPayload());
+			}
+		});
+		tap.subscribe(new MessageHandler() {
+			@Override
+			public void handleMessage(Message<?> message) throws MessagingException {
+				tapMessageReceived.set(true);
+				assertEquals("hello", message.getPayload());
+			}
+		});
+		outbound.send(new GenericMessage<String>("hello"));
+		assertTrue(originalMessageReceived.get());
+		assertTrue(tapMessageReceived.get());
+	}
+
+}
