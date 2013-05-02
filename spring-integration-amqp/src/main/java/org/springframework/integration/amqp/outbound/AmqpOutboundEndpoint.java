31745e5764fc3e7c57eb05b31ecd721431cf62d8
Gary Russell <grussell@gopivotal.com>
2013-05-02 12:18:27 -0400

    INT-3002 Fix Content-Type Mapping
    https://jira.springsource.org/browse/INT-3002
    
    The AMQP outbound endpoint behaves differently when configured
    as a gateway compared to when it is configured an adapter.
    
    With the adapter, the header mapper is invoked after the message
    converter (via the MPP callback).
    
    With the gateway, the mapper is invoked before the converter.
    
    The SimpleMessageConverter unconditionally sets the content-type,
    overriding any mapped content type.
    
    Change the code to invoke the header mapper after the converter
    to match the adapter logic.
    
    Remove the TODO. The method now exists, but Using a MPP in the
    gateway case does not provide access to headers on the reply.
    
    Add test cases.

--- spring-integration-amqp/src/main/java/org/springframework/integration/amqp/outbound/AmqpOutboundEndpoint.java
+++ spring-integration-amqp/src/main/java/org/springframework/integration/amqp/outbound/AmqpOutboundEndpoint.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -220,12 +220,11 @@ public class AmqpOutboundEndpoint extends AbstractReplyProducingMessageHandler
 	}
 
 	private Message<?> sendAndReceive(String exchangeName, String routingKey, Message<?> requestMessage) {
-		// TODO: add a convertSendAndReceive method that accepts a MessagePostProcessor so we can map headers?
 		Assert.isTrue(amqpTemplate instanceof RabbitTemplate, "RabbitTemplate implementation is required for send and receive");
 		MessageConverter converter = ((RabbitTemplate) this.amqpTemplate).getMessageConverter();
 		MessageProperties amqpMessageProperties = new MessageProperties();
-		this.headerMapper.fromHeadersToRequest(requestMessage.getHeaders(), amqpMessageProperties);
 		org.springframework.amqp.core.Message amqpMessage = converter.toMessage(requestMessage.getPayload(), amqpMessageProperties);
+		this.headerMapper.fromHeadersToRequest(requestMessage.getHeaders(), amqpMessageProperties);
 		org.springframework.amqp.core.Message amqpReplyMessage = this.amqpTemplate.sendAndReceive(exchangeName, routingKey, amqpMessage);
 		if (amqpReplyMessage == null) {
 			return null;
