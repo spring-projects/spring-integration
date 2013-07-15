91ead921701a25876fcee971854cec95fbd73e8c
jodiestjohn <jodie.stjohn@espn.com>
2013-07-15 14:59:31 -0400

    INT-3087:Fix MongoDbMessageStore RemoveMessage Bug
    Add new query so the same message found in pollMessageFromGroup is the
    one which gets deleted.
    
    See jira for more details: https://jira.springsource.org/browse/INT-3087
    
    INT-3087 Polishing
    
    Use an atomic method in pollMessageFromGroup to pop the first message.
    
    Add test cases for pollMessageFromGroup and removeMessageFromGroup
    where the same message exists in multiple groups.

--- spring-integration-mongodb/src/test/java/org/springframework/integration/mongodb/rules/MongoDbAvailableRule.java
+++ spring-integration-mongodb/src/test/java/org/springframework/integration/mongodb/rules/MongoDbAvailableRule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2011 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@ package org.springframework.integration.mongodb.rules;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.junit.rules.MethodRule;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
@@ -27,8 +26,9 @@ import com.mongodb.Mongo;
 
 /**
  * A {@link MethodRule} implementation that checks for a running MongoDB process.
- * 
+ *
  * @author Oleg Zhurakousky
+ * @author Gary Russell
  * @since 2.1
  */
 public final class MongoDbAvailableRule implements MethodRule {
@@ -39,10 +39,10 @@ public final class MongoDbAvailableRule implements MethodRule {
 		return new Statement() {
 			@Override
 			public void evaluate() throws Throwable {
-				MongoDbAvailable redisAvailable = method.getAnnotation(MongoDbAvailable.class);
-				if (redisAvailable != null) {
+				MongoDbAvailable mongoAvailable = method.getAnnotation(MongoDbAvailable.class);
+				if (mongoAvailable != null) {
 					try {
-						Mongo mongo = new Mongo();	
+						Mongo mongo = new Mongo();
 						mongo.getDatabaseNames();
 					}
 					catch (Exception e) {
