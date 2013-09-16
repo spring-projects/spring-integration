7517963be2a8efdd9eae512bc95ef0b850ce0e42
Artem Bilan <abilan@gopivotal.com>
2013-09-16 18:51:36 +0300

    INT-3027: Headers Enrichment for <enricher>
    * Add `<header>` sub-element to `<enricher>`
    * Refactoring of `EnricherParser`
    * Add Headers Enrichment logic to `ContentEnricher`
    * Add tests
    * What's new and Content Enricher's `<header>` note
    
    JIRA: https://jira.springsource.org/browse/INT-3027
    
    INT-3027: Addressing PR comments
    
    INT-3027: add NPE asserts to map properties
    
    INT-3027: add overwrite & type to enricher header
    
    Move `HeaderValueMessageProcessor` hierarchy to 'transformer.support' package
    
    INT-3027: Make header's overwrite=true by default
    
    Doc Polishing

---
+++ spring-integration-core/src/main/java/org/springframework/integration/transformer/support/ExpressionEvaluatingHeaderValueMessageProcessor.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.transformer.support;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.SpelParserConfiguration;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.integration.Message;
+import org.springframework.integration.handler.ExpressionEvaluatingMessageProcessor;
+
+/**
+ * @author Mark Fisher
+ * @author Artem Bilan
+ * @since 3.0
+ */
+class ExpressionEvaluatingHeaderValueMessageProcessor<T> extends AbstractHeaderValueMessageProcessor<T>
+		implements BeanFactoryAware {
+
+	private static final ExpressionParser expressionParser = new SpelExpressionParser(new SpelParserConfiguration(
+			true, true));
+
+	private final ExpressionEvaluatingMessageProcessor<T> targetProcessor;
+
+	/**
+	 * Create a header value processor for the given Expression and the
+	 * expected type of the expression evaluation result. The expectedType
+	 * may be null if unknown.
+	 */
+	public ExpressionEvaluatingHeaderValueMessageProcessor(Expression expression, Class<T> expectedType) {
+		this.targetProcessor = new ExpressionEvaluatingMessageProcessor<T>(expression, expectedType);
+	}
+
+	/**
+	 * Create a header value processor for the given expression string and
+	 * the expected type of the expression evaluation result. The
+	 * expectedType may be null if unknown.
+	 */
+	public ExpressionEvaluatingHeaderValueMessageProcessor(String expressionString, Class<T> expectedType) {
+		Expression expression = expressionParser.parseExpression(expressionString);
+		this.targetProcessor = new ExpressionEvaluatingMessageProcessor<T>(expression, expectedType);
+	}
+
+	public void setBeanFactory(BeanFactory beanFactory) {
+		this.targetProcessor.setBeanFactory(beanFactory);
+	}
+
+	public T processMessage(Message<?> message) {
+		return this.targetProcessor.processMessage(message);
+	}
+
+}
