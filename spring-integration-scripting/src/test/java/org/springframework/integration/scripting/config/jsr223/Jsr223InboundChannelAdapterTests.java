9a62c41e725558ac6c5e4209ee0ed16e8bfd59a0
Artem Bilan <abilan@gopivotal.com>
2013-09-17 21:59:41 +0300

    INT-2867: add script support to inbound-c-a
    * Add `<script>` and `<expression>` to `<int:inbound-channel-adapter>`
    * Refactoring for `DefaultInboundChannelAdapterParser`
    * Introduce `ScriptExecutingMessageSource` to allow scripts' `MessageProcessor`
    implementations work as a source for polling endpoint
    
    JIRA: https://jira.springsource.org/browse/INT-2867

---
+++ spring-integration-scripting/src/test/java/org/springframework/integration/scripting/config/jsr223/Jsr223InboundChannelAdapterTests.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.scripting.config.jsr223;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.hamcrest.Matchers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.integration.Message;
+import org.springframework.integration.core.PollableChannel;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Artem Bilan
+ * @since 2.0
+ */
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class Jsr223InboundChannelAdapterTests {
+
+	@Autowired
+	@Qualifier("inbound-channel-adapter-channel")
+	private PollableChannel inboundChannelAdapterChannel;
+
+	@Test
+	public void testInt2867InboundChannelAdapter() throws Exception {
+		Message<?> message = this.inboundChannelAdapterChannel.receive(2000);
+		assertNotNull(message);
+		Object payload = message.getPayload();
+		assertThat(payload, Matchers.instanceOf(Date.class));
+		assertTrue(((Date) payload).before(new Date()));
+		assertEquals("bar", message.getHeaders().get("foo"));
+
+		message = this.inboundChannelAdapterChannel.receive(1000);
+		assertNotNull(message);
+
+		message = this.inboundChannelAdapterChannel.receive(10);
+		assertNull(message);
+	}
+
+}
