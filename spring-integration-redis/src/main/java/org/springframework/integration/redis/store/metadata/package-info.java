5e02fe901ebe777d3de08285c272d0fca26ca67e
Gunnar Hillert <ghillert@gopivotal.com>
2013-09-25 00:13:45 -0400

    INT-3085 Add a Redis-backed MetadataStore
    * Add tests
    * Add documentation

---
+++ spring-integration-redis/src/main/java/org/springframework/integration/redis/store/metadata/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides support for Redis-based
+ * {@link org.springframework.integration.store.metadata.MetadataStore}s.
+ */
+package org.springframework.integration.redis.store.metadata;
