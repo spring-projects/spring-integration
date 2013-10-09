0f3ac4d867e33b5af8079fe289bac46026c1a2aa
Artem Bilan <abilan@gopivotal.com>
2013-10-09 14:45:22 +0300

    INT-3126: Document DefaultScriptVariableGenerator
    JIRA: https://jira.springsource.org/browse/INT-3126

--- spring-integration-scripting/src/main/java/org/springframework/integration/scripting/ScriptVariableGenerator.java
+++ spring-integration-scripting/src/main/java/org/springframework/integration/scripting/ScriptVariableGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2011 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,9 @@ import java.util.Map;
 import org.springframework.integration.Message;
 
 /**
+ * Strategy interface to provide a {@link Map} of variables to the script execution context.
+ * Variables may be extracted from the {@link Message} argument.
+ *
  * @author Oleg Zhurakousky
  * @since 2.0.2
  */
