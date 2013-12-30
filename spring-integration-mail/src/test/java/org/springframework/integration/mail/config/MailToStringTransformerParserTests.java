fed608142c293a6c8ed3a59d5148874fe905125d
Artem Bilan <abilan@gopivotal.com>
2013-12-30 19:47:58 +0200

    INT-3241: Upgrade Gradle and some dependencies
    JIRA: https://jira.springsource.org/browse/INT-3241

--- spring-integration-mail/src/test/java/org/springframework/integration/mail/config/MailToStringTransformerParserTests.java
+++ spring-integration-mail/src/test/java/org/springframework/integration/mail/config/MailToStringTransformerParserTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import static org.junit.Assert.assertTrue;
 
 import javax.mail.internet.MimeMessage;
 
-import org.easymock.classextension.EasyMock;
+import org.easymock.EasyMock;
 import org.junit.Test;
 
 import org.springframework.beans.factory.BeanDefinitionStoreException;
