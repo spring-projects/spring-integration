4ae5152effa50192e68bad22837957891a3a4975
Artem Bilan <abilan@gopivotal.com>
2013-12-30 13:21:28 +0200

    INT-3250: Add @EnableIntegration Infrastructure
    JIRA: https://jira.springsource.org/browse/INT-3250
    
    * The main purpose of this change to move all infrastructure bean definitions
    from `AbstractIntegrationNamespaceHandler` to a new `IntegrationRegistrar`.
    * `IntegrationRegistrar` is invoked by the standard `@Configuration` process and also directly from `AbstractIntegrationNamespaceHandler`.
    * Provide some refactoring for consistency.
    * Add AnnotationContext tests
    
    INT-3250: Polishing
    
    * Remove `@EnableIntegration#defaultPublisherChannel`
    * Revert `#jsonPath` & `#xpath` registration logic
    * Fix for DEBUG message on each parse
    
    JIRA: https://jira.springsource.org/browse/INT-3258
    
    INT-3250 Fix JavaDocs & IDEA+Gradle srcDirs issue
    
    INT-3250: Revert `build.gradle` changes
    
    INT-3250 Polishing
    
    * Remove commented out code
    * Add javadoc to IntegrationRegistrar.registerBeanDefinitions()
    * Fix javadoc links to other Spring projects (and JVM, JEE)
    * Add reference documentation and what's new

---
+++ spring-integration-core/src/main/java/org/springframework/integration/config/annotation/EnableIntegration.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.config.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Import;
+import org.springframework.integration.config.IntegrationRegistrar;
+
+/**
+ * Add this annotation to an {@code @Configuration} class to have
+ * the imported Spring Integration configuration :
+ * <pre class="code">
+ * &#064;Configuration
+ * &#064;EnableIntegration
+ * &#064;ComponentScan(basePackageClasses = { MyConfiguration.class })
+ * public class MyIntegrationConfiguration {
+ * }
+ * </pre>
+ *
+ * @author Artem Bilan
+ * @since 4.0
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import(IntegrationRegistrar.class)
+public @interface EnableIntegration {
+
+}
---
+++ spring-integration-core/src/test/java/org/springframework/integration/configuration/EnableIntegrationTests.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.configuration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.Payload;
+import org.springframework.integration.annotation.Publisher;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.config.annotation.EnableIntegration;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.PollableChannel;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+/**
+ * @author Artem Bilan
+ * @since 4.0
+ */
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class EnableIntegrationTests {
+
+	@Autowired
+	private MessageChannel input;
+
+	@Autowired
+	private PollableChannel output;
+
+	@Autowired
+	private PollableChannel publishedChannel;
+
+	@Test
+	public void testAnnotatedServiceActivator() {
+		this.input.send(MessageBuilder.withPayload("Foo").build());
+		Message<?> receive = this.output.receive(1000);
+		assertNotNull(receive);
+		assertEquals("FOO", receive.getPayload());
+
+		receive = this.publishedChannel.receive(1000);
+		assertNotNull(receive);
+		assertEquals("foo", receive.getPayload());
+	}
+
+
+	@Configuration
+	@ComponentScan(basePackageClasses = EnableIntegrationTests.class)
+	@EnableIntegration
+	public static class ContextConfiguration {
+
+		@Bean
+		public MessageChannel input() {
+			return new DirectChannel();
+		}
+
+		@Bean
+		public PollableChannel output() {
+			return new QueueChannel();
+		}
+
+		@Bean
+		public PollableChannel publishedChannel() {
+			return new QueueChannel();
+		}
+
+	}
+
+	@MessageEndpoint
+	public static class AnnotationTestService {
+
+		@ServiceActivator(inputChannel = "input", outputChannel = "output")
+		@Publisher(channel = "publishedChannel")
+		@Payload("#args[0].toLowerCase()")
+		public String handle(String payload) {
+			return payload.toUpperCase();
+		}
+
+	}
+
+}
