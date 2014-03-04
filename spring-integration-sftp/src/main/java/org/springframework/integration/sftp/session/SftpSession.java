c85905ec1cc38df0244ba3e377e8bb5592c574c9
Gary Russell <grussell@gopivotal.com>
2014-03-04 12:08:27 -0500

    INT-3305 SFTP Close Session on Error
    JIRA: https://jira.springsource.org/browse/INT-3305
    
    Previously, if the channel can't be connected (e.g. if the server
    does not support SFTP), the connection to the server remained open.
    
    Close the session in the event of a failure.
    
    Add test with the Apache SSHD server.

--- spring-integration-sftp/src/main/java/org/springframework/integration/sftp/session/SftpSession.java
+++ spring-integration-sftp/src/main/java/org/springframework/integration/sftp/session/SftpSession.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -244,6 +244,7 @@ class SftpSession implements Session<LsEntry> {
 			}
 		}
 		catch (JSchException e) {
+			this.close();
 			throw new IllegalStateException("failed to connect", e);
 		}
 	}
