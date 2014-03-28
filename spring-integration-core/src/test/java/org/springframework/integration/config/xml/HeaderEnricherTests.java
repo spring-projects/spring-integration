61339afc41e8b18ab6fa46f37d37370d7138e8f4
Artem Bilan <abilan@gopivotal.com>
2014-03-28 11:20:45 +0200

    INT-2743: improve `<header-enricher>.<priority>`
    JIRA: https://jira.spring.io/browse/INT-2743

--- spring-integration-core/src/test/java/org/springframework/integration/config/xml/HeaderEnricherTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/config/xml/HeaderEnricherTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2011 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,22 +21,25 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.parsing.BeanDefinitionParsingException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.integration.IntegrationMessageHeaderAccessor;
-import org.springframework.messaging.support.GenericMessage;
+import org.springframework.integration.core.MessagingTemplate;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.integration.transformer.MessageTransformationException;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.PollableChannel;
-import org.springframework.integration.core.MessagingTemplate;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -153,6 +156,15 @@ public class HeaderEnricherTests {
 		Message<?> result = template.sendAndReceive(channel, new GenericMessage<String>("test"));
 		assertNotNull(result);
 		assertEquals(new Integer(42), new IntegrationMessageHeaderAccessor(result).getPriority());
+	}
+
+	@Test
+	public void priorityExpression() {
+		MessagingTemplate template = new MessagingTemplate();
+		MessageChannel channel = context.getBean("priorityExpressionInput", MessageChannel.class);
+		Message<?> result = template.sendAndReceive(channel, new GenericMessage<Map<String, String>>(Collections.singletonMap("priority", "-10")));
+		assertNotNull(result);
+		assertEquals(new Integer(-10), new IntegrationMessageHeaderAccessor(result).getPriority());
 	}
 
 	@Test
