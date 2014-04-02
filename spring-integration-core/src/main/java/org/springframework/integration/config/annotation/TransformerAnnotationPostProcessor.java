8ffc86f3d2a2185149f4edb6349cbf4e010c6a8f
Artem Bilan <abilan@gopivotal.com>
2014-04-02 21:13:43 +0300

    INT-3351: Add `@Poller` for Messaging Annotations
    JIRA: https://jira.spring.io/browse/INT-3351
    
    INT-3351: Polishing according PR comments
    
    * Add `property-placeholder` support for `@Poller`
    * Change `ref` to `value`
    * Add JavaDoc for `array` workaround
    * Add docs
    
    INT-3351: Polishing and fixes
    
    INT-3351 More Tests
    
    INT-3351: PR comments
    
    INT-3351 Final Polishing

--- spring-integration-core/src/main/java/org/springframework/integration/config/annotation/TransformerAnnotationPostProcessor.java
+++ spring-integration-core/src/main/java/org/springframework/integration/config/annotation/TransformerAnnotationPostProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2009 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ package org.springframework.integration.config.annotation;
 import java.lang.reflect.Method;
 
 import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.integration.annotation.Transformer;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.integration.transformer.MessageTransformingHandler;
@@ -32,8 +33,8 @@ import org.springframework.util.StringUtils;
  */
 public class TransformerAnnotationPostProcessor extends AbstractMethodAnnotationPostProcessor<Transformer> {
 
-	public TransformerAnnotationPostProcessor(ListableBeanFactory beanFactory) {
-		super(beanFactory);
+	public TransformerAnnotationPostProcessor(ListableBeanFactory beanFactory, Environment environment) {
+		super(beanFactory, environment);
 	}
 
 
