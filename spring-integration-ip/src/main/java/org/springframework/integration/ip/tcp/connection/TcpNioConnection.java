7e0eb7ad4e90748a23c87b68b1059aa9d876615f
Artem Bilan <abilan@gopivotal.com>
2014-06-25 16:36:39 +0300

    Revert "Fixed infinite loop described in INT-3453."

--- spring-integration-ip/src/main/java/org/springframework/integration/ip/tcp/connection/TcpNioConnection.java
+++ spring-integration-ip/src/main/java/org/springframework/integration/ip/tcp/connection/TcpNioConnection.java
@@ -295,16 +295,8 @@ public class TcpNioConnection extends TcpConnectionSupport {
 	 * @throws IOException
 	 */
 	private synchronized Message<?> convert() throws Exception {
-		if (this.channelInputStream.available() <= 0) {
-			if (!writingToPipe) {
-				return null;
-			}
-			else {
-				Thread.sleep(5);
-				if (this.channelInputStream.available() <= 0) {
-					return null;
-				}
-			}
+		if (!dataAvailable()) {
+			return null;
 		}
 		Message<?> message = null;
 		try {
