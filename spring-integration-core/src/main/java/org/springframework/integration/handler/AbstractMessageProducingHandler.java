f0ab3b03bd236121641f64a122e96281a0c34a8f
Artem Bilan <abilan@pivotal.io>
2014-08-12 13:01:07 +0300

    INT-3402 ChannelNames lazy-resolution Refactoring
    JIRA: https://jira.spring.io/browse/INT-3402
    
    After applying introduction of `AbstractMessageProducingHandler` there was need some simply refactoring to follow with DRY

--- spring-integration-core/src/main/java/org/springframework/integration/handler/AbstractMessageProducingHandler.java
+++ spring-integration-core/src/main/java/org/springframework/integration/handler/AbstractMessageProducingHandler.java
@@ -16,14 +16,18 @@
 
 package org.springframework.integration.handler;
 
+import org.springframework.beans.BeansException;
 import org.springframework.integration.core.MessageProducer;
 import org.springframework.integration.core.MessagingTemplate;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.core.DestinationResolutionException;
+import org.springframework.util.Assert;
 
 /**
  * The base {@link AbstractMessageHandler} implementation for the {@link MessageProducer}.
  *
  * @author David Liu
+ * @author Artem Bilan
  * since 4.1
  */
 public abstract class AbstractMessageProducingHandler extends AbstractMessageHandler
@@ -49,15 +53,37 @@ public abstract class AbstractMessageProducingHandler extends AbstractMessageHan
 	}
 
 	public void setOutputChannelName(String outputChannelName) {
+		Assert.hasText(outputChannelName, "'outputChannelName' must not be empty");
 		this.outputChannelName = outputChannelName;
 	}
 
-	public MessageChannel getOutputChannel() {
-		return outputChannel;
+	@Override
+	protected void onInit() throws Exception {
+		super.onInit();
+		Assert.state(!(this.outputChannelName != null && this.outputChannel != null),
+				"'outputChannelName' and 'outputChannel' are mutually exclusive.");
+		if (getBeanFactory() != null) {
+			this.messagingTemplate.setBeanFactory(getBeanFactory());
+		}
 	}
 
-	public String getOutputChannelName() {
-		return outputChannelName;
+	public MessageChannel getOutputChannel() {
+		if (this.outputChannelName != null) {
+			synchronized (this) {
+				if (this.outputChannelName != null) {
+					try {
+						Assert.state(getBeanFactory() != null, "A bean factory is required to resolve the outputChannel at runtime.");
+						this.outputChannel = getBeanFactory().getBean(this.outputChannelName, MessageChannel.class);
+						this.outputChannelName = null;
+					}
+					catch (BeansException e) {
+						throw new DestinationResolutionException("Failed to look up MessageChannel with name '"
+								+ this.outputChannelName + "' in the BeanFactory.");
+					}
+				}
+			}
+		}
+		return outputChannel;
 	}
 
 }
