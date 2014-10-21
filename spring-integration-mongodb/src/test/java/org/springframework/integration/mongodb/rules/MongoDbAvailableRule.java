e7a64b8ef5ef518a852135d4535ea8d0813249f1
Gary Russell <grussell@pivotal.io>
2014-10-21 17:36:59 -0400

    Speed Up Mongo Build When No Mongo
    @Rule used a 10 second connection timeout.
    
    Polishing

--- spring-integration-mongodb/src/test/java/org/springframework/integration/mongodb/rules/MongoDbAvailableRule.java
+++ spring-integration-mongodb/src/test/java/org/springframework/integration/mongodb/rules/MongoDbAvailableRule.java
@@ -16,7 +16,6 @@
 
 package org.springframework.integration.mongodb.rules;
 
-import com.mongodb.MongoClient;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.rules.MethodRule;
@@ -24,6 +23,9 @@ import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
 
 import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.ServerAddress;
 
 /**
  * A {@link MethodRule} implementation that checks for a running MongoDB process.
@@ -36,6 +38,7 @@ public final class MongoDbAvailableRule implements MethodRule {
 
 	private final Log logger = LogFactory.getLog(this.getClass());
 
+	@Override
 	public Statement apply(final Statement base, final FrameworkMethod method, final Object target) {
 		return new Statement() {
 			@Override
@@ -43,7 +46,8 @@ public final class MongoDbAvailableRule implements MethodRule {
 				MongoDbAvailable mongoAvailable = method.getAnnotation(MongoDbAvailable.class);
 				if (mongoAvailable != null) {
 					try {
-						Mongo mongo = new MongoClient();
+						MongoClientOptions options = new MongoClientOptions.Builder().connectTimeout(100).build();
+						Mongo mongo = new MongoClient(ServerAddress.defaultHost(), options);
 						mongo.getDatabaseNames();
 					}
 					catch (Exception e) {
