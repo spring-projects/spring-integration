b28dd81ac0e9b37d6a56d3949b5802690bdcc1a1
Gary Russell <grussell@pivotal.io>
2015-01-03 11:44:36 -0500

    INT-3592: Fix (S)FTP Gateway CTOR Visibility
    JIRA: https://jira.spring.io/browse/INT-3592
    
    CTORs were incorrectly `private` which is ok for XML
    configuration, but not for DSL/Java Config.

--- spring-integration-ftp/src/main/java/org/springframework/integration/ftp/gateway/FtpOutboundGateway.java
+++ spring-integration-ftp/src/main/java/org/springframework/integration/ftp/gateway/FtpOutboundGateway.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,7 +41,7 @@ public class FtpOutboundGateway extends AbstractRemoteFileOutboundGateway<FTPFil
 		super(sessionFactory, command, expression);
 	}
 
-	private FtpOutboundGateway(RemoteFileTemplate<FTPFile> remoteFileTemplate, String command, String expression) {
+	public FtpOutboundGateway(RemoteFileTemplate<FTPFile> remoteFileTemplate, String command, String expression) {
 		super(remoteFileTemplate, command, expression);
 	}
 
