b741b86f7e333c5405cbb5e48821a4e361833aa3
Artem Bilan <abilan@pivotal.io>
2015-01-08 13:09:56 +0200

    INT-3595: Rename setters for `EERHandlerAdvice`
    JIRA: https://jira.spring.io/browse/INT-3595
    
    The https://jira.spring.io/browse/INT-3554 introduced `Expression` setter variants for those component who accepted only String before.
    Since SF `BeanDefinition` is based on the JavaBean specification there is guaranty for stable work when we have overloaded setters.
    
    The `ExpressionEvaluatingRequestHandlerAdvice` has been missed to fix that requirement

--- spring-integration-core/src/main/java/org/springframework/integration/handler/advice/ExpressionEvaluatingRequestHandlerAdvice.java
+++ spring-integration-core/src/main/java/org/springframework/integration/handler/advice/ExpressionEvaluatingRequestHandlerAdvice.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,7 +68,7 @@ public class ExpressionEvaluatingRequestHandlerAdvice extends AbstractRequestHan
 		this.onSuccessExpression = new SpelExpressionParser().parseExpression(onSuccessExpression);
 	}
 
-	public void setOnSuccessExpression(Expression onSuccessExpression) {
+	public void setExpressionOnSuccess(Expression onSuccessExpression) {
 		this.onSuccessExpression = onSuccessExpression;
 	}
 
@@ -77,7 +77,7 @@ public class ExpressionEvaluatingRequestHandlerAdvice extends AbstractRequestHan
 		this.onFailureExpression = new SpelExpressionParser().parseExpression(onFailureExpression);
 	}
 
-	public void setOnFailureExpression(Expression onFailureExpression) {
+	public void setExpressionOnFailure(Expression onFailureExpression) {
 		this.onFailureExpression = onFailureExpression;
 	}
 
