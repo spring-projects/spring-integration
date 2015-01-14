763b751f200ccb28426c7c8dd4b4482116edfc0c
Gary Russell <grussell@pivotal.io>
2015-01-14 10:37:19 -0500

    Add JMS OG Stop/Start Tests
    http://stackoverflow.com/questions/27935021/spring-batch-partitioning-with-permanent-queue-as-reply-channel/27944832#27944832

---
+++ spring-integration-jms/src/test/java/org/springframework/integration/jms/request_reply/StopStartTests.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.jms.request_reply;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.Lifecycle;
+import org.springframework.integration.core.MessagingTemplate;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Gary Russell
+ * @since 4.2
+ *
+ */
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext
+public class StopStartTests {
+
+	@Autowired
+	@Qualifier("out")
+	private Lifecycle outGateway;
+
+	@Autowired
+	private MessageChannel test;
+
+	@Test
+	public void test() {
+		MessagingTemplate template = new MessagingTemplate(this.test);
+		this.outGateway.start();
+		assertEquals("FOO", template.convertSendAndReceive("foo", String.class));
+		this.outGateway.stop();
+		this.outGateway.start();
+		assertEquals("BAR", template.convertSendAndReceive("bar", String.class));
+		this.outGateway.stop();
+	}
+
+}
