5fe9dec27504db8891f1b6cd135d7c9386f0b92e
Gary Russell <grussell@pivotal.io>
2015-02-07 12:24:33 -0500

    Fix Redis Synchronization Tests
    The "transaction" synchronization tests use a `QueueChannel`.
    
    The payload is a live reference to the Redis object.
    
    It is possible that the test for the returned size occurs after
    the "transaction" commits.
    
    Change the tests to extract the size() on the poller thread so
    that the size is always captured before the "transaction" commits.

--- spring-integration-redis/src/test/java/org/springframework/integration/redis/rules/RedisAvailableTests.java
+++ spring-integration-redis/src/test/java/org/springframework/integration/redis/rules/RedisAvailableTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -129,7 +129,7 @@ public class RedisAvailableTests {
 		ops.add("Ronald Reagan", 20);
 		ops.add("William J. Clinton", 20);
 		ops.add("Abraham Lincoln", 19);
-		ops.add("George Wahington", 18);
+		ops.add("George Washington", 18);
 	}
 
 	protected void deletePresidents(RedisConnectionFactory connectionFactory){
