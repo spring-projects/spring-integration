61e959377dad4217e9c9069f7e0a2b81601e37b4
Artem Bilan <abilan@pivotal.io>
2015-02-20 14:04:48 +0200

    INT-2230: Fix for Double Global Wire-Taps
    JIRA: https://jira.spring.io/browse/INT-2230
    
    Previously two global `<wire-tap>`s with the same `id` registered
    two `GlobalChannelInterceptorWrapper` wrappers with generated names
    for the same (last) `WireTap`. Hence the same `Message` has been sent to the the `wire-tap channel` twice (or more).
    
    Fix the issue with overriding `GlobalChannelInterceptorParser#resolveId` with the `.globalChannelInterceptor` suffix.
    Therefore the last `GlobalChannelInterceptorWrapper` wins overriding all others, if all our `<wire-tap>`s has the same `id`.
    
    INT-2230: Add `id` to the global `channel-interceptor`
    
    Polishing

--- spring-integration-core/src/main/java/org/springframework/integration/config/xml/GlobalWireTapParser.java
+++ spring-integration-core/src/main/java/org/springframework/integration/config/xml/GlobalWireTapParser.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2002-2011 the original author or authors.
- * 
+ * Copyright 2002-2015 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -15,12 +15,15 @@ package org.springframework.integration.config.xml;
 
 import org.w3c.dom.Element;
 
+import org.springframework.beans.factory.BeanDefinitionStoreException;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.xml.ParserContext;
 
 /**
  * Parser for the top level 'wire-tap' element
  * @author David Turanski
+ * @author Artem Bilan
  * @since 2.1
  *
  */
@@ -30,6 +33,12 @@ public class GlobalWireTapParser extends GlobalChannelInterceptorParser {
 	protected Object getBeanDefinitionBuilderConstructorValue(Element element, ParserContext parserContext) {
 		String wireTapBeanName = new WireTapParser().parse(element, parserContext);
 		return new RuntimeBeanReference(wireTapBeanName);
+	}
+
+	@Override
+	protected String resolveId(Element element, AbstractBeanDefinition definition, ParserContext parserContext)
+			throws BeanDefinitionStoreException {
+		return super.resolveId(element, definition, parserContext) + ".globalChannelInterceptor";
 	}
 
 }
