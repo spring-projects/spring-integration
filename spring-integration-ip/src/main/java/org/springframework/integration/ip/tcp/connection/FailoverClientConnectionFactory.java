4a0809432b435f69fd72592407c2688d077f155f
Gary Russell <grussell@pivotal.io>
2015-06-05 17:34:25 -0400

    Fix Failover Tests
    `FailoverClientConnectionFactory`
    - propagate enable manual listener registration to underlying factories

--- spring-integration-ip/src/main/java/org/springframework/integration/ip/tcp/connection/FailoverClientConnectionFactory.java
+++ spring-integration-ip/src/main/java/org/springframework/integration/ip/tcp/connection/FailoverClientConnectionFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,6 +90,13 @@ public class FailoverClientConnectionFactory extends AbstractClientConnectionFac
 					return false;
 				}
 			});
+		}
+	}
+
+	@Override
+	public void enableManualListenerRegistration() {
+		for (AbstractClientConnectionFactory factory : this.factories) {
+			factory.enableManualListenerRegistration();
 		}
 	}
 
