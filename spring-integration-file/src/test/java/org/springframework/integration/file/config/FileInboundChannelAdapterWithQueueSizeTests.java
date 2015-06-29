1738e32ec48bdcd75a6c2dc36e862e7081ad30ad
Gary Russell <grussell@pivotal.io>
2015-06-29 16:13:44 -0400

    INT-2507: File: Fix HeadDirectoryScanner
    JIRA: https://jira.spring.io/browse/INT-2507
    
    When using a `HeadDirectoryScanner`, the use of any filter removed the
    size limitation passed into the contstructor (or `queue-size` attribute
    of a file inbound channel adapter.
    
    The `HeadFilter` was overwritten.
    
    Combine the head filter into a `CompositeFileListFilter` (creating one
    if necessary).

--- spring-integration-file/src/test/java/org/springframework/integration/file/config/FileInboundChannelAdapterWithQueueSizeTests.java
+++ spring-integration-file/src/test/java/org/springframework/integration/file/config/FileInboundChannelAdapterWithQueueSizeTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,40 +16,87 @@
 
 package org.springframework.integration.file.config;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.DirectFieldAccessor;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.integration.file.FileReadingMessageSource;
 import org.springframework.integration.file.HeadDirectoryScanner;
+import org.springframework.integration.test.util.TestUtils;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.assertThat;
-
 /**
  * @author Gunnar Hillert
+ * @author Gary Russell
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class FileInboundChannelAdapterWithQueueSizeTests {
 
-	@Autowired
-	FileReadingMessageSource source;
+	private static final String PATHNAME = System.getProperty("java.io.tmpdir") + "/"
+			+ FileInboundChannelAdapterWithQueueSizeTests.class.getSimpleName();
 
-	private DirectFieldAccessor accessor;
+	private static File inputDir;
+
+	@Autowired
+	@Qualifier("inputDirPoller.adapter.source")
+	FileReadingMessageSource source1;
+
+	@Autowired
+	@Qualifier("inputDirPollerSimpleFilter.adapter.source")
+	FileReadingMessageSource source2;
+
+	@BeforeClass
+	public static void setupInputDir() {
+		inputDir = new File(PATHNAME);
+		inputDir.mkdir();
+		clean();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		clean();
+		inputDir.delete();
+	}
+
+	private static void clean() {
+		File[] files = inputDir.listFiles();
+		for (File file : files) {
+			file.delete();
+		}
+	}
 
 	@Before
-	public void init() {
-		accessor = new DirectFieldAccessor(source);
+	public void generateTestFiles() throws Exception {
+		File.createTempFile("test", null, inputDir).setLastModified(System.currentTimeMillis() - 1000);
+		File.createTempFile("test", null, inputDir).setLastModified(System.currentTimeMillis() - 1000);
+		File.createTempFile("test", null, inputDir).setLastModified(System.currentTimeMillis() - 1000);
 	}
 
 	@Test
 	public void queueSize() {
-		Object scanner = accessor.getPropertyValue("scanner");
-		assertThat(scanner, is(instanceOf(HeadDirectoryScanner.class)));
+		HeadDirectoryScanner scanner1 = TestUtils.getPropertyValue(source1, "scanner", HeadDirectoryScanner.class);
+		HeadDirectoryScanner scanner2 = TestUtils.getPropertyValue(source2, "scanner", HeadDirectoryScanner.class);
+		List<File> files = scanner1.listFiles(new File(PATHNAME));
+		assertEquals(2, files.size());
+		files = scanner2.listFiles(new File(PATHNAME));
+		assertEquals(2, files.size());
+		files.get(0).delete();
+		files.get(1).delete();
+		files = scanner1.listFiles(new File(PATHNAME));
+		assertEquals(1, files.size());
+		files = scanner2.listFiles(new File(PATHNAME));
+		assertEquals(1, files.size());
 	}
+
 }
