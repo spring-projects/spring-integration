583d432f2218f566c9c48cd83f159aa78cb8a3dc
Gary Russell <grussell@pivotal.io>
2015-07-13 17:27:22 -0400

    INT-3764: LastModifiedFileListFilter
    JIRA: https://jira.spring.io/browse/INT-3764
    
    * Add JavaDocs to the `LastModifiedFileListFilter`
    * Add `Namespace Support` to the `file.adoc`

---
+++ spring-integration-file/src/main/java/org/springframework/integration/file/filters/LastModifiedFileListFilter.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.file.filters;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The {@link FileListFilter} implementation to filter those files which
+ * {@link File#lastModified()} is less than the {@link #age} in comparison
+ * with the current time.
+ * <p>
+ * The resolution is done in seconds.
+ *
+ * @author Gary Russell
+ * @since 4.2
+ *
+ */
+public class LastModifiedFileListFilter implements FileListFilter<File> {
+
+	private static final long DEFAULT_AGE = 60;
+
+	private volatile long age = DEFAULT_AGE;
+
+	public long getAge() {
+		return age;
+	}
+
+	/**
+	 * Set the age that files have to be before being passed by this filter.
+	 * If {@link File#lastModified()} plus age is greater than the current time, the file
+	 * is filtered. The resolution is seconds.
+	 * Defaults to 60 seconds.
+	 * @param age the age
+	 */
+	public void setAge(long age) {
+		setAge(age, TimeUnit.SECONDS);
+	}
+
+	/**
+	 * Set the age that files have to be before being passed by this filter.
+	 * If {@link File#lastModified()} plus age is greater than the current time, the file
+	 * is filtered. The resolution is seconds.
+	 * Defaults to 60 seconds.
+	 * @param age the age
+	 * @param unit the timeUnit.
+	 */
+	public void setAge(long age, TimeUnit unit) {
+		this.age = unit.toSeconds(age);
+	}
+
+	@Override
+	public List<File> filterFiles(File[] files) {
+		List<File> list = new ArrayList<File>();
+		long now = System.currentTimeMillis() / 1000;
+		for (File file : files) {
+			if (file.lastModified() / 1000 + this.age <= now) {
+				list.add(file);
+			}
+		}
+		return list;
+	}
+
+}
---
+++ spring-integration-file/src/test/java/org/springframework/integration/file/filters/LastModifiedFileListFilterTests.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.file.filters;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ * @author Gary Russell
+ * @since 4.2
+ *
+ */
+public class LastModifiedFileListFilterTests {
+
+	@Rule
+	public TemporaryFolder folder = new TemporaryFolder();
+
+	@Test
+	public void testAge() throws Exception {
+		LastModifiedFileListFilter filter = new LastModifiedFileListFilter();
+		filter.setAge(1, TimeUnit.SECONDS);
+		File foo = this.folder.newFile();
+		FileOutputStream fileOutputStream = new FileOutputStream(foo);
+		fileOutputStream.write("x".getBytes());
+		fileOutputStream.close();
+		assertEquals(0, filter.filterFiles(new File[] { foo }).size());
+		Thread.sleep(2000);
+		assertEquals(1, filter.filterFiles(new File[] { foo }).size());
+	}
+
+}
