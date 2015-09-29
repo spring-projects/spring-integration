6e838b50ab089eb86aad0980e050cf853ff8cf79
Artem Bilan <abilan@pivotal.io>
2015-09-29 14:03:42 -0400

    INT-3827: ResettableFileListFilter
    JIRA: https://jira.spring.io/browse/INT-3827
    
    Provide a hook to enable removing a file from an `AcceptOnceFileListFilter`,
    for example after a message processing failure.
    
    Make the `CompositeFileListFilter` a `ReversibleFileListFilter` so it can
    delegate to any of its composed filters that are reversible.
    
    INT-3827: Polishing - PR Comments
    (cherry picked from commit 0d721739e92df765503ef57180c00e9a11a51e32)
    
    Conflicts:
    	src/reference/asciidoc/ftp.adoc
    	src/reference/asciidoc/sftp.adoc

---
+++ spring-integration-file/src/main/java/org/springframework/integration/file/filters/ResettableFileListFilter.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.file.filters;
+
+/**
+ * A {@link FileListFilter} that can be reset by removing a specific file from its
+ * state.
+ * @author Gary Russell
+ * @since 4.1.7
+ *
+ */
+public interface ResettableFileListFilter<F> extends FileListFilter<F> {
+
+	/**
+	 * Remove the specified file from the filter so it will pass on the next attempt.
+	 * @param f the element to remove.
+	 * @return true if the file was removed as a result of this call.
+	 */
+	boolean remove(F f);
+
+}
