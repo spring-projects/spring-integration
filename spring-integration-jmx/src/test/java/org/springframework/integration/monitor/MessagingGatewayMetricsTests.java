2082b8d08c2009f333bb986e8685f5b3f7da3e27
Gary Russell <grussell@pivotal.io>
2015-10-24 09:56:04 -0400

    INT-3831: Pure JavaConfig Test

---
+++ spring-integration-jmx/src/test/java/org/springframework/integration/monitor/MessagingGatewayMetricsTests.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2015 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.monitor;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Set;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.gateway.MessagingGatewaySupport;
+import org.springframework.integration.jmx.config.EnableIntegrationMBeanExport;
+import org.springframework.jmx.support.MBeanServerFactoryBean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Artem Bilan
+ * @author Gary Russell
+ * @since 4.2.1
+ */
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext
+public class MessagingGatewayMetricsTests {
+
+	@Autowired
+	private MBeanServer server;
+
+	@Test
+	public void testHandlerMBeanRegistration() throws Exception {
+		Set<ObjectName> names = server.queryNames(new ObjectName("org.springframework.integration:*,name=testGateway"), null);
+		assertEquals(1, names.size());
+	}
+
+	@Configuration
+	@EnableIntegration
+	@EnableIntegrationMBeanExport
+	public static class ContextConfiguration {
+
+		@Bean
+		public MessagingGatewaySupport testGateway() {
+			return new MessagingGatewaySupport() {
+
+				@Override
+				protected void onInit() throws Exception {
+
+				}
+
+			};
+		}
+
+		@Bean
+		public MBeanServerFactoryBean server() {
+			MBeanServerFactoryBean fb = new MBeanServerFactoryBean();
+			fb.setLocateExistingServerIfPossible(true);
+			return fb;
+		}
+
+	}
+
+}
