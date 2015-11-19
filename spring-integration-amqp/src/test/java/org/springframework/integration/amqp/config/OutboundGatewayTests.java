60fe776805d9984ace5a3689ca7b030382a9425b
Gary Russell <grussell@pivotal.io>
2015-11-19 11:14:25 -0500

    INT-3889: Fix AMQP PubSub Channel Test Race Cond.
    JIRA: https://jira.spring.io/browse/INT-3889
    
    The test case `pubSubLostConnectionTest` forces the connection closed and
    then sends another message. It is testing recovery.
    
    However, since the pub/sub queue is an auto-delete queue, we need to wait until
    the new consumer is established and the queue exists, before sending the message after the test.
    Otherwise, since the exchange is fanout, the message is simply dropped.
    
    Also set `@DirtiesContext` on other tests.
    
    Also remove non-tests.
    
    I have __not__ removed the diagnostics because I don't believe this addresses AMQP-543.

--- spring-integration-amqp/src/test/java/org/springframework/integration/amqp/config/OutboundGatewayTests.java
+++ spring-integration-amqp/src/test/java/org/springframework/integration/amqp/config/OutboundGatewayTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,6 +24,7 @@ import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.junit.After;
 import org.junit.Test;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
@@ -37,9 +38,9 @@ import org.springframework.integration.amqp.outbound.AmqpOutboundEndpoint;
 import org.springframework.integration.config.IntegrationEvaluationContextFactoryBean;
 import org.springframework.integration.context.IntegrationContextUtils;
 import org.springframework.integration.handler.MessageProcessor;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.integration.test.util.TestUtils;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.util.ClassUtils;
 
@@ -55,6 +56,11 @@ public class OutboundGatewayTests {
 
 	private final ClassPathXmlApplicationContext context =
 			new ClassPathXmlApplicationContext(getClass().getSimpleName() + "-context.xml", getClass());
+
+	@After
+	public void tearDown() {
+		context.close();
+	}
 
 	@Test
 	public void testVanillaConfiguration() throws Exception {
