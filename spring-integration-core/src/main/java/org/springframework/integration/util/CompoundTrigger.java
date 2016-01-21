1760572c8b1888460a9c08a469816bc9707dfa07
Artem Bilan <abilan@pivotal.io>
2016-01-21 17:18:53 -0500

    Change Reactor `Assert` to Spring `Assert`
    `4.3` only

--- spring-integration-core/src/main/java/org/springframework/integration/util/CompoundTrigger.java
+++ spring-integration-core/src/main/java/org/springframework/integration/util/CompoundTrigger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright 2015-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,14 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.integration.util;
 
 import java.util.Date;
 
 import org.springframework.scheduling.Trigger;
 import org.springframework.scheduling.TriggerContext;
-
-import reactor.core.support.Assert;
+import org.springframework.util.Assert;
 
 /**
  * A {@link Trigger} that delegates the {@link #nextExecutionTime(TriggerContext)}
