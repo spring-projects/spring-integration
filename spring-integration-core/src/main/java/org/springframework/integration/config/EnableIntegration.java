d93d5c3b1189c7f01d15cf4a2554948e6185da51
Artem Bilan <abilan@pivotal.io>
2016-03-18 17:19:46 -0400

    INT-3964: More `@EnableIntegration` documentation
    JIRA: https://jira.spring.io/browse/INT-3964

--- spring-integration-core/src/main/java/org/springframework/integration/config/EnableIntegration.java
+++ spring-integration-core/src/main/java/org/springframework/integration/config/EnableIntegration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2014-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,9 +23,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.springframework.context.annotation.Import;
-import org.springframework.integration.config.IntegrationRegistrar;
 
 /**
+ * The main configuration annotation to enable Spring Integration infrastructure:
+ * - Registers some built-in beans;
+ * - Adds several {@code BeanFactoryPostProcessor}s;
+ * - Adds several {@code BeanPostProcessor}s;
+ * - Adds annotations processors.
+ * <p>
  * Add this annotation to an {@code @Configuration} class to have
  * the imported Spring Integration configuration :
  * <pre class="code">
@@ -38,6 +43,7 @@ import org.springframework.integration.config.IntegrationRegistrar;
  *
  * @author Artem Bilan
  * @since 4.0
+ * @see IntegrationRegistrar
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
