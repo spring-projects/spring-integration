b363bdb15eee00dd4781c8b48f7973f5c594c7f4
Artem Bilan <abilan@pivotal.io>
2016-03-25 16:28:47 -0400

    INT-3541: The XMPP Extension Support on Outbound
    JIRA: https://jira.spring.io/browse/INT-3541
    
    Address PR comments

--- spring-integration-xmpp/src/main/java/org/springframework/integration/xmpp/config/ChatMessageOutboundChannelAdapterParser.java
+++ spring-integration-xmpp/src/main/java/org/springframework/integration/xmpp/config/ChatMessageOutboundChannelAdapterParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,17 +17,38 @@
 package org.springframework.integration.xmpp.config;
 
 
+import org.w3c.dom.Element;
+
+import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.integration.xmpp.outbound.ChatMessageSendingMessageHandler;
+import org.springframework.util.StringUtils;
+
 /**
  * Parser for the XMPP 'outbound-channel-adapter' element
  *
  * @author Oleg Zhurakousky
+ * @author Artem Bilan
+ *
  * @since 2.0
  */
 public class ChatMessageOutboundChannelAdapterParser extends AbstractXmppOutboundChannelAdapterParser {
 
 	@Override
 	protected String getHandlerClassName() {
-		return "org.springframework.integration.xmpp.outbound.ChatMessageSendingMessageHandler";
+		return ChatMessageSendingMessageHandler.class.getName();
+	}
+
+	@Override
+	protected AbstractBeanDefinition parseConsumer(Element element, ParserContext parserContext) {
+		AbstractBeanDefinition beanDefinition = super.parseConsumer(element, parserContext);
+		String extensionProvider = element.getAttribute("extension-provider");
+		if (StringUtils.hasText(extensionProvider)) {
+			beanDefinition.getPropertyValues()
+					.addPropertyValue("extensionProvider", new RuntimeBeanReference(extensionProvider));
+		}
+		return beanDefinition;
 	}
 
 }
