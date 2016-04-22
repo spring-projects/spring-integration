8df487c96ffadf125a6911c6bbf2771f14f26746
Artem Bilan <abilan@pivotal.io>
2016-04-22 15:30:20 -0400

    INT-3991: Add `IntegrationGraphController`
    JIRA: https://jira.spring.io/browse/INT-3991
    
    * Add `IntegrationGraphController` `@RestController` over `IntegrationGraphServer` bean
    * Add `@EnableIntegrationGraphController` and `<int-http:graph-controller>` to register
    `IntegrationGraphController` if the `DispatcherServlet` is in classpath.
    * Also register the `IntegrationGraphServer` bean from the same place, if that isn't presented in the application context yet.
    * Allow to configure the "root" `path` for the `IntegrationGraphController` as a placeholder value via
    `spring.integration.graph.controller.request.mapping.path` property
    * Add tests for both `@EnableIntegrationGraphController` and `<int-http:graph-controller>` cases based on the `MockMvc`

---
+++ spring-integration-http/src/main/java/org/springframework/integration/http/config/EnableIntegrationGraphController.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.http.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.integration.http.management.IntegrationGraphController;
+import org.springframework.integration.http.support.HttpContextUtils;
+
+/**
+ * Enables the {@link IntegrationGraphController} if {@code DispatcherServlet} is present in the classpath.
+ *
+ * @author Artem Bilan
+ *
+ * @since 4.3
+ *
+ * @see  IntegrationGraphController
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Import(IntegrationGraphControllerRegistrarImportSelector.class)
+public @interface EnableIntegrationGraphController {
+
+	/**
+	 * Specify the Request Mapping path for the {@link IntegrationGraphController}.
+	 * Defaults to {@value HttpContextUtils#GRAPH_CONTROLLER_DEFAULT_PATH}.
+	 * @return The Request Mapping path for the {@link IntegrationGraphController}
+	 */
+	@AliasFor("path")
+	String value() default HttpContextUtils.GRAPH_CONTROLLER_DEFAULT_PATH;
+
+	/**
+	 * Specify the Request Mapping path for the {@link IntegrationGraphController}.
+	 * Defaults to {@value HttpContextUtils#GRAPH_CONTROLLER_DEFAULT_PATH}.
+	 * @return The Request Mapping path for the {@link IntegrationGraphController}
+	 */
+	@AliasFor("value")
+	String path() default HttpContextUtils.GRAPH_CONTROLLER_DEFAULT_PATH;
+
+}
