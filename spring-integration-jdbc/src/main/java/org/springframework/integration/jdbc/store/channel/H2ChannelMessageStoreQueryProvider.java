c6821ec3875bde148131e29a0e07fe5a166a3a24
Manuel Jordan <mjordangithub@gmail.com>
2016-05-16 14:30:45 -0500

    INT-3997: Add H2ChannelMessageStoreQueryProvider
    JIRA: https://jira.spring.io/browse/INT-3997
    
    * Created the `H2ChannelMessageStoreQueryProvider` class for the
    `ChannelMessageStoreQueryProvider` interface
    * The `LongRunningIntegrationTest` class needs the
    `RUN_LONG_INTEGRATION_TESTS` variable defined in the 'system'
    and set in true. It to let work the test methods for the
    `H2TxTimeoutMessageStoreTests` class
    * Updated reference documentation about `ChannelMessageStoreQueryProvider`
    * Polishing

---
+++ spring-integration-jdbc/src/main/java/org/springframework/integration/jdbc/store/channel/H2ChannelMessageStoreQueryProvider.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.jdbc.store.channel;
+
+/**
+ * @author Gunnar Hillert
+ * @author Artem Bilan
+ * @author Manuel Jordan
+ * @since 4.3
+ *
+ */
+public class H2ChannelMessageStoreQueryProvider extends AbstractChannelMessageStoreQueryProvider {
+
+	@Override
+	public String getCreateMessageQuery() {
+		return "INSERT into %PREFIX%CHANNEL_MESSAGE(MESSAGE_ID, GROUP_KEY, REGION, CREATED_DATE, MESSAGE_PRIORITY, " +
+				"MESSAGE_SEQUENCE, MESSAGE_BYTES) " +
+				"values (?, ?, ?, ?, ?, NEXT VALUE FOR %PREFIX%MESSAGE_SEQ, ?)";
+	}
+
+	@Override
+	public String getPollFromGroupExcludeIdsQuery() {
+		return "SELECT %PREFIX%CHANNEL_MESSAGE.MESSAGE_ID, %PREFIX%CHANNEL_MESSAGE.MESSAGE_BYTES " +
+				"from %PREFIX%CHANNEL_MESSAGE " +
+				"where %PREFIX%CHANNEL_MESSAGE.GROUP_KEY = :group_key and %PREFIX%CHANNEL_MESSAGE.REGION = :region " +
+				"and %PREFIX%CHANNEL_MESSAGE.MESSAGE_ID not in (:message_ids) " +
+				"order by CREATED_DATE, MESSAGE_SEQUENCE LIMIT 1";
+	}
+
+	@Override
+	public String getPollFromGroupQuery() {
+		return "SELECT %PREFIX%CHANNEL_MESSAGE.MESSAGE_ID, %PREFIX%CHANNEL_MESSAGE.MESSAGE_BYTES " +
+				"from %PREFIX%CHANNEL_MESSAGE " +
+				"where %PREFIX%CHANNEL_MESSAGE.GROUP_KEY = :group_key and %PREFIX%CHANNEL_MESSAGE.REGION = :region " +
+				"order by CREATED_DATE, MESSAGE_SEQUENCE LIMIT 1";
+	}
+
+	@Override
+	public String getPriorityPollFromGroupExcludeIdsQuery() {
+		return "SELECT %PREFIX%CHANNEL_MESSAGE.MESSAGE_ID, %PREFIX%CHANNEL_MESSAGE.MESSAGE_BYTES " +
+				"from %PREFIX%CHANNEL_MESSAGE " +
+				"where %PREFIX%CHANNEL_MESSAGE.GROUP_KEY = :group_key and %PREFIX%CHANNEL_MESSAGE.REGION = :region " +
+				"and %PREFIX%CHANNEL_MESSAGE.MESSAGE_ID not in (:message_ids) " +
+				"order by MESSAGE_PRIORITY DESC NULLS LAST, CREATED_DATE, MESSAGE_SEQUENCE LIMIT 1";
+	}
+
+	@Override
+	public String getPriorityPollFromGroupQuery() {
+		return "SELECT %PREFIX%CHANNEL_MESSAGE.MESSAGE_ID, %PREFIX%CHANNEL_MESSAGE.MESSAGE_BYTES " +
+				"from %PREFIX%CHANNEL_MESSAGE " +
+				"where %PREFIX%CHANNEL_MESSAGE.GROUP_KEY = :group_key and %PREFIX%CHANNEL_MESSAGE.REGION = :region " +
+				"order by MESSAGE_PRIORITY DESC NULLS LAST, CREATED_DATE, MESSAGE_SEQUENCE LIMIT 1";
+	}
+
+}
