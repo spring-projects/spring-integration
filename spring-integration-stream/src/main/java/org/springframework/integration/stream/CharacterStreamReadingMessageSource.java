f9ddefec2cca10bb8f1a0d030cb1aacf79b140e8
Gary Russell <grussell@pivotal.io>
2016-09-22 11:54:00 -0400

    INT-4118: Detect EOF on stdin
    JIRA: https://jira.spring.io/browse/INT-4118
    
    Add an option to the `CharacterStreamReadingMessageSource` to detect EOF
    on the stream and close the context.
    
    Publish event instead of closing context.
    
    Polishing - PR Comments
    
    Polishing
    
    Docs about stopping the poller.
    
    * Fix typos in the docs

--- spring-integration-stream/src/main/java/org/springframework/integration/stream/CharacterStreamReadingMessageSource.java
+++ spring-integration-stream/src/main/java/org/springframework/integration/stream/CharacterStreamReadingMessageSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,10 +22,12 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessagingException;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.integration.context.IntegrationObjectSupport;
 import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.util.Assert;
 
@@ -33,19 +35,61 @@ import org.springframework.util.Assert;
  * A pollable source for {@link Reader Readers}.
  *
  * @author Mark Fisher
+ * @author Gary Russell
  */
-public class CharacterStreamReadingMessageSource extends IntegrationObjectSupport implements MessageSource<String> {
+public class CharacterStreamReadingMessageSource extends IntegrationObjectSupport implements MessageSource<String>,
+		ApplicationEventPublisherAware {
 
 	private final BufferedReader reader;
 
 	private final Object monitor;
 
+	private final boolean blockToDetectEOF;
 
+	private ApplicationEventPublisher applicationEventPublisher;
+
+	/**
+	 * Construct an instance with the provider reader.
+	 * {@link #receive()} will return {@code null} when the reader is not ready.
+	 * @param reader the reader.
+	 */
 	public CharacterStreamReadingMessageSource(Reader reader) {
-		this(reader, -1);
+		this(reader, -1, false);
 	}
 
+	/**
+	 * Construct an instance with the provider reader and buffer size.
+	 * {@link #receive()} will return {@code null} when the reader is not ready.
+	 * @param reader the reader.
+	 * @param bufferSize the buffer size.
+	 */
 	public CharacterStreamReadingMessageSource(Reader reader, int bufferSize) {
+		this(reader, bufferSize, false);
+	}
+
+	/**
+	 * Construct an instance with the provided reader and buffer size.
+	 * When {@code blockToDetectEOF} is {@code false},
+	 * {@link #receive()} will return {@code null} when the reader is not ready.
+	 * When it is {@code true}, the thread will block until data is available; when the
+	 * underlying stream is closed, a {@link StreamClosedEvent} is published to inform
+	 * the application via an {@link org.springframework.context.ApplicationListener}.
+	 * This can be useful, for example, when piping stdin
+	 * <pre class="code">
+	 *     cat foo.txt | java -jar my.jar
+	 * </pre>
+	 * or
+	 * <pre class="code">
+	 *     java -jar my.jar &lt; foo.txt
+	 * </pre>
+	 * @param reader the reader.
+	 * @param bufferSize the buffer size; if negative use the default in
+	 * {@link BufferedReader}.
+	 * @param blockToDetectEOF true to block the thread until data is available and
+	 * publish a {@link StreamClosedEvent} at EOF.
+	 * @since 5.0
+	 */
+	public CharacterStreamReadingMessageSource(Reader reader, int bufferSize, boolean blockToDetectEOF) {
 		Assert.notNull(reader, "reader must not be null");
 		this.monitor = reader;
 		if (reader instanceof BufferedReader) {
@@ -57,20 +101,30 @@ public class CharacterStreamReadingMessageSource extends IntegrationObjectSuppor
 		else {
 			this.reader = new BufferedReader(reader);
 		}
+		this.blockToDetectEOF = blockToDetectEOF;
 	}
 
+	@Override
+	public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+		this.applicationEventPublisher = applicationEventPublisher;
+	}
 
+	@Override
 	public String getComponentType() {
 		return "stream:stdin-channel-adapter(character)";
 	}
 
+	@Override
 	public Message<String> receive() {
 		try {
 			synchronized (this.monitor) {
-				if (!this.reader.ready()) {
+				if (!this.blockToDetectEOF && !this.reader.ready()) {
 					return null;
 				}
 				String line = this.reader.readLine();
+				if (line == null && this.applicationEventPublisher != null) {
+					this.applicationEventPublisher.publishEvent(new StreamClosedEvent(this));
+				}
 				return (line != null) ? new GenericMessage<String>(line) : null;
 			}
 		}
@@ -80,13 +134,50 @@ public class CharacterStreamReadingMessageSource extends IntegrationObjectSuppor
 	}
 
 
+	/**
+	 * Create a source that reads from {@link System#in}. EOF will not be detected.
+	 * @return the stream.
+	 */
 	public static final CharacterStreamReadingMessageSource stdin() {
 		return new CharacterStreamReadingMessageSource(new InputStreamReader(System.in));
 	}
 
+	/**
+	 * Create a source that reads from {@link System#in}. EOF will not be detected.
+	 * @param charsetName the charset to use when converting bytes to String.
+	 * @return the stream.
+	 */
 	public static final CharacterStreamReadingMessageSource stdin(String charsetName) {
 		try {
 			return new CharacterStreamReadingMessageSource(new InputStreamReader(System.in, charsetName));
+		}
+		catch (UnsupportedEncodingException e) {
+			throw new IllegalArgumentException("unsupported encoding: " + charsetName, e);
+		}
+	}
+
+	/**
+	 * Create a source that reads from {@link System#in}. EOF will be detected and the application
+	 * context closed.
+	 * @return the stream.
+	 * @see CharacterStreamReadingMessageSource#CharacterStreamReadingMessageSource(Reader, int, boolean)
+	 * @since 5.0
+	 */
+	public static final CharacterStreamReadingMessageSource stdinPipe() {
+		return new CharacterStreamReadingMessageSource(new InputStreamReader(System.in), -1, true);
+	}
+
+	/**
+	 * Create a source that reads from {@link System#in}. EOF will be detected and the application
+	 * context closed.
+	 * @param charsetName the charset to use when converting bytes to String.
+	 * @return the stream.
+	 * @see CharacterStreamReadingMessageSource#CharacterStreamReadingMessageSource(Reader, int, boolean)
+	 * @since 5.0
+	 */
+	public static final CharacterStreamReadingMessageSource stdinPipe(String charsetName) {
+		try {
+			return new CharacterStreamReadingMessageSource(new InputStreamReader(System.in, charsetName), -1, true);
 		}
 		catch (UnsupportedEncodingException e) {
 			throw new IllegalArgumentException("unsupported encoding: " + charsetName, e);
--- spring-integration-stream/src/test/java/org/springframework/integration/stream/CharacterStreamSourceTests.java
+++ spring-integration-stream/src/test/java/org/springframework/integration/stream/CharacterStreamSourceTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2008 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,16 +17,31 @@
 package org.springframework.integration.stream;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import java.io.StringReader;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.endpoint.SourcePollingChannelAdapter;
 import org.springframework.messaging.Message;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.PeriodicTrigger;
 
 /**
  * @author Mark Fisher
+ * @author Gary Russell
  */
 public class CharacterStreamSourceTests {
 
@@ -38,6 +53,51 @@ public class CharacterStreamSourceTests {
 		assertEquals("test", message1.getPayload());
 		Message<?> message2 = source.receive();
 		assertNull(message2);
+	}
+
+	@Test
+	public void testEOF() {
+		StringReader reader = new StringReader("test");
+		CharacterStreamReadingMessageSource source = new CharacterStreamReadingMessageSource(reader, -1, true);
+		ApplicationEventPublisher publisher = mock(ApplicationEventPublisher.class);
+		source.setApplicationEventPublisher(publisher);
+		Message<?> message1 = source.receive();
+		assertEquals("test", message1.getPayload());
+		Message<?> message2 = source.receive();
+		assertNull(message2);
+		verify(publisher).publishEvent(any(StreamClosedEvent.class));
+	}
+
+	@Test
+	public void testEOFIntegrationTest() throws Exception {
+		StringReader reader = new StringReader("test");
+		CharacterStreamReadingMessageSource source = new CharacterStreamReadingMessageSource(reader, -1, true);
+		SourcePollingChannelAdapter adapter = new SourcePollingChannelAdapter();
+		CountDownLatch latch = new CountDownLatch(2);
+		source.setApplicationEventPublisher(e -> {
+			if (e instanceof StreamClosedEvent) {
+				if (latch.getCount() == 1) {
+					adapter.stop();
+				}
+				latch.countDown();
+			}
+		});
+		adapter.setSource(source);
+		ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+		scheduler.afterPropertiesSet();
+		adapter.setTaskScheduler(scheduler);
+		adapter.setTrigger(new PeriodicTrigger(100));
+		QueueChannel out = new QueueChannel();
+		adapter.setOutputChannel(out);
+		adapter.setBeanFactory(mock(BeanFactory.class));
+		adapter.afterPropertiesSet();
+		adapter.start();
+		Message<?> received = out.receive(10000);
+		assertNotNull(received);
+		assertEquals("test", received.getPayload());
+		assertTrue(latch.await(10, TimeUnit.SECONDS));
+		assertFalse(adapter.isRunning());
+		scheduler.shutdown();
 	}
 
 }
