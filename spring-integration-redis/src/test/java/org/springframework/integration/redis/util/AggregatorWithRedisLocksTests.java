d3fb41045c3dba452168a0dbfa1aecb7076c3a55
Artem Bilan <abilan@pivotal.io>
2016-09-27 21:44:55 -0400

    INT-4126: Fix Redis lock to use `SET NX EX`
    JIRA: https://jira.spring.io/browse/INT-4126
    
    Looks like `WATCH` doesn't provide good isolation when the call is performed concurrently at the same time from different Threads.
    
    * Change locking algorithm as it is recommended by Redis `SET` command: http://redis.io/commands/set:
    ```
    SET resource-name anystring NX EX max-lock-time
    ```
    
    * Add `@Repeat(10)` to the `AggregatorWithRedisLocksTests#testDistributedAggregator()` since that helped to reproduce the issue
    
    **Cherry-pick to 4.3.x & 4.2.x**
    
    Conflicts:
    	spring-integration-redis/src/main/java/org/springframework/integration/redis/util/RedisLockRegistry.java
    Resolved.

--- spring-integration-redis/src/test/java/org/springframework/integration/redis/util/AggregatorWithRedisLocksTests.java
+++ spring-integration-redis/src/test/java/org/springframework/integration/redis/util/AggregatorWithRedisLocksTests.java
@@ -45,11 +45,13 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.PollableChannel;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.Repeat;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author Gary Russell
+ * @author Artem Bilan
  * @since 4.0
  *
  */
@@ -124,6 +126,7 @@ public class AggregatorWithRedisLocksTests extends RedisAvailableTests {
 
 	@Test
 	@RedisAvailable
+	@Repeat(10)
 	public void testDistributedAggregator() throws Exception {
 		this.releaseStrategy.reset(1);
 		Executors.newSingleThreadExecutor().execute(asyncSend("foo", 1, 1));
@@ -222,5 +225,7 @@ public class AggregatorWithRedisLocksTests extends RedisAvailableTests {
 			this.callers = new AtomicInteger();
 			this.maxCallers = new AtomicInteger();
 		}
+
 	}
+
 }
