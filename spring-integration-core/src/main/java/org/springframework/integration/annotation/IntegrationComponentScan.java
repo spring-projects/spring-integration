7679d0d76ff9b3d3fea29c985029620169d2174a
Artem Bilan <abilan@pivotal.io>
2016-10-28 13:09:36 -0400

    INT-3502: filters for `@IntegrationComponentScan`
    JIRA: https://jira.spring.io/browse/INT-3502
    
    For some use-cases it is really useful to filter `@MessagingGateway` components as any other `@ComponentScan` capable.
    
    * Add `useDefaultFilters()`, `includeFilters()` and `excludeFilters()` to the `@IntegrationComponentScan`
    * Modify `GatewayInterfaceTests` to reflect changes and be sure that logic is applied properly

--- spring-integration-core/src/main/java/org/springframework/integration/annotation/IntegrationComponentScan.java
+++ spring-integration-core/src/main/java/org/springframework/integration/annotation/IntegrationComponentScan.java
@@ -23,6 +23,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.integration.config.IntegrationComponentScanRegistrar;
@@ -53,7 +54,7 @@ public @interface IntegrationComponentScan {
 	 * @return the array of 'basePackages'.
 	 */
 	@AliasFor("basePackages")
-	String[] value() default {};
+	String[] value() default { };
 
 	/**
 	 * Base packages to scan for annotated components.
@@ -62,7 +63,7 @@ public @interface IntegrationComponentScan {
 	 * @return the array of 'basePackages'.
 	 */
 	@AliasFor("value")
-	String[] basePackages() default {};
+	String[] basePackages() default { };
 
 	/**
 	 * Type-safe alternative to {@link #basePackages()} for specifying the packages
@@ -71,6 +72,33 @@ public @interface IntegrationComponentScan {
 	 * that serves no purpose other than being referenced by this attribute.
 	 * @return the array of 'basePackageClasses'.
 	 */
-	Class<?>[] basePackageClasses() default {};
+	Class<?>[] basePackageClasses() default { };
+
+
+	/**
+	 * Indicates whether automatic detection of classes annotated with
+	 * {@code @MessagingGateway} should be enabled.
+	 * @since 5.0
+	 */
+	boolean useDefaultFilters() default true;
+
+	/**
+	 * Specifies which types are eligible for component scanning.
+	 * <p>Further narrows the set of candidate components from everything in {@link #basePackages}
+	 * to everything in the base packages that matches the given filter or filters.
+	 * <p>Note that these filters will be applied in addition to the default filters, if specified.
+	 * Any type under the specified base packages which matches a given filter will be included,
+	 * even if it does not match the default filters (i.e. is not annotated with {@code @Component}).
+	 * @since 5.0
+	 * @see #excludeFilters()
+	 */
+	Filter[] includeFilters() default { };
+
+	/**
+	 * Specifies which types are not eligible for component scanning.
+	 * @since 5.0
+	 * @see #includeFilters()
+	 */
+	Filter[] excludeFilters() default { };
 
 }
