0fa8849f7f44f70ac84a36153351a7a45ccc6f72
Xavier Padro <xpadro.sw@gmail.com>
2016-12-09 00:02:45 +0100

    INT-3335: Implement MongoDb Outbound Gateway
    JIRA: https://jira.spring.io/browse/INT-3335
    
    INT-3335: PR fixes
    
    INT-3335: fix code style
    
    INT-3335: Add queryExpressionString + minor fixes
    
    * Polishing. Mostly code style

---
+++ spring-integration-mongodb/src/main/java/org/springframework/integration/mongodb/outbound/MongoDbOutboundGateway.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.mongodb.outbound;
+
+import org.bson.Document;
+
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.core.query.BasicQuery;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.TypeLocator;
+import org.springframework.expression.spel.support.StandardTypeLocator;
+import org.springframework.integration.expression.ExpressionUtils;
+import org.springframework.integration.handler.AbstractReplyProducingMessageHandler;
+import org.springframework.messaging.Message;
+import org.springframework.util.Assert;
+
+/**
+ * Makes outbound operations to query a MongoDb database using a {@link MongoOperations}
+ *
+ * @author Xavier Padró
+ * @since 5.0
+ */
+public class MongoDbOutboundGateway extends AbstractReplyProducingMessageHandler {
+
+	private MongoDbFactory mongoDbFactory;
+
+	private MongoConverter mongoConverter;
+
+	private MongoOperations mongoTemplate;
+
+	private EvaluationContext evaluationContext;
+
+	private Expression queryExpression;
+
+	private boolean expectSingleResult = false;
+
+	private Class<?> entityClass = Document.class;
+
+	private Expression collectionNameExpression;
+
+	public MongoDbOutboundGateway(MongoDbFactory mongoDbFactory) {
+		this(mongoDbFactory, new MappingMongoConverter(new DefaultDbRefResolver(mongoDbFactory),
+				new MongoMappingContext()));
+	}
+
+	public MongoDbOutboundGateway(MongoDbFactory mongoDbFactory, MongoConverter mongoConverter) {
+		Assert.notNull(mongoDbFactory, "mongoDbFactory must not be null.");
+		Assert.notNull(mongoConverter, "mongoConverter must not be null.");
+		this.mongoDbFactory = mongoDbFactory;
+		this.mongoConverter = mongoConverter;
+	}
+
+	public MongoDbOutboundGateway(MongoOperations mongoTemplate) {
+		Assert.notNull(mongoTemplate, "mongoTemplate must not be null.");
+		this.mongoTemplate = mongoTemplate;
+	}
+
+	public void setQueryExpression(Expression queryExpression) {
+		Assert.notNull(queryExpression, "queryExpression must not be null.");
+		this.queryExpression = queryExpression;
+	}
+
+	public void setQueryExpressionString(String queryExpressionString) {
+		Assert.notNull(queryExpressionString, "queryExpressionString must not be null.");
+		this.queryExpression = EXPRESSION_PARSER.parseExpression(queryExpressionString);
+	}
+
+	public void setExpectSingleResult(boolean expectSingleResult) {
+		this.expectSingleResult = expectSingleResult;
+	}
+
+	public void setEntityClass(Class<?> entityClass) {
+		Assert.notNull(entityClass, "entityClass must not be null.");
+		this.entityClass = entityClass;
+	}
+
+	public void setCollectionNameExpression(Expression collectionNameExpression) {
+		Assert.notNull(collectionNameExpression, "collectionNameExpression must not be null.");
+		this.collectionNameExpression = collectionNameExpression;
+	}
+
+	public void setCollectionNameExpressionString(String collectionNameExpressionString) {
+		Assert.notNull(collectionNameExpressionString, "collectionNameExpressionString must not be null.");
+		this.collectionNameExpression = EXPRESSION_PARSER.parseExpression(collectionNameExpressionString);
+	}
+
+	public void setMongoConverter(MongoConverter mongoConverter) {
+		Assert.notNull(mongoConverter, "mongoConverter cannot be null");
+		Assert.isNull(this.mongoTemplate,
+				"'mongoConverter' can not be set when instance was constructed with MongoTemplate");
+		this.mongoConverter = mongoConverter;
+	}
+
+	@Override
+	protected void doInit() {
+		Assert.state(this.queryExpression != null, "no query specified");
+		Assert.state(this.collectionNameExpression != null, "no collection name specified");
+
+		if (this.evaluationContext == null) {
+			this.evaluationContext = ExpressionUtils.createStandardEvaluationContext(this.getBeanFactory());
+
+			TypeLocator typeLocator = this.evaluationContext.getTypeLocator();
+			if (typeLocator instanceof StandardTypeLocator) {
+				((StandardTypeLocator) typeLocator).registerImport(Query.class.getPackage().getName());
+			}
+		}
+
+		if (this.mongoTemplate == null) {
+			this.mongoTemplate = new MongoTemplate(this.mongoDbFactory, this.mongoConverter);
+		}
+	}
+
+	@Override
+	protected Object handleRequestMessage(Message<?> requestMessage) {
+		String collectionName =
+				this.collectionNameExpression.getValue(this.evaluationContext, requestMessage, String.class);
+		Query query = buildQuery(requestMessage);
+
+		Object result;
+
+		if (this.expectSingleResult) {
+			result = this.mongoTemplate.findOne(query, this.entityClass, collectionName);
+		}
+		else {
+			result = this.mongoTemplate.find(query, this.entityClass, collectionName);
+		}
+
+		return result;
+	}
+
+	private Query buildQuery(Message<?> requestMessage) {
+		Query query;
+
+		Object expressionValue =
+				this.queryExpression.getValue(this.evaluationContext, requestMessage, Object.class);
+
+		if (expressionValue instanceof String) {
+			query = new BasicQuery((String) expressionValue);
+		}
+		else if (expressionValue instanceof Query) {
+			query = ((Query) expressionValue);
+		}
+		else {
+			throw new IllegalStateException("'queryExpression' must evaluate to " +
+					"String or org.springframework.data.mongodb.core.query.Query");
+		}
+
+		return query;
+	}
+
+}
---
+++ spring-integration-mongodb/src/test/java/org/springframework/integration/mongodb/config/MongoDbOutboundGatewayParserTests.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.mongodb.config;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.parsing.BeanDefinitionParsingException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
+import org.springframework.expression.common.LiteralExpression;
+import org.springframework.expression.spel.standard.SpelExpression;
+import org.springframework.integration.mongodb.outbound.MongoDbOutboundGateway;
+import org.springframework.integration.test.util.TestUtils;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Xavier Padró
+ * @since 5.0
+ */
+@ContextConfiguration
+@RunWith(SpringRunner.class)
+@DirtiesContext
+public class MongoDbOutboundGatewayParserTests {
+
+	@Autowired
+	private ApplicationContext context;
+
+	@Autowired
+	private MongoDbFactory mongoDbFactory;
+
+	@Autowired
+	private MongoConverter mongoConverter;
+
+	@Test
+	public void minimalConfig() {
+		MongoDbOutboundGateway gateway =
+				TestUtils.getPropertyValue(context.getBean("minimalConfig"), "handler", MongoDbOutboundGateway.class);
+
+		assertNotNull(TestUtils.getPropertyValue(gateway, "mongoTemplate"));
+		assertSame(this.mongoDbFactory, TestUtils.getPropertyValue(gateway, "mongoDbFactory"));
+		assertNotNull(TestUtils.getPropertyValue(gateway, "evaluationContext"));
+		assertTrue(TestUtils.getPropertyValue(gateway, "collectionNameExpression") instanceof LiteralExpression);
+		assertEquals("foo", TestUtils.getPropertyValue(gateway, "collectionNameExpression.literalValue"));
+	}
+
+	@Test
+	public void fullConfigWithCollectionExpression() {
+		MongoDbOutboundGateway gateway = TestUtils.getPropertyValue(
+				context.getBean("fullConfigWithCollectionExpression"), "handler", MongoDbOutboundGateway.class);
+
+		assertNotNull(TestUtils.getPropertyValue(gateway, "mongoTemplate"));
+		assertSame(this.mongoDbFactory, TestUtils.getPropertyValue(gateway, "mongoDbFactory"));
+		assertSame(this.mongoConverter, TestUtils.getPropertyValue(gateway, "mongoConverter"));
+		assertNotNull(TestUtils.getPropertyValue(gateway, "evaluationContext"));
+		assertTrue(TestUtils.getPropertyValue(gateway, "collectionNameExpression") instanceof SpelExpression);
+		assertEquals("headers.collectionName",
+				TestUtils.getPropertyValue(gateway, "collectionNameExpression.expression"));
+	}
+
+	@Test
+	public void fullConfigWithCollection() {
+		MongoDbOutboundGateway gateway = TestUtils.getPropertyValue(
+				context.getBean("fullConfigWithCollection"), "handler", MongoDbOutboundGateway.class);
+
+		assertNotNull(TestUtils.getPropertyValue(gateway, "mongoTemplate"));
+		assertSame(this.mongoDbFactory, TestUtils.getPropertyValue(gateway, "mongoDbFactory"));
+		assertSame(this.mongoConverter, TestUtils.getPropertyValue(gateway, "mongoConverter"));
+		assertNotNull(TestUtils.getPropertyValue(gateway, "evaluationContext"));
+		assertTrue(TestUtils.getPropertyValue(gateway, "collectionNameExpression") instanceof LiteralExpression);
+		assertEquals("foo", TestUtils.getPropertyValue(gateway, "collectionNameExpression.literalValue"));
+	}
+
+	@Test
+	public void fullConfigWithMongoTemplate() {
+		MongoDbOutboundGateway gateway = TestUtils.getPropertyValue(
+				context.getBean("fullConfigWithTemplate"), "handler", MongoDbOutboundGateway.class);
+
+		assertEquals(context.getBean("mongoDbTemplate"), TestUtils.getPropertyValue(gateway, "mongoTemplate"));
+		assertNull(TestUtils.getPropertyValue(gateway, "mongoDbFactory"));
+		assertNull(TestUtils.getPropertyValue(gateway, "mongoConverter"));
+		assertNotNull(TestUtils.getPropertyValue(gateway, "evaluationContext"));
+		assertTrue(TestUtils.getPropertyValue(gateway, "collectionNameExpression") instanceof LiteralExpression);
+		assertEquals("foo", TestUtils.getPropertyValue(gateway, "collectionNameExpression.literalValue"));
+	}
+
+	@Test(expected = BeanDefinitionParsingException.class)
+	public void templateAndFactoryFail() {
+		new ClassPathXmlApplicationContext("outbound-gateway-fail-template-factory-config.xml", this.getClass())
+				.close();
+	}
+
+	@Test(expected = BeanDefinitionParsingException.class)
+	public void templateAndConverterFail() {
+		new ClassPathXmlApplicationContext("outbound-gateway-fail-template-converter-config.xml",
+				this.getClass()).close();
+	}
+
+}
