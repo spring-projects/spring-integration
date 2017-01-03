dd34f3de2aaba039a7e099b767266e1f554162f7
Artem Bilan <abilan@pivotal.io>
2017-01-03 13:06:47 -0500

    INT-4197: Fix Annotation Case when not `messages`
    JIRA: https://jira.spring.io/browse/INT-4197
    
    Since `Collection<Message<?>>` can be possible only if we are dealing with messages as a group (`MethodInvokingMessageListProcessor`), the case with the `List<?>` param for Messaging Annotation method should not be treated as candidate for `messages` collection.
    
    * Add `this.canProcessMessageList` condition to avoid `messages` SpEL expression when we are not in the `MethodInvokingMessageListProcessor` environment
    
    **Cherry-pick to 4.3.x**
    
    * Fix Array creating formatting
    * Add comment about `ReflectiveMethodExecutor` to the `testRouterWithListParam()`
    * Send one test data as as `Collection` to be sure that `@Router` parameters mapping works well

--- spring-integration-core/src/test/java/org/springframework/integration/router/config/RouterParserTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/router/config/RouterParserTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -285,7 +285,7 @@ public class RouterParserTests {
 	}
 
 
-	static class TestChannelResover implements DestinationResolver<MessageChannel> {
+	static class TestChannelResolver implements DestinationResolver<MessageChannel> {
 
 		@Override
 		public MessageChannel resolveDestination(String channelName) {
