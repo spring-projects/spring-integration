4bbb022b6cd813f2dfbead68daec217a92943041
Aaron Grant <aaron.grant@tarigma.com>
2017-01-25 20:50:56 -0500

    Add failing integration test for ChainFileListFilter

---
+++ spring-integration-file/src/test/java/org/springframework/integration/file/ChainFileListFilterIntegrationTests.java
@@ -0,0 +1,72 @@
+package org.springframework.integration.file;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.integration.file.filters.ChainFileListFilter;
+import org.springframework.integration.file.filters.LastModifiedFileListFilter;
+import org.springframework.integration.file.filters.SimplePatternFileListFilter;
+
+/**
+ * @author Aaron Grant
+ */
+public class ChainFileListFilterIntegrationTests {
+
+	private class MockOldFile extends File {
+		private static final long serialVersionUID = 1L;
+
+		public MockOldFile(String pathname) {
+			super(pathname);
+		}
+
+		@Override
+		public long lastModified() {
+			return 1;
+		}
+	}
+
+	private File[] files = new File[] { new MockOldFile("file.txt") };
+
+	@Test
+	public void singlePatternFilter() throws IOException {
+		try (ChainFileListFilter<File> chain = new ChainFileListFilter<>()) {
+			chain.addFilter(new SimplePatternFileListFilter("*.txt"));
+			List<File> result = chain.filterFiles(files);
+			assertEquals(1, result.size());
+		}
+	}
+
+	@Test
+	public void singleModifiedFilter() throws IOException {
+		try (ChainFileListFilter<File> chain = new ChainFileListFilter<>()) {
+			chain.addFilter(new LastModifiedFileListFilter());
+			List<File> result = chain.filterFiles(files);
+			assertEquals(1, result.size());
+		}
+	}
+
+	@Test
+	public void patternThenModifiedFilters() throws IOException {
+		try (ChainFileListFilter<File> chain = new ChainFileListFilter<>()) {
+			chain.addFilter(new SimplePatternFileListFilter("*.txt"));
+			chain.addFilter(new LastModifiedFileListFilter());
+			List<File> result = chain.filterFiles(files);
+			assertEquals(1, result.size());
+		}
+	}
+
+	@Test
+	public void modifiedThenPatternFilters() throws IOException {
+		try (ChainFileListFilter<File> chain = new ChainFileListFilter<>()) {
+			chain.addFilter(new LastModifiedFileListFilter());
+			chain.addFilter(new SimplePatternFileListFilter("*.txt"));
+			List<File> result = chain.filterFiles(files);
+			assertEquals(1, result.size());
+		}
+	}
+
+}
