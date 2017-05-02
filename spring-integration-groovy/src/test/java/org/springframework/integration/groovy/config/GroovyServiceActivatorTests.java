f166bd1bd7c14a626d11d0d6fe7051dd77340aa9
Artem Bilan <abilan@pivotal.io>
2017-05-02 17:31:20 -0400

    Groovy test: Fix format for `MM` instead of `mm`
    https://build.spring.io/browse/INT-AT42SIO-498/
    
    **Cherry-pick to 4.3.x and 4.2.x**
    
    (cherry picked from commit 9de2856)
    
    Conflicts:
    	spring-integration-groovy/src/test/java/org/springframework/integration/groovy/config/GroovyServiceActivatorTests.java

--- spring-integration-groovy/src/test/java/org/springframework/integration/groovy/config/GroovyServiceActivatorTests.java
+++ spring-integration-groovy/src/test/java/org/springframework/integration/groovy/config/GroovyServiceActivatorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -148,7 +148,7 @@ public class GroovyServiceActivatorTests {
 			this.inlineScriptInput.send(message);
 		}
 
-		DateFormat format = new SimpleDateFormat("dd.mm.yyyy");
+		DateFormat format = new SimpleDateFormat("dd.MM.yyyy");
 
 		String now = format.format(new Date());
 
@@ -168,7 +168,7 @@ public class GroovyServiceActivatorTests {
 			this.scriptWithoutVariablesInput.send(message);
 		}
 
-		DateFormat format = new SimpleDateFormat("dd.mm.yyyy");
+		DateFormat format = new SimpleDateFormat("dd.MM.yyyy");
 
 		String now = format.format(new Date());
 
