2b4c343eaac7cfb7b838dd7d3563d3eb9e940779
Gary Russell <grussell@pivotal.io>
2017-05-04 17:55:06 -0400

    INT-4270: CGLib Compatibility
    JIRA: https://jira.spring.io/browse/INT-4270
    
    Initial Commit
    
    Remove `final` modifier from certain critical framework methods that prevent CGLib proxies working.
    
    Since Spring Boot 2.0 now uses proxyTargetClass=true by default (and has done that for
    transactional proxies since 1.4), we must relax these restrictions so that CGLib can
    proxy channels (which is often done to make subflows run in a transaction).
    
    When CGLib can't override a `final` method, the fields used within those methods are uninitialized.
    In the case of `AbstractMessageChannel` this causes an NPE on the unitiallized `dataTypes` field.
    With `MessageHandler`, fields like `shouldTrack` are always false.
    
    Further, methods on `IntegrationObjectSupport` - such as `getComponentName()` return null.
    
    I have left setters and `afterPropertiesSet` (on IOS) `final` since these will typically be called
    before the object is proxied.
    
    I suspect there will be other methods we need to open up, but perhaps we should only do them on-demand.
    
    __cherry-pick to 4.3.x__

---
+++ spring-integration-core/src/test/java/org/springframework/integration/handler/CGLibProxyHandlerTests.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.handler;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.config.EnableMessageHistory;
+import org.springframework.integration.history.MessageHistory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Gary Russell
+ * @since 4.3.10
+ *
+ */
+@RunWith(SpringRunner.class)
+public class CGLibProxyHandlerTests {
+
+	@Autowired
+	private BridgeHandler bridge;
+
+	@Autowired
+	private QueueChannel out;
+
+	@Test
+	public void testProxyBridge() {
+		assertTrue(AopUtils.isCglibProxy(this.bridge));
+		this.bridge.handleMessage(new GenericMessage<>("foo"));
+		Message<?> received = this.out.receive(0);
+		assertNotNull(received);
+		assertNotNull(received.getHeaders().get(MessageHistory.HEADER_NAME));
+		MessageHistory history = received.getHeaders().get(MessageHistory.HEADER_NAME, MessageHistory.class);
+		assertThat(history.size(), equalTo(2));
+	}
+
+	@Configuration
+	@EnableMessageHistory
+	public static class Config {
+
+		@Bean
+		public BridgeHandler bridgeTarget() { // separate bean so setters and aPS are called
+			BridgeHandler bridgeHandler = new BridgeHandler();
+			bridgeHandler.setOutputChannel(out());
+			bridgeHandler.setShouldTrack(true);
+			return bridgeHandler;
+		}
+
+		@Bean
+		public ProxyFactoryBean bridge() {
+			return createProxyFactory(bridgeTarget());
+		}
+
+		@Bean
+		public QueueChannel out() {
+			return new QueueChannel();
+		}
+
+		private ProxyFactoryBean createProxyFactory(MessageHandler handler) {
+			ProxyFactoryBean fb = new ProxyFactoryBean();
+			fb.setProxyTargetClass(true);
+			fb.setTarget(handler);
+			return fb;
+		}
+
+	}
+
+}
