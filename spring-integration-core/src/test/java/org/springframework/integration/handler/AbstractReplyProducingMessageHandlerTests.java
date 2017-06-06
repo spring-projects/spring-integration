87c061fda8edf481998aa4701ebf77457bd1e163
Marius Bogoevici <mbogoevici@pivotal.io>
2017-06-06 14:17:53 -0400

    INT-4288: Additional header blacklisting options for AbstractMessageProducingHandler
    - Add `addNotPropagatedHeaders` so that new
      headers can be added without overriding existing ones.
    - Add `getNotPropagatedHeaders` so that existing
      excluded headers can be consulted.

--- spring-integration-core/src/test/java/org/springframework/integration/handler/AbstractReplyProducingMessageHandlerTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/handler/AbstractReplyProducingMessageHandlerTests.java
@@ -16,15 +16,6 @@
 
 package org.springframework.integration.handler;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willReturn;
-
 import java.util.Collections;
 
 import org.junit.Test;
@@ -38,6 +29,17 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.support.GenericMessage;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.emptyCollectionOf;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willReturn;
 
 /**
  * @author Iwein Fuld
@@ -86,9 +88,41 @@ public class AbstractReplyProducingMessageHandlerTests {
 			}
 
 		};
+		assertThat(handler.getNotPropagatedHeaders(), emptyCollectionOf(String.class));
 		handler.setNotPropagatedHeaders("foo", "bar");
 		handler.setOutputChannel(this.channel);
+		assertThat(handler.getNotPropagatedHeaders(), containsInAnyOrder("foo", "bar"));
 		ArgumentCaptor<Message<?>> captor = ArgumentCaptor.forClass(Message.class);
+		willReturn(true).given(this.channel).send(captor.capture());
+		handler.handleMessage(MessageBuilder.withPayload("hello")
+				.setHeader("foo", "FOO")
+				.setHeader("bar", "BAR")
+				.setHeader("baz", "BAZ")
+				.build());
+		Message<?> out = captor.getValue();
+		assertThat(out, notNullValue());
+		assertThat(out.getHeaders().get("foo"), nullValue());
+		assertThat(out.getHeaders().get("bar"), equalTo("RAB"));
+		assertThat(out.getHeaders().get("baz"), equalTo("BAZ"));
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testNotPropagateAdd() {
+		AbstractReplyProducingMessageHandler handler = new AbstractReplyProducingMessageHandler() {
+
+			@Override
+			protected Object handleRequestMessage(Message<?> requestMessage) {
+				return new GenericMessage<String>("world", Collections.singletonMap("bar", "RAB"));
+			}
+
+		};
+		assertThat(handler.getNotPropagatedHeaders(), emptyCollectionOf(String.class));
+		handler.setNotPropagatedHeaders("foo");
+		handler.addNotPropagatedHeaders("bar");
+		handler.setOutputChannel(this.channel);
+		assertThat(handler.getNotPropagatedHeaders(), containsInAnyOrder("foo", "bar"));
+		ArgumentCaptor<Message<?>> captor = (ArgumentCaptor<Message<?>>) (ArgumentCaptor<?>) ArgumentCaptor.forClass(Message.class);
 		willReturn(true).given(this.channel).send(captor.capture());
 		handler.handleMessage(MessageBuilder.withPayload("hello")
 				.setHeader("foo", "FOO")
