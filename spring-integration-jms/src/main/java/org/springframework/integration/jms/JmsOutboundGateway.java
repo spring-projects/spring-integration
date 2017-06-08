spring-projects/spring-integration

commit 45ae13809157f7d83e6a7f7516876d71b663e6cb

Fix JMS and JMX module to the latest SF
* Fix `ControlBusParserTests` test to pass `Object.class` to the
`MessageConverter` since we can't use `null` there any more
* Fix `JmsOutboundGateway` for empty string for the
`replyDestinationName` since we can't pass `null` there any more

spring-integration-jms/src/main/java/org/springframework/integration/jms/JmsOutboundGateway.java
@@ -600,6 +600,10 @@ public class JmsOutboundGateway extends AbstractReplyProducingMessageHandler imp
 		if (StringUtils.hasText(this.replyDestinationName)) {
 			container.setDestinationName(this.replyDestinationName);
 		}
+		else {
+			// to be resolved to the TemporaryQueue
+			container.setDestinationName("");
+		}
 		if (this.destinationResolver != null) {
 			container.setDestinationResolver(this.destinationResolver);
 		}
@@ -1399,7 +1403,7 @@ public class JmsOutboundGateway extends AbstractReplyProducingMessageHandler imp
 		@Override
 		protected String getDestinationDescription() {
 			if (this.replyDestination instanceof TemporaryQueue) {
-				return "Temporary queue:" + this.replyDestination.toString();
+				return "Temporary queue: " + this.replyDestination.toString();
 			}
 			else if (super.getDestination() != null) {
 				try {

spring-integration-jmx/src/test/java/org/springframework/integration/jmx/config/ControlBusParserTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,16 +27,16 @@ import org.springframework.integration.core.MessagingTemplate;
 import org.springframework.jmx.export.MBeanExporter;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author Mark Fisher
  * @author Gary Russell
+ * @author Artem Bilan
+ *
  * @since 2.0
  */
-@ContextConfiguration
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @DirtiesContext
 public class ControlBusParserTests {
 
@@ -48,7 +48,7 @@ public class ControlBusParserTests {
 		MessageChannel control = this.context.getBean("controlChannel", MessageChannel.class);
 		MessagingTemplate messagingTemplate = new MessagingTemplate();
 		Object value = messagingTemplate.convertSendAndReceive(control,
-				"@integrationMbeanExporter.getChannelSendRate('testChannel').count", null);
+				"@integrationMbeanExporter.getChannelSendRate('testChannel').count", Object.class);
 		assertEquals(0, value);
 		MBeanExporter exporter = this.context.getBean(MBeanExporter.class);
 		exporter.destroy();

commit c7aa508434b49f9d320c6b137915083373f0e988

Fix JMS Outbound Gateway
45ae13809157f7d83e6a7f7516876d71b663e6cb caused the reply destination to overridden to "".

spring-integration-jms/src/main/java/org/springframework/integration/jms/JmsOutboundGateway.java
@@ -597,7 +597,7 @@ public class JmsOutboundGateway extends AbstractReplyProducingMessageHandler imp
 		if (this.replyDestination != null) {
 			container.setDestination(this.replyDestination);
 		}
-		if (StringUtils.hasText(this.replyDestinationName)) {
+		else if (StringUtils.hasText(this.replyDestinationName)) {
 			container.setDestinationName(this.replyDestinationName);
 		}
 		else {
@@ -1163,15 +1163,11 @@ public class JmsOutboundGateway extends AbstractReplyProducingMessageHandler imp
 		}
 		if (logger.isDebugEnabled()) {
 			if (reply == null) {
-				if (logger.isDebugEnabled()) {
-					logger.debug(this.getComponentName() + " Timed out waiting for reply with CorrelationId "
-							+ correlationId);
-				}
+				logger.debug(this.getComponentName() + " Timed out waiting for reply with CorrelationId "
+						+ correlationId);
 			}
 			else {
-				if (logger.isDebugEnabled()) {
-					logger.debug(this.getComponentName() + " Obtained reply with CorrelationId " + correlationId);
-				}
+				logger.debug(this.getComponentName() + " Obtained reply with CorrelationId " + correlationId);
 			}
 		}
 		return reply;
@@ -1372,7 +1368,7 @@ public class JmsOutboundGateway extends AbstractReplyProducingMessageHandler imp
 		}
 
 		public Destination getReplyDestination() {
-			Destination replyDest = this.getDestination();
+			Destination replyDest = getDestination();
 			if (replyDest == null) {
 				replyDest = this.replyDestination;
 			}

spring-integration-jms/src/test/java/org/springframework/integration/jms/request_reply/producer-cached-consumers.xml
@@ -52,6 +52,7 @@
 
 	<int-jms:outbound-gateway request-channel="jmsInOptimizedC"
 		connection-factory="jmsConnectionFactory"
+		receive-timeout="10000"
 		request-destination="siOutQueueOptimizedC"
 		reply-destination="siInQueueOptimizedC"
 		correlation-key="JMSCorrelationID">
@@ -72,6 +73,7 @@
 
 	<int-jms:outbound-gateway request-channel="jmsInNonOptimizedD"
 		connection-factory="jmsConnectionFactory"
+		receive-timeout="10000"
 		request-destination="siOutQueueNonOptimizedD"
 		reply-destination="siInQueueNonOptimizedD">
 		<int-jms:reply-listener />