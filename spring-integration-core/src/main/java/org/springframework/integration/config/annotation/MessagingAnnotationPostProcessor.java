a5adf92e4f1a3909da72e18ccf8f72a3c36e19fb
Rick Hogge <rhogge@paypal.com>
2017-08-10 23:18:48 -0500

    INT-4327: Performance optimization for SI Bean Post Processors

--- spring-integration-core/src/main/java/org/springframework/integration/config/annotation/MessagingAnnotationPostProcessor.java
+++ spring-integration-core/src/main/java/org/springframework/integration/config/annotation/MessagingAnnotationPostProcessor.java
@@ -26,6 +26,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -71,6 +72,7 @@ import org.springframework.util.StringUtils;
  * @author Marius Bogoevici
  * @author Artem Bilan
  * @author Gary Russell
+ * @author Rick Hogge
  */
 public class MessagingAnnotationPostProcessor implements BeanPostProcessor, BeanFactoryAware,
 		InitializingBean, SmartInitializingSingleton {
@@ -84,6 +86,8 @@ public class MessagingAnnotationPostProcessor implements BeanPostProcessor, Bean
 
 	private ConfigurableListableBeanFactory beanFactory;
 
+	private final Set<String> noAnnotationsCache =
+			Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>(256));
 
 	@Override
 	public void setBeanFactory(BeanFactory beanFactory) {
@@ -151,6 +155,11 @@ public class MessagingAnnotationPostProcessor implements BeanPostProcessor, Bean
 
 		Class<?> beanClass = AopUtils.getTargetClass(bean);
 
+		// the set will hold records of prior class scans and indicate if no messaging annotations were found
+		if(this.noAnnotationsCache.contains(beanClass.getName())) {
+			return bean;
+		}
+
 		ReflectionUtils.doWithMethods(beanClass, method -> {
 			Map<Class<? extends Annotation>, List<Annotation>> annotationChains = new HashMap<>();
 			for (Class<? extends Annotation> annotationType :
@@ -168,6 +177,8 @@ public class MessagingAnnotationPostProcessor implements BeanPostProcessor, Bean
 				List<Annotation> annotations = entry.getValue();
 				processAnnotationTypeOnMethod(bean, beanName, method, annotationType, annotations);
 			}
+			if(annotationChains.size() == 0)
+				noAnnotationsCache.add(beanClass.getName());
 		}, ReflectionUtils.USER_DECLARED_METHODS);
 
 		return bean;
