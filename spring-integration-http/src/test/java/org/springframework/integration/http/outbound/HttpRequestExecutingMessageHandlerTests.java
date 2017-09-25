bea5706e67fc0ea665f0796bc86551a2ad948774
Gary Russell <grussell@pivotal.io>
2017-09-25 10:35:08 -0400

    Fix HTTP For Latest Spring Framework
    https://build.spring.io/browse/INT-FATS5IC-277/
    
    Fix mock for request.getRawStatusCode()
    
    https://github.com/spring-projects/spring-framework/commit/4cbef27f90ded887af9ae9639550999aa81f3847
    
    Also excluded transitive SF dependencies from spring-security.

--- spring-integration-http/src/test/java/org/springframework/integration/http/outbound/HttpRequestExecutingMessageHandlerTests.java
+++ spring-integration-http/src/test/java/org/springframework/integration/http/outbound/HttpRequestExecutingMessageHandlerTests.java
@@ -848,7 +848,7 @@ public class HttpRequestExecutingMessageHandlerTests {
 		when(requestFactory.createRequest(any(URI.class), any(HttpMethod.class))).thenReturn(clientRequest);
 
 		ClientHttpResponse response = mock(ClientHttpResponse.class);
-		when(response.getStatusCode()).thenReturn(HttpStatus.NOT_FOUND);
+		when(response.getRawStatusCode()).thenReturn(HttpStatus.NOT_FOUND.value());
 		when(response.getStatusText()).thenReturn("Not Found");
 		when(response.getBody()).thenReturn(new ByteArrayInputStream(new byte[0]));
 
