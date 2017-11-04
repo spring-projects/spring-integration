50674bbd97dee016d9dd41a591aca1e354bf6db2
Meherzad <meherzad.lahewala@gmail.com>
2017-11-04 20:01:49 +0530

    INT-4291 Fix checkstyle trailing space

--- spring-integration-jdbc/src/test/java/org/springframework/integration/jdbc/ExpressionEvaluatingSqlParameterSourceFactoryTests.java
+++ spring-integration-jdbc/src/test/java/org/springframework/integration/jdbc/ExpressionEvaluatingSqlParameterSourceFactoryTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ import org.springframework.jdbc.support.JdbcUtils;
  *
  */
 public class ExpressionEvaluatingSqlParameterSourceFactoryTests {
-	
+
 	private final ExpressionEvaluatingSqlParameterSourceFactory factory = new ExpressionEvaluatingSqlParameterSourceFactory();
 
 	@Test
