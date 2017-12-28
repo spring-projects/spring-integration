fb9d34ead7ba4714fc0215511e71cff7fdf496e9
krizsan <ivan.krizsan@gmail.com>
2017-12-28 10:44:57 +0100

    INT-4373: Fix channel error count
    JIRA: https://jira.spring.io/browse/INT-4373
    
    Error count is wrong when just counts enabled.
    
    CheckStyle fix.
    
    Reduce send timeout in test.

--- spring-integration-core/src/main/java/org/springframework/integration/support/management/DefaultMessageChannelMetrics.java
+++ spring-integration-core/src/main/java/org/springframework/integration/support/management/DefaultMessageChannelMetrics.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2015 the original author or authors.
+ * Copyright 2009-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -21,6 +21,7 @@ import java.util.concurrent.atomic.AtomicLong;
  * @author Dave Syer
  * @author Helena Edelson
  * @author Gary Russell
+ * @author Ivan Krizsan
  * @since 2.0
  */
 public class DefaultMessageChannelMetrics extends AbstractMessageChannelMetrics {
@@ -106,10 +107,12 @@ public class DefaultMessageChannelMetrics extends AbstractMessageChannelMetrics 
 
 	@Override
 	public void afterSend(MetricsContext context, boolean result) {
-		if (result && isFullStatsEnabled()) {
-			long now = System.nanoTime();
-			this.sendSuccessRatio.success(now);
-			this.sendDuration.append(now - ((DefaultChannelMetricsContext) context).start);
+		if (result) {
+			if (isFullStatsEnabled()) {
+				long now = System.nanoTime();
+				this.sendSuccessRatio.success(now);
+				this.sendDuration.append(now - ((DefaultChannelMetricsContext) context).start);
+			}
 		}
 		else {
 			if (isFullStatsEnabled()) {
