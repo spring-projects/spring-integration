d890f14c5d2cb88fdb1129ceb233b4ebb722a58d
Gary Russell <grussell@pivotal.io>
2018-01-18 15:20:47 -0500

    More JMS Test speed improvements
    - reduce timeouts for tests that expect to time out
    - reduce iterations

--- spring-integration-jms/src/test/java/org/springframework/integration/jms/request_reply/RequestReplyScenariosWithCachedConsumersTests.java
+++ spring-integration-jms/src/test/java/org/springframework/integration/jms/request_reply/RequestReplyScenariosWithCachedConsumersTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -166,8 +166,7 @@ public class RequestReplyScenariosWithCachedConsumersTests extends ActiveMQMulti
 					return message;
 				});
 			}).start();
-			org.springframework.messaging.Message<?> siReplyMessage = gateway.exchange(new GenericMessage<String>("foo"));
-			assertEquals("bar", siReplyMessage.getPayload());
+			gateway.exchange(new GenericMessage<String>("foo"));
 		}
 		finally {
 			context.close();
