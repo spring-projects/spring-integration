1788fbf97a36956c24afe725e2a65adb75aa904a
Artem Bilan <abilan@pivotal.io>
2018-01-18 11:31:08 -0500

    GH-2301: Add SimpMessageGrFactory.GroupType.LIST
    Fixes https://github.com/spring-projects/spring-integration/issues/2301
    
    * For non-ordered, without deduplication logic use-case the `ArrayList`
    option for internal `SimpleMessageGroup` collection is added
    * Make `SimpleMessageGroup` `protected` ctor as `public` for better
    reuse in case of custom `MessageGroupFactory`

--- spring-integration-core/src/test/java/org/springframework/integration/aggregator/AggregatorTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/aggregator/AggregatorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -102,7 +102,7 @@ public class AggregatorTests {
 		SimpleMessageStore store = new SimpleMessageStore();
 
 		SimpleMessageGroupFactory messageGroupFactory =
-				new SimpleMessageGroupFactory(SimpleMessageGroupFactory.GroupType.BLOCKING_QUEUE);
+				new SimpleMessageGroupFactory(SimpleMessageGroupFactory.GroupType.LIST);
 
 		store.setMessageGroupFactory(messageGroupFactory);
 
