04a61f1dd509e9c51b95b37daf40e920ad91f036
Artem Bilan <abilan@pivotal.io>
2018-03-27 12:23:05 -0400

    Fix `Transformers` usage in Docs
    **Cherry-pick to 5.0.x**
    
    (cherry picked from commit 8afdcb4)

--- spring-integration-core/src/main/java/org/springframework/integration/dsl/Transformers.java
+++ spring-integration-core/src/main/java/org/springframework/integration/dsl/Transformers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017 the original author or authors.
+ * Copyright 2016-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -76,7 +76,7 @@ public abstract class Transformers {
 	}
 
 	public static ObjectToMapTransformer toMap(JsonObjectMapper<?, ?> jsonObjectMapper, boolean shouldFlattenKeys) {
-		ObjectToMapTransformer transformer = new ObjectToMapTransformer();
+		ObjectToMapTransformer transformer = new ObjectToMapTransformer(jsonObjectMapper);
 		transformer.setShouldFlattenKeys(shouldFlattenKeys);
 		return transformer;
 	}
