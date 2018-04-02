477c071af9afaa555df9b717efa560fdf7d9acb9
Artem Bilan <abilan@pivotal.io>
2018-04-02 13:13:42 -0400

    Fix NPE in the TcpNetServerConnectionFactory
    https://build.spring.io/browse/INTSAMPLES-NIGHTLY-2413
    
    When we fail with the
    `java.net.BindException: Address already in use (Bind failed)` in the
    `TcpNetServerConnectionFactory.run()`, the `serverSocket` property
    remains `null` and we get `NPE` in the `catch` block trying to `close()`
    the socket.
    
    * Call `stop()` instead which has all the required protections.
    
    **Cherry-pick to 5.0.x and 4.3.x**

--- spring-integration-ip/src/main/java/org/springframework/integration/ip/tcp/connection/TcpNetServerConnectionFactory.java
+++ spring-integration-ip/src/main/java/org/springframework/integration/ip/tcp/connection/TcpNetServerConnectionFactory.java
@@ -33,6 +33,8 @@ import org.springframework.util.Assert;
  * a {@link ServerSocket}. Must have a {@link TcpListener} registered.
  *
  * @author Gary Russell
+ * @author Artem Bilan
+ *
  * @since 2.0
  *
  */
@@ -155,6 +157,7 @@ public class TcpNetServerConnectionFactory extends AbstractServerConnectionFacto
 			else if (isActive()) {
 				logger.error("Error on ServerSocket; port = " + getPort(), e);
 				publishServerExceptionEvent(e);
+				stop();
 			}
 		}
 		finally {
@@ -191,7 +194,8 @@ public class TcpNetServerConnectionFactory extends AbstractServerConnectionFacto
 		try {
 			this.serverSocket.close();
 		}
-		catch (IOException e) { }
+		catch (IOException e) {
+		}
 		this.serverSocket = null;
 		super.stop();
 	}
