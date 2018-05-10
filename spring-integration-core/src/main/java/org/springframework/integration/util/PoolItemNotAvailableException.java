c2895f67cc54397dd0a469992d193ac6ed6cf05e
Gary Russell <grussell@pivotal.io>
2018-05-10 14:43:42 -0400

    INT-4422: No MessagingException in SimplePool
    JIRA: https://jira.spring.io/browse/INT-4422
    
    Inappropriate exception type, there is no message in the context of the pool.
    
    Add a new exception.

---
+++ spring-integration-core/src/main/java/org/springframework/integration/util/PoolItemNotAvailableException.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.util;
+
+/**
+ * Thrown when a pooled item could not be obtained for some reason.
+ *
+ * @author Gary Russell
+ * @since 5.1
+ *
+ */
+@SuppressWarnings("serial")
+public class PoolItemNotAvailableException extends RuntimeException {
+
+	public PoolItemNotAvailableException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public PoolItemNotAvailableException(String message) {
+		super(message);
+	}
+
+}
