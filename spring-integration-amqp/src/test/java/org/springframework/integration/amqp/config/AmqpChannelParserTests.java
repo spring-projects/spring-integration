6f109116d226a1d981ec272e90d5f26bbfc9e4d4
Artem Bilan <abilan@pivotal.io>
2018-06-13 13:13:44 -0400

    Upgrade to SA-2.0.4
    * Fix deprecation in the `AmqpChannelParserTests`

--- spring-integration-amqp/src/test/java/org/springframework/integration/amqp/config/AmqpChannelParserTests.java
+++ spring-integration-amqp/src/test/java/org/springframework/integration/amqp/config/AmqpChannelParserTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ import org.springframework.integration.amqp.channel.PublishSubscribeAmqpChannel;
 import org.springframework.integration.support.utils.IntegrationUtils;
 import org.springframework.integration.test.util.TestUtils;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.ChannelInterceptorAdapter;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -50,6 +50,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 /**
  * @author Mark Fisher
  * @author Gary Russell
+ * @author Artem Bilan
+ *
  * @since 2.1
  */
 @ContextConfiguration
@@ -119,7 +121,8 @@ public class AmqpChannelParserTests {
 		assertTrue(TestUtils.getPropertyValue(channel, "extractPayload", Boolean.class));
 	}
 
-	private static class TestInterceptor extends ChannelInterceptorAdapter {
+	private static class TestInterceptor implements ChannelInterceptor {
+
 	}
 
 }
