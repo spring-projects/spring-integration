b96046663c6a5b0e5a24fd699895fe856e43dc8c
Sundara Balaji J K <sundarabalajijk@gmail.com>
2018-07-05 13:06:44 +0530

    indentation issue fixed

--- spring-integration-jdbc/src/main/java/org/springframework/integration/jdbc/store/channel/SqlChannelMessageStoreQueryProvider.java
+++ spring-integration-jdbc/src/main/java/org/springframework/integration/jdbc/store/channel/SqlChannelMessageStoreQueryProvider.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
  
- package org.springframework.integration.jdbc.store.channel;
+package org.springframework.integration.jdbc.store.channel;
 
 import org.springframework.integration.jdbc.store.channel.AbstractChannelMessageStoreQueryProvider;
 
