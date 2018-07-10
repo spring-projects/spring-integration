9a39731f51961c31e3cf13c186cc74a0a48307bc
Sundara Balaji J K <sundarabalajijk@gmail.com>
2018-07-10 08:30:29 +0530

    fixed pull request comments

--- spring-integration-jdbc/src/main/java/org/springframework/integration/jdbc/store/channel/SqlChannelMessageStoreQueryProvider.java
+++ spring-integration-jdbc/src/main/java/org/springframework/integration/jdbc/store/channel/SqlChannelMessageStoreQueryProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,10 +17,11 @@
 package org.springframework.integration.jdbc.store.channel;
 
 /**
+ * Channel message store query provider for Microsoft SQL Server / Azure SQL database.
  * @author Sundara Balaji
- * @since 2.2
+ * @since 5.1
  */
-public class SqlChannelMessageStoreQueryProvider extends AbstractChannelMessageStoreQueryProvider {
+public class SqlServerChannelMessageStoreQueryProvider extends AbstractChannelMessageStoreQueryProvider {
 
 	@Override
 	public String getPollFromGroupExcludeIdsQuery() {
