bebde260db912c4a7065657ae1325150ddd3d047
Gary Russell <grussell@pivotal.io>
2018-07-25 10:45:50 -0400

    INT-4506: Splitter: close stream and Closeables
    JIRA: https://jira.spring.io/browse/INT-4506
    
    Capture the actual splitter result in the `FunctionIterator and close it if necessary.
    
    **cherry-pick to 5.0.x**
    
    Add Closeable to proxy in test; remove AutoCloseable check.
    
    Closeable extends AutoCloseable.
    
    Fix javadocs; test for AutoCloseable instead of Closeable
    
    Polishing - PR comments; tighten up API.
    
    A `CloseableIterator` is now an `AutoCloseable` with an overridden
    `close()`` (no exception) to avoid lint problem.
    
    * Polishing omissions, code style and `@author`
    
    **Cherry-pick to 5.0.x**

--- spring-integration-xml/src/main/java/org/springframework/integration/xml/splitter/XPathMessageSplitter.java
+++ spring-integration-xml/src/main/java/org/springframework/integration/xml/splitter/XPathMessageSplitter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -329,7 +329,7 @@ public class XPathMessageSplitter extends AbstractMessageSplitter {
 
 		TransformFunctionIterator(Iterator<Node> delegate,
 				Function<? super Node, ? extends String> function) {
-			super(delegate, function);
+			super(null, delegate, function);
 			this.delegate = delegate;
 		}
 
