e7e1127717be3d7e7e125ff6777ab36dbd54c8e7
Artem Bilan <abilan@pivotal.io>
2018-08-02 14:48:47 -0400

    INT-3364: Add batchUpdate into JdbcMessageHandler
    JIRA: https://jira.spring.io/browse/INT-3364

--- spring-integration-jdbc/src/main/java/org/springframework/integration/jdbc/JdbcMessageHandler.java
+++ spring-integration-jdbc/src/main/java/org/springframework/integration/jdbc/JdbcMessageHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,22 +18,27 @@ package org.springframework.integration.jdbc;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import javax.sql.DataSource;
 
 import org.springframework.integration.handler.AbstractMessageHandler;
+import org.springframework.integration.support.MutableMessage;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.ColumnMapRowMapper;
 import org.springframework.jdbc.core.JdbcOperations;
-import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapperResultSetExtractor;
-import org.springframework.jdbc.core.namedparam.EmptySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -47,41 +52,52 @@ import org.springframework.util.LinkedCaseInsensitiveMap;
 /**
  * A message handler that executes an SQL update. Dynamic query parameters are supported through the
  * {@link SqlParameterSourceFactory} abstraction, the default implementation of which wraps the message so that its bean
- * properties can be referred to by name in the query string E.g.
+ * properties can be referred to by name in the query string, e.g.
  *
  * <pre class="code">
  * INSERT INTO FOOS (MESSAGE_ID, PAYLOAD) VALUES (:headers[id], :payload)
  * </pre>
+ *
+ * <p>
+ * When message payload is an instance of {@link Iterable}, a
+ * {@link NamedParameterJdbcOperations#batchUpdate(String, SqlParameterSource[])} is performed, where each
+ * {@link SqlParameterSource} instance is based on each item wrapped into the {@link MutableMessage} and headers
+ * copied from the request message.
+ * <p>
+ * When a {@link #preparedStatementSetter} is configured, it is applied for each item in the appropriate
+ * {@link JdbcOperations#batchUpdate(String, BatchPreparedStatementSetter)} function.
+ * <p>
+ * NOTE: The batch update is not supported when {@link #keysGenerated} is in use.
  *
  * N.B. do not use quotes to escape the header keys. The default SQL parameter source (from Spring JDBC) can also handle
  * headers with dotted names (e.g. <code>business.id</code>)
  *
  * @author Dave Syer
  * @author Artem Bilan
+ *
  * @since 2.0
  */
 public class JdbcMessageHandler extends AbstractMessageHandler {
 
 	private final ResultSetExtractor<List<Map<String, Object>>> generatedKeysResultSetExtractor =
-			new RowMapperResultSetExtractor<Map<String, Object>>(new ColumnMapRowMapper(), 1);
+			new RowMapperResultSetExtractor<>(new ColumnMapRowMapper(), 1);
 
 	private final NamedParameterJdbcOperations jdbcOperations;
 
-	private final PreparedStatementCreator generatedKeysStatementCreator = con ->
-			con.prepareStatement(JdbcMessageHandler.this.updateSql, Statement.RETURN_GENERATED_KEYS);
+	private final PreparedStatementCreator generatedKeysStatementCreator =
+			con -> con.prepareStatement(JdbcMessageHandler.this.updateSql, Statement.RETURN_GENERATED_KEYS);
 
-	private volatile String updateSql;
+	private String updateSql;
 
-	private volatile SqlParameterSourceFactory sqlParameterSourceFactory;
+	private SqlParameterSourceFactory sqlParameterSourceFactory;
 
-	private volatile boolean keysGenerated;
+	private boolean keysGenerated;
 
 	private MessagePreparedStatementSetter preparedStatementSetter;
 
 	/**
 	 * Constructor taking {@link DataSource} from which the DB Connection can be obtained and the select query to
 	 * execute to retrieve new rows.
-	 *
 	 * @param dataSource Must not be null
 	 * @param updateSql query to execute
 	 */
@@ -93,13 +109,12 @@ public class JdbcMessageHandler extends AbstractMessageHandler {
 	/**
 	 * Constructor taking {@link JdbcOperations} instance to use for query execution and the select query to execute to
 	 * retrieve new rows.
-	 *
 	 * @param jdbcOperations instance to use for query execution
 	 * @param updateSql query to execute
 	 */
 	public JdbcMessageHandler(JdbcOperations jdbcOperations, String updateSql) {
 		this.jdbcOperations = new NamedParameterJdbcTemplate(jdbcOperations);
-		this.updateSql = updateSql;
+		setUpdateSql(updateSql);
 	}
 
 	/**
@@ -111,7 +126,12 @@ public class JdbcMessageHandler extends AbstractMessageHandler {
 		this.keysGenerated = keysGenerated;
 	}
 
-	public void setUpdateSql(String updateSql) {
+	/**
+	 * Configure an SQL statement to perform an UPDATE on the target database.
+	 * @param updateSql the SQL statement to perform.
+	 */
+	public final void setUpdateSql(String updateSql) {
+		Assert.hasText(updateSql, "'updateSql' must not be empty.");
 		this.updateSql = updateSql;
 	}
 
@@ -149,7 +169,7 @@ public class JdbcMessageHandler extends AbstractMessageHandler {
 	 * Executes the update, passing the message into the {@link SqlParameterSourceFactory}.
 	 */
 	@Override
-	protected void handleMessageInternal(Message<?> message) throws Exception {
+	protected void handleMessageInternal(Message<?> message) {
 		List<? extends Map<String, Object>> keys = executeUpdateQuery(message, this.keysGenerated);
 		if (!keys.isEmpty() && logger.isDebugEnabled()) {
 			logger.debug("Generated keys: " + keys);
@@ -157,49 +177,91 @@ public class JdbcMessageHandler extends AbstractMessageHandler {
 	}
 
 	protected List<? extends Map<String, Object>> executeUpdateQuery(final Message<?> message, boolean keysGenerated) {
-		SqlParameterSource updateParameterSource = EmptySqlParameterSource.INSTANCE;
-		if (this.preparedStatementSetter == null) {
-			if (this.sqlParameterSourceFactory != null) {
-				updateParameterSource = this.sqlParameterSourceFactory.createParameterSource(message);
-			}
-		}
 		if (keysGenerated) {
 			if (this.preparedStatementSetter != null) {
-				return this.jdbcOperations.getJdbcOperations().execute(this.generatedKeysStatementCreator,
-						(PreparedStatementCallback<List<Map<String, Object>>>) ps -> {
-							JdbcMessageHandler.this.preparedStatementSetter.setValues(ps, message);
-							ps.executeUpdate();
-							ResultSet keys = ps.getGeneratedKeys(); // NOSONAR closed in JdbcUtils
-							if (keys != null) {
-								try {
+				return this.jdbcOperations.getJdbcOperations()
+						.execute(this.generatedKeysStatementCreator,
+								ps -> {
+									this.preparedStatementSetter.setValues(ps, message);
+									ps.executeUpdate();
+									ResultSet keys = ps.getGeneratedKeys(); // NOSONAR closed in JdbcUtils
+									if (keys != null) {
+										try {
 
-									return JdbcMessageHandler.this.generatedKeysResultSetExtractor.extractData(keys);
-								}
-								finally {
-									JdbcUtils.closeResultSet(keys);
-								}
-							}
-							return new LinkedList<Map<String, Object>>();
-						});
+											return this.generatedKeysResultSetExtractor.extractData(keys);
+										}
+										finally {
+											JdbcUtils.closeResultSet(keys);
+										}
+									}
+									return new LinkedList<>();
+								});
 			}
 			else {
 				KeyHolder keyHolder = new GeneratedKeyHolder();
-				this.jdbcOperations.update(this.updateSql, updateParameterSource, keyHolder);
+				this.jdbcOperations.update(this.updateSql,
+						this.sqlParameterSourceFactory.createParameterSource(message), keyHolder);
 				return keyHolder.getKeyList();
 			}
 		}
 		else {
-			int updated;
-			if (this.preparedStatementSetter != null) {
-				updated = this.jdbcOperations.getJdbcOperations().update(this.updateSql,
-						ps -> JdbcMessageHandler.this.preparedStatementSetter.setValues(ps, message));
+			if (message.getPayload() instanceof Iterable) {
+				Stream<? extends MutableMessage<?>> messageStream =
+						StreamSupport.stream(((Iterable<?>) message.getPayload()).spliterator(), false)
+								.map(payload -> new MutableMessage<>(payload, message.getHeaders()));
+
+				int[] updates;
+
+				if (this.preparedStatementSetter != null) {
+					Message<?>[] messages = messageStream.toArray(Message<?>[]::new);
+
+					updates = this.jdbcOperations.getJdbcOperations()
+							.batchUpdate(this.updateSql, new BatchPreparedStatementSetter() {
+
+								@Override
+								public void setValues(PreparedStatement ps, int i) throws SQLException {
+									JdbcMessageHandler.this.preparedStatementSetter.setValues(ps, messages[i]);
+								}
+
+								@Override
+								public int getBatchSize() {
+									return messages.length;
+								}
+
+							});
+				}
+				else {
+					SqlParameterSource[] sqlParameterSources =
+							messageStream.map(this.sqlParameterSourceFactory::createParameterSource)
+									.toArray(SqlParameterSource[]::new);
+
+					updates = this.jdbcOperations.batchUpdate(this.updateSql, sqlParameterSources);
+				}
+
+				return Arrays.stream(updates)
+						.mapToObj(updated -> {
+							Map<String, Object> map = new LinkedCaseInsensitiveMap<>();
+							map.put("UPDATED", updated);
+							return map;
+						})
+						.collect(Collectors.toList());
 			}
 			else {
-				updated = this.jdbcOperations.update(this.updateSql, updateParameterSource);
+				int updated;
+
+				if (this.preparedStatementSetter != null) {
+					updated = this.jdbcOperations.getJdbcOperations()
+							.update(this.updateSql, ps -> this.preparedStatementSetter.setValues(ps, message));
+				}
+				else {
+					updated = this.jdbcOperations.update(this.updateSql,
+							this.sqlParameterSourceFactory.createParameterSource(message));
+				}
+
+				LinkedCaseInsensitiveMap<Object> map = new LinkedCaseInsensitiveMap<>();
+				map.put("UPDATED", updated);
+				return Collections.singletonList(map);
 			}
-			LinkedCaseInsensitiveMap<Object> map = new LinkedCaseInsensitiveMap<Object>();
-			map.put("UPDATED", updated);
-			return Collections.singletonList(map);
 		}
 	}
 
