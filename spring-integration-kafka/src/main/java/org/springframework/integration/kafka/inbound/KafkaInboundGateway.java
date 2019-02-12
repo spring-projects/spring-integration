4ab5a3b51dcea00ccd78b5a80a6ef5defa85af28
Gary Russell <grussell@pivotal.io>
2019-02-12 17:53:02 -0500

    Remove unnecessary throws Exception clause

--- spring-integration-kafka/src/main/java/org/springframework/integration/kafka/inbound/KafkaInboundGateway.java
+++ spring-integration-kafka/src/main/java/org/springframework/integration/kafka/inbound/KafkaInboundGateway.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2018-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -156,7 +156,7 @@ public class KafkaInboundGateway<K, V, R> extends MessagingGatewaySupport implem
 	}
 
 	@Override
-	protected void onInit() throws Exception {
+	protected void onInit() {
 		super.onInit();
 		MessageListener<K, V> listener = this.listener;
 		if (this.retryTemplate != null) {
