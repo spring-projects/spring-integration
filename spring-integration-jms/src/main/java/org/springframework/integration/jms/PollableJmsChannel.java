00df16674888d695fcb6f518b912bb298f2b624c
Artem Bilan <abilan@pivotal.io>
2019-02-28 13:02:26 -0500

    Fix meter leaks in the Pollable channels

--- spring-integration-jms/src/main/java/org/springframework/integration/jms/PollableJmsChannel.java
+++ spring-integration-jms/src/main/java/org/springframework/integration/jms/PollableJmsChannel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -150,14 +150,17 @@ public class PollableJmsChannel extends AbstractJmsChannel
 		catch (RuntimeException e) {
 			if (countsEnabled && !counted) {
 				if (getMetricsCaptor() != null) {
-					getMetricsCaptor().counterBuilder(RECEIVE_COUNTER_NAME)
-							.tag("name", getComponentName() == null ? "unknown" : getComponentName())
-							.tag("type", "channel")
-							.tag("result", "failure")
-							.tag("exception", e.getClass().getSimpleName())
-							.description("Messages received")
-							.build()
-							.increment();
+					CounterFacade counterFacade =
+							getMetricsCaptor()
+									.counterBuilder(RECEIVE_COUNTER_NAME)
+									.tag("name", getComponentName() == null ? "unknown" : getComponentName())
+									.tag("type", "channel")
+									.tag("result", "failure")
+									.tag("exception", e.getClass().getSimpleName())
+									.description("Messages received")
+									.build();
+					this.meters.add(counterFacade);
+					counterFacade.increment();
 				}
 				getMetrics().afterError();
 			}
@@ -177,6 +180,7 @@ public class PollableJmsChannel extends AbstractJmsChannel
 					.tag("exception", "none")
 					.description("Messages received")
 					.build();
+			this.meters.add(this.receiveCounter);
 		}
 		this.receiveCounter.increment();
 	}
