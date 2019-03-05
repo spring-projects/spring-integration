047790ec389ff72b03e75e46bcc3455940e241de
Gary Russell <grussell@pivotal.io>
2019-03-05 13:06:18 -0500

    Fix race in TcpOutboundGatewayTests

--- spring-integration-ip/src/test/java/org/springframework/integration/ip/tcp/TcpOutboundGatewayTests.java
+++ spring-integration-ip/src/test/java/org/springframework/integration/ip/tcp/TcpOutboundGatewayTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -377,11 +377,13 @@ public class TcpOutboundGatewayTests extends LogAdjustingTestSupport {
 								oos.writeObject(request.replace("Test", "Reply"));
 								logger.debug("Replied to " + request);
 								lastReceived.set(request);
-								serverLatch.countDown();
 							}
 							catch (IOException e) {
 								logger.debug("error on write " + e.getClass().getSimpleName());
 								socket.close();
+							}
+							finally {
+								serverLatch.countDown();
 							}
 						}
 					}
