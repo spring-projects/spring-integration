d21e8f03499c93939b0a618ce96a026eef940ca2
Artem Bilan <abilan@pivotal.io>
2019-03-21 19:22:26 -0400

    URL Cleanup
    This commit updates URLs to prefer the https protocol.
    Redirects are not followed to avoid accidentally expanding intentionally
    shortened URLs (i.e. if using a URL shortener).
    
    # Fixed URLs
    
    ## Fixed Success
    These URLs were switched to an https URL with a 2xx status.
    While the status was successful, your review is still recommended.
    
    * [ ] http://www.apache.org/licenses/ with 1 occurrences migrated to:
      https://www.apache.org/licenses/ ([https](https://www.apache.org/licenses/) result 200).
    * [ ] http://www.apache.org/licenses/LICENSE-2.0 with 2638 occurrences migrated to:
      https://www.apache.org/licenses/LICENSE-2.0 ([https](https://www.apache.org/licenses/LICENSE-2.0) result 200).

--- spring-integration-file/src/test/java/org/springframework/integration/file/remote/RemoteFileTestSupport.java
+++ spring-integration-file/src/test/java/org/springframework/integration/file/remote/RemoteFileTestSupport.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2016-2017 the original author or authors.
+ * Copyright 2016-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -89,17 +89,17 @@ public abstract class RemoteFileTestSupport {
 	 * <pre class="code">
 	 *  $ tree remoteSource/
 	 *  remoteSource/
-	 *  ├── remoteSource1.txt - contains 'source1'
-	 *  ├── remoteSource2.txt - contains 'source2'
-	 *  ├── subRemoteSource
-	 *      ├── subRemoteSource1.txt - contains 'subSource1'
+	 *  ??? remoteSource1.txt - contains 'source1'
+	 *  ??? remoteSource2.txt - contains 'source2'
+	 *  ??? subRemoteSource
+	 *      ??? subRemoteSource1.txt - contains 'subSource1'
 	 *  remoteTarget/
 	 *  $ tree localSource/
 	 *  localSource/
-	 *  ├── localSource1.txt - contains 'local1'
-	 *  ├── localSource2.txt - contains 'local2'
-	 *  ├── subLocalSource
-	 *      ├── subLocalSource1.txt - contains 'subLocal1'
+	 *  ??? localSource1.txt - contains 'local1'
+	 *  ??? localSource2.txt - contains 'local2'
+	 *  ??? subLocalSource
+	 *      ??? subLocalSource1.txt - contains 'subLocal1'
 	 *  localTarget/
 	 * </pre>
 	 *
