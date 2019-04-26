4199ff57cd36a593873a90d92fabffc2eae1f953
Artem Bilan <abilan@pivotal.io>
2019-04-26 09:14:42 -0400

    Make `RSocketOutboundGateway` for server side
    * Introduce a `ClientRSocketConnector` to represent a common logic for
    client side connection and management an obtained `RSocket`
    * Rework the `RSocketOutboundGateway` to perform similar RSocket request
    logic on the server side as well.
    * Refactor `RSocketOutboundGatewayIntegrationTests` to demonstrate that
    RSocket requests work the same way from the server side as well.
    For this reason a `CommonConfig` has been extracted with the same
    `RSocketOutboundGateway` configuration reuse on both server and client
    sides.

---
+++ spring-integration-rsocket/src/main/java/org/springframework/integration/rsocket/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides classes for RSocket XML namespace parsing and configuration support.
+ */
+package org.springframework.integration.rsocket.config;
---
+++ spring-integration-rsocket/src/main/java/org/springframework/integration/rsocket/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides common classes for RSocket components.
+ */
+package org.springframework.integration.rsocket;
