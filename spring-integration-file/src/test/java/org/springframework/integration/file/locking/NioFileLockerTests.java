f2bd8c4119f466e6d3f55728d2a50a7077ee1e6b
Gary Russell <grussell@pivotal.io>
2019-07-22 15:10:18 -0400

    Clear lock cache in NioFileLocker (#2998)
    Fixes https://github.com/spring-projects/spring-integration/issues/2980
    # Conflicts:
    #	spring-integration-file/src/test/java/org/springframework/integration/file/locking/NioFileLockerTests.java
    
    # Conflicts:
    #	spring-integration-file/src/main/java/org/springframework/integration/file/locking/NioFileLocker.java
    #	spring-integration-file/src/test/java/org/springframework/integration/file/locking/NioFileLockerTests.java

--- spring-integration-file/src/test/java/org/springframework/integration/file/locking/NioFileLockerTests.java
+++ spring-integration-file/src/test/java/org/springframework/integration/file/locking/NioFileLockerTests.java
@@ -18,20 +18,24 @@ package org.springframework.integration.file.locking;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import org.springframework.integration.file.filters.FileListFilter;
+import org.springframework.integration.test.util.TestUtils;
 
 /**
  * @author Iwein Fuld
+ * @author Gary Russell
  */
 public class NioFileLockerTests {
 
@@ -46,25 +50,31 @@ public class NioFileLockerTests {
 		}
 	};
 
-    @Test
-    public void fileListedByFirstFilter() throws IOException {
-        NioFileLocker filter = new NioFileLocker();
-        File testFile = new File(workdir, "test0");
-        testFile.createNewFile();
-        assertThat(filter.filterFiles(workdir.listFiles()).get(0), is(testFile));
-        filter.lock(testFile);
-        assertThat(filter.filterFiles(workdir.listFiles()).get(0), is(testFile));
-    }
+	@Test
+	public void fileListedByFirstFilter() throws Exception {
+		NioFileLocker filter = new NioFileLocker();
+		File testFile = new File(workdir, "test0");
+		testFile.createNewFile();
+		assertThat(filter.filterFiles(workdir.listFiles()).get(0), is(testFile));
+		filter.lock(testFile);
+		assertThat(filter.filterFiles(workdir.listFiles()).get(0), is(testFile));
+		filter.unlock(testFile);
+		Field channelCache = FileChannelCache.class.getDeclaredField("channelCache");
+		channelCache.setAccessible(true);
+		assertTrue(((Map<?, ?>) channelCache.get(null)).isEmpty());
+		assertTrue(TestUtils.getPropertyValue(filter, "lockCache", Map.class).isEmpty());
+	}
 
-    @Test
-    public void fileNotListedWhenLockedByOtherFilter() throws IOException {
-        NioFileLocker filter1 = new NioFileLocker();
-        FileListFilter<File> filter2 = new NioFileLocker();
-        File testFile = new File(workdir, "test1");
-        testFile.createNewFile();
-        assertThat(filter1.filterFiles(workdir.listFiles()).get(0), is(testFile));
-        filter1.lock(testFile);
-        assertThat(filter2.filterFiles(workdir.listFiles()), is((List<File>) new ArrayList<File>()));
-    }
+	@Test
+	public void fileNotListedWhenLockedByOtherFilter() throws IOException {
+		NioFileLocker filter1 = new NioFileLocker();
+		FileListFilter<File> filter2 = new NioFileLocker();
+		File testFile = new File(workdir, "test1");
+		testFile.createNewFile();
+		assertThat(filter1.filterFiles(workdir.listFiles()).get(0), is(testFile));
+		filter1.lock(testFile);
+		assertThat(filter2.filterFiles(workdir.listFiles()), is(new ArrayList<File>()));
+		filter1.unlock(testFile);
+	}
 
 }
