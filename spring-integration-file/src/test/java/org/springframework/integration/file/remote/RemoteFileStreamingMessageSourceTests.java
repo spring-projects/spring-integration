aec7fba4c5715c13acfd3b3a8b3e9dcc41c79346
lsgeme <lukas.gemela@gfk.com>
2019-11-08 12:25:30 +0100

    fixes GH-3105: Spring integration stream SFTP: pattern filter not working
    https://github.com/spring-projects/spring-integration/issues/3105
    
    fixed validation issue

--- spring-integration-file/src/test/java/org/springframework/integration/file/remote/RemoteFileStreamingMessageSourceTests.java
+++ spring-integration-file/src/test/java/org/springframework/integration/file/remote/RemoteFileStreamingMessageSourceTests.java
@@ -1,16 +1,33 @@
+/*
+ * Copyright 2015-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.integration.file.remote;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.integration.file.filters.FileListFilter;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import org.junit.Test;
+
+import org.springframework.integration.file.filters.FileListFilter;
 
 /**
  * @author Lukas Gemela
@@ -20,7 +37,7 @@ import static org.mockito.Mockito.when;
 class RemoteFileStreamingMessageSourceTests {
 
 	@Test
-	void filterOutFilesNotAcceptedByFilter() {
+	public void filterOutFilesNotAcceptedByFilter() {
 		@SuppressWarnings("unchecked")
 		RemoteFileTemplate<String> remoteFileTemplate = mock(RemoteFileTemplate.class);
 		when(remoteFileTemplate.list("remoteDirectory")).thenReturn(new String[]{"file1", "file2"});
