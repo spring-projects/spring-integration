61f91e59ecbcc2f85bd3c40956ddddc93d00d49c
Artem Bilan <abilan@pivotal.io>
2020-02-14 17:57:32 -0500

    Upgrade to latest releases
    * Upgrade to Gradle 6.1.1
    * Fix for latest Checkstyle
    * Fix for latest SI-Kotlin-DSL

--- spring-integration-kafka/src/main/java/org/springframework/integration/kafka/outbound/KafkaProducerMessageHandler.java
+++ spring-integration-kafka/src/main/java/org/springframework/integration/kafka/outbound/KafkaProducerMessageHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2019 the original author or authors.
+ * Copyright 2013-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -522,7 +522,7 @@ public class KafkaProducerMessageHandler<K, V> extends AbstractReplyProducingMes
 	}
 
 	private void determineValidReplyTopicsAndPartitions() {
-		ReplyingKafkaTemplate<?, ?, ?> rkt = (ReplyingKafkaTemplate<?, ?, ?>) kafkaTemplate;
+		ReplyingKafkaTemplate<?, ?, ?> rkt = (ReplyingKafkaTemplate<?, ?, ?>) this.kafkaTemplate;
 		Collection<TopicPartition> replyTopics = rkt.getAssignedReplyTopicPartitions();
 		Map<String, Set<Integer>> topicsAndPartitions = new HashMap<>();
 		if (replyTopics != null) {
