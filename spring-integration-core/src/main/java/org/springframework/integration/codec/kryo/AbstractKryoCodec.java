fcb8cafd1e358fe49a81578db84307b068a9912a
Artem Bilan <abilan@vmware.com>
2020-07-22 12:07:05 -0400

    Improve Kryo Codec for registrations
    # Conflicts:
    #	spring-integration-core/src/test/java/org/springframework/integration/codec/kryo/KryoCodecTests.java

--- spring-integration-core/src/main/java/org/springframework/integration/codec/kryo/AbstractKryoCodec.java
+++ spring-integration-core/src/main/java/org/springframework/integration/codec/kryo/AbstractKryoCodec.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2019 the original author or authors.
+ * Copyright 2015-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,6 +44,7 @@ public abstract class AbstractKryoCodec implements Codec {
 	protected AbstractKryoCodec() {
 		KryoFactory factory = () -> {
 			Kryo kryo = new Kryo();
+			kryo.setRegistrationRequired(true);
 			// configure Kryo instance, customize settings
 			configureKryoInstance(kryo);
 			return kryo;
