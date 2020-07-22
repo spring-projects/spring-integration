b8b45128218ad1b6b460f5f6add5db3812dd6367
Artem Bilan <abilan@vmware.com>
2020-07-22 13:36:17 -0400

    Improve MessageKryoRegistrar for registrations
    (cherry picked from commit 5ec71d4b4ac973f963a931db63f37c3a529f8222)

--- spring-integration-core/src/main/java/org/springframework/integration/codec/kryo/MessageCodec.java
+++ spring-integration-core/src/main/java/org/springframework/integration/codec/kryo/MessageCodec.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2019 the original author or authors.
+ * Copyright 2015-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,9 @@ package org.springframework.integration.codec.kryo;
 
 /**
  * {@link PojoCodec} configured to encode/decode {@code Message<?>}s.
+ *
  * @author Gary Russell
+ *
  * @since 4.2
  *
  */
