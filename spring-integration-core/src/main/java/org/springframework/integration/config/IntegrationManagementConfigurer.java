94ef2716cf47e8255e5ce90dd0c641032fa868b7
Artem Bilan <abilan@vmware.com>
2020-07-28 16:59:35 -0400

    Add gauges for queue channel size (#3349)
    * Add gauges for queue channel size
    
    The `QueueChannel` provides a current size and remaining capacity metrics
    
    * Add Micrometer gauges into `QueueChannel` to expose the current values
    of the size and remaining capacity
    
    **Cherry-pick to 5.3.x, 5.2.x & 5.1.x**
    
    * * Revert `@SuppressWarnings("unchecked")` for test
    * Document new gauges for queue channel
    
    * * Fix IntegrationManagementConfigurer for NPE on `metricsCaptor`
    
    * Fix wording in meter descriptions
    
    Co-authored-by: Michel Jung <michel.jung89@gmail.com>
    
    Co-authored-by: Michel Jung <michel.jung89@gmail.com>

--- spring-integration-core/src/main/java/org/springframework/integration/config/IntegrationManagementConfigurer.java
+++ spring-integration-core/src/main/java/org/springframework/integration/config/IntegrationManagementConfigurer.java
@@ -288,8 +288,8 @@ public class IntegrationManagementConfigurer
 	}
 
 	private void injectCaptor() {
-		Map<String, IntegrationManagement> managed = this.applicationContext
-				.getBeansOfType(IntegrationManagement.class);
+		Map<String, IntegrationManagement> managed =
+				this.applicationContext.getBeansOfType(IntegrationManagement.class);
 		for (Entry<String, IntegrationManagement> entry : managed.entrySet()) {
 			IntegrationManagement bean = entry.getValue();
 			if (!bean.getOverrides().loggingConfigured) {
@@ -302,7 +302,7 @@ public class IntegrationManagementConfigurer
 	@Override
 	public Object postProcessAfterInitialization(Object bean, String name) throws BeansException {
 		if (this.singletonsInstantiated) {
-			if (bean instanceof IntegrationManagement) {
+			if (this.metricsCaptor != null && bean instanceof IntegrationManagement) {
 				((IntegrationManagement) bean).registerMetricsCaptor(this.metricsCaptor);
 			}
 			return doConfigureMetrics(bean, name);
