e8e6483fe0ebe97ca652302f30cd2bbaf91a809c
Artem Bilan <abilan@vmware.com>
2020-09-10 11:00:07 -0400

    GH-3370: Remove synchronized from RemoteFileUtils
    Fixes https://github.com/spring-projects/spring-integration/issues/3370
    
    The `synchronized` on the `RemoteFileUtils.makeDirectories()` makes an application too
     slow, especially when we deal with different paths in different sessions
    
    * Remove the `synchronized` from that method and rework `SftpSession.mkdir()`
    to return `false` when "A file cannot be created if it already exists" exception
    is thrown from the server.
    Essentially make an `exists()` call to be sure that an exception is really related
    to "file-already-exists" answer from the server
    
    **Cherry-pick to 5.3.x, 5.2.x & 4.3.x**

--- spring-integration-file/src/main/java/org/springframework/integration/file/remote/RemoteFileUtils.java
+++ spring-integration-file/src/main/java/org/springframework/integration/file/remote/RemoteFileUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -46,7 +46,7 @@ public final class RemoteFileUtils {
 	 * @param logger The logger.
 	 * @throws IOException Any IOException.
 	 */
-	public static synchronized <F> void makeDirectories(String path, Session<F> session, String remoteFileSeparator,
+	public static <F> void makeDirectories(String path, Session<F> session, String remoteFileSeparator,
 			Log logger) throws IOException {
 
 		if (!session.exists(path)) {
