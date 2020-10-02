cbe37e8942db394d5edd1e60b350f2a3c6e2c10b
Gary Russell <grussell@pivotal.io>
2020-10-02 14:31:53 -0400

    GH-3395: Fix XML expression default for ARFOGateway
    Resolves https://github.com/spring-projects/spring-integration/issues/3395
    
    Outbound remote file gateway parser requires `expression` even though some
    commands don't need or use it.
    
    * Propagate the empty string for `expression` attribute
    * Fix default (`payload`) expression logic in the `AbstractRemoteFileOutboundGateway`
    
    **Cherry-pick to `5.3.x`**

--- spring-integration-file/src/main/java/org/springframework/integration/file/config/AbstractRemoteFileOutboundGatewayParser.java
+++ spring-integration-file/src/main/java/org/springframework/integration/file/config/AbstractRemoteFileOutboundGatewayParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,9 +58,7 @@ public abstract class AbstractRemoteFileOutboundGatewayParser extends AbstractCo
 		}
 		else {
 			builder.addConstructorArgValue(element.getAttribute("command"));
-			if (element.hasAttribute(EXPRESSION_ATTRIBUTE)) {
-				builder.addConstructorArgValue(element.getAttribute(EXPRESSION_ATTRIBUTE));
-			}
+			builder.addConstructorArgValue(element.getAttribute(EXPRESSION_ATTRIBUTE));
 		}
 		IntegrationNamespaceUtils.setValueIfAttributeDefined(builder, element, "command-options", "options");
 		IntegrationNamespaceUtils.setValueIfAttributeDefined(builder, element, "reply-timeout", "sendTimeout");
