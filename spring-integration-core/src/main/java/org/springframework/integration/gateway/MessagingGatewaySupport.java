3b60e456791bca6be74b2e093c2168c156fab1a4
Artem Bilan <abilan@vmware.com>
2020-10-22 11:41:34 -0400

    Gateway: Propagate Error to the errorChannel
    See SO for more info:
    https://stackoverflow.com/questions/64456946/handle-exceptions-errors-other-than-messagingexception-ie-other-error-excepti
    
    In the versions before `5.2.x` the `Error` was wrapped to the `MessagingException`
    in the `AbstractRequestHandlerAdvice` and this one was handled properly
    on the gateway level with its `errorChannel` configured
    
    * Fix `MessagingGatewaySupport` to catch all the `Throwable` and try to send them as `ErrorMessage`
    to the `errorChannel`.
    Otherwise unwrap returned `MessagingException` and re-throw an `Error` as is to keep
    the current behavior for non-handled exceptions
    
    * Do not wrap `Error` into a `MessagingException` and re-throw as is if there is no `errorChannel`
    
    **Cherry-pick to 5.3.x & 5.2.x**

--- spring-integration-core/src/main/java/org/springframework/integration/gateway/MessagingGatewaySupport.java
+++ spring-integration-core/src/main/java/org/springframework/integration/gateway/MessagingGatewaySupport.java
@@ -526,7 +526,7 @@ public abstract class MessagingGatewaySupport extends AbstractEndpoint
 				throwMessageTimeoutException(object, "No reply received within timeout");
 			}
 		}
-		catch (Exception ex) {
+		catch (Throwable ex) { // NOSONAR (catch throwable)
 			if (logger.isDebugEnabled()) {
 				logger.debug("failure occurred in gateway sendAndReceive: " + ex.getMessage());
 			}
@@ -560,6 +560,9 @@ public abstract class MessagingGatewaySupport extends AbstractEndpoint
 						? errorFlowReply.getPayload()
 						: errorFlowReply;
 			}
+		}
+		else if (error instanceof Error) {
+			throw (Error) error;
 		}
 		else {
 			Throwable errorToReThrow = error;
