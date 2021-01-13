c3eda50297364ebc85ed9a3bd9843d1ceb62ae45
Gary Russell <grussell@vmware.com>
2021-01-13 14:59:54 -0500

    Upgrade to spring-amqp 2.3.4
    - remove uses of deprecated method
    - correctly call delegate in correlation data wrapper
    
    **Cherry-pick to `5.4.x`**
    # Conflicts:
    #	build.gradle

--- spring-integration-amqp/src/test/java/org/springframework/integration/amqp/outbound/AmqpOutboundEndpointTests2.java
+++ spring-integration-amqp/src/test/java/org/springframework/integration/amqp/outbound/AmqpOutboundEndpointTests2.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -86,7 +86,7 @@ public class AmqpOutboundEndpointTests2 {
 				.setHeader(AmqpHeaders.PUBLISH_CONFIRM_CORRELATION, corr)
 				.build());
 		assertThat(corr.getFuture().get(10, TimeUnit.SECONDS).isAck()).isTrue();
-		assertThat(corr.getReturnedMessage()).isNotNull();
+		assertThat(corr.getReturned()).isNotNull();
 	}
 
 	@Test
