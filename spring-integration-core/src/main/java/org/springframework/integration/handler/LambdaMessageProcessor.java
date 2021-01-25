31863f06e30e0f70ac06b2377ba10581895ae0a4
Artem Bilan <abilan@vmware.com>
2021-01-25 16:34:51 -0500

    GH-3474: LambdaMPP: rethrow runtime ex as is
    Fixes https://github.com/spring-projects/spring-integration/issues/3474
    
    The `AbstractTransformer` implementation just wraps a thrown exception into
    a `MessageTransformationException`.
    It is expected with any method transformer impl (POJO, Lambda direct etc.)
    the exception is thrown as is without extra wrapping into an `IllegalStateException`
    for consistency
    
    * Fix `LambdaMessageProcessor` to rethrow `RuntimeException` as is
    and only wrap into an `IllegalStateException` for all other exceptions
    * Fix tests for missed extra stack trace

--- spring-integration-core/src/main/java/org/springframework/integration/handler/LambdaMessageProcessor.java
+++ spring-integration-core/src/main/java/org/springframework/integration/handler/LambdaMessageProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2019 the original author or authors.
+ * Copyright 2016-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -97,15 +97,19 @@ public class LambdaMessageProcessor implements MessageProcessor<Object>, BeanFac
 			return this.method.invoke(this.target, args);
 		}
 		catch (InvocationTargetException e) {
+			final Throwable cause = e.getCause();
 			if (e.getTargetException() instanceof ClassCastException) {
 				LOGGER.error("Could not invoke the method '" + this.method + "' due to a class cast exception, " +
 						"if using a lambda in the DSL, consider using an overloaded EIP method " +
 						"that takes a Class<?> argument to explicitly  specify the type. " +
 						"An example of when this often occurs is if the lambda is configured to " +
-						"receive a Message<?> argument.", e.getCause());
+						"receive a Message<?> argument.", cause);
+			}
+			if (cause instanceof RuntimeException) { // NOSONAR
+				throw (RuntimeException) cause;
 			}
 			throw new IllegalStateException(// NOSONAR lost stack trace
-					"Could not invoke the method '" + this.method + "'", e.getCause());
+					"Could not invoke the method '" + this.method + "'", cause);
 		}
 		catch (Exception e) {
 			throw new IllegalStateException(
--- spring-integration-core/src/test/java/org/springframework/integration/dsl/routers/RouterTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/dsl/routers/RouterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2020 the original author or authors.
+ * Copyright 2016-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -913,7 +913,7 @@ public class RouterTests {
 
 		@ServiceActivator(inputChannel = "scatterGatherErrorChannel")
 		public Message<?> processAsyncScatterError(MessagingException payload) {
-			return MessageBuilder.withPayload(payload.getCause().getCause())
+			return MessageBuilder.withPayload(payload.getCause())
 					.copyHeaders(payload.getFailedMessage().getHeaders())
 					.build();
 		}
