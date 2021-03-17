ab418cc764658de4324bc523a7e3c8b6a3d020ca
Gary Russell <grussell@vmware.com>
2021-03-17 18:05:26 -0400

    GH-3509: Fix if test in previous commit
    Caused interceptor to be skipped.
    
    **cherry-pick to 5.4.x**

--- spring-integration-ip/src/test/java/org/springframework/integration/ip/tcp/connection/HelloWorldInterceptor.java
+++ spring-integration-ip/src/test/java/org/springframework/integration/ip/tcp/connection/HelloWorldInterceptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -128,6 +128,11 @@ public class HelloWorldInterceptor extends TcpConnectionInterceptorSupport {
 			this.pendingSend = false;
 			this.checkDeferredClose();
 		}
+	}
+
+	@Override
+	public String getConnectionId() {
+		return super.getConnectionId() + "-test-modified-id";
 	}
 
 	/**
