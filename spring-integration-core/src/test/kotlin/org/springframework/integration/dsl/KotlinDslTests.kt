9ba5790c28965c60c7a35ecfa6463f0e7604e01a
Artem Bilan <abilan@vmware.com>
2021-05-05 14:33:36 -0400

    GH-3558: Kotlin DSL: propagate generics info
    Fixes https://github.com/spring-projects/spring-integration/issues/3558
    
    Kotlin lambdas mostly used to configure endpoints in DSL manner
    are not really Java lambdas, but rather anonymous classes implementing
    respective Java interfaces.
    
    While in most cases such classes carry generic info for their method impls
    properly in Java, it is somehow doesn't work well for `GenericHandler`
    implemented by Kotlin lambdas
    
    * Wrap provided `GenericHandler` in the `BaseIntegrationFlowDefinition.handle()`
    into a Java lambda and call `handle()` recursively to carry an expected type to
    the `LambdaMessageProcessor`
    * Fix `LambdaMessageProcessor` to handle `ClassUtils.isKotlinUnit()` result of
    an invocation as a `null` reply
    
    **Cherry-pick to `5.4.x` & `5.3.x`**

--- spring-integration-core/src/test/kotlin/org/springframework/integration/dsl/KotlinDslTests.kt
+++ spring-integration-core/src/test/kotlin/org/springframework/integration/dsl/KotlinDslTests.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 the original author or authors.
+ * Copyright 2020-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -212,8 +212,8 @@ class KotlinDslTests {
 	fun `no reply from handle`() {
 		val payloadReference = AtomicReference<String>()
 		val integrationFlow =
-			integrationFlow("handlerInputChanenl") {
-				handle<String> { payload, _ -> payloadReference.set(payload) }
+			integrationFlow("handlerInputChannel") {
+				handle<Message<String>> { message, _ -> payloadReference.set(message.payload) }
 			}
 
 		val registration = this.integrationFlowContext.registration(integrationFlow).register()
