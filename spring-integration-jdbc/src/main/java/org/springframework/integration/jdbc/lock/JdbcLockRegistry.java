feb578c661453bb52fe63fa8c5304da49d34b55e
Fran Aranda <fran.culebras@gmail.com>
2021-05-25 20:39:16 +0200

    GH-3567: Retry for TransactionSystemException in JdbcLockRegistry
    Fixes spring-projects/spring-integration#3567

--- spring-integration-jdbc/src/main/java/org/springframework/integration/jdbc/lock/JdbcLockRegistry.java
+++ spring-integration-jdbc/src/main/java/org/springframework/integration/jdbc/lock/JdbcLockRegistry.java
@@ -32,6 +32,7 @@ import org.springframework.dao.TransientDataAccessException;
 import org.springframework.integration.support.locks.ExpirableLockRegistry;
 import org.springframework.integration.support.locks.RenewableLockRegistry;
 import org.springframework.integration.util.UUIDConverter;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.transaction.TransactionTimedOutException;
 import org.springframework.util.Assert;
 
@@ -52,6 +53,7 @@ import org.springframework.util.Assert;
  * @author Alexandre Strubel
  * @author Stefan Vassilev
  * @author Olivier Hubaut
+ * @author Fran Aranda
  *
  * @since 4.3
  */
@@ -149,7 +151,7 @@ public class JdbcLockRegistry implements ExpirableLockRegistry, RenewableLockReg
 					}
 					break;
 				}
-				catch (TransientDataAccessException | TransactionTimedOutException e) {
+				catch (TransientDataAccessException | TransactionTimedOutException | TransactionSystemException e) {
 					// try again
 				}
 				catch (InterruptedException e) {
@@ -183,7 +185,7 @@ public class JdbcLockRegistry implements ExpirableLockRegistry, RenewableLockReg
 					}
 					break;
 				}
-				catch (TransientDataAccessException | TransactionTimedOutException e) {
+				catch (TransientDataAccessException | TransactionTimedOutException | TransactionSystemException e) {
 					// try again
 				}
 				catch (InterruptedException ie) {
@@ -227,7 +229,7 @@ public class JdbcLockRegistry implements ExpirableLockRegistry, RenewableLockReg
 					}
 					return acquired;
 				}
-				catch (TransientDataAccessException | TransactionTimedOutException e) {
+				catch (TransientDataAccessException | TransactionTimedOutException | TransactionSystemException e) {
 					// try again
 				}
 				catch (Exception e) {
@@ -260,7 +262,7 @@ public class JdbcLockRegistry implements ExpirableLockRegistry, RenewableLockReg
 						this.mutex.delete(this.path);
 						return;
 					}
-					catch (TransientDataAccessException | TransactionTimedOutException e) {
+					catch (TransientDataAccessException | TransactionTimedOutException | TransactionSystemException e) {
 						// try again
 					}
 					catch (Exception e) {
@@ -294,7 +296,7 @@ public class JdbcLockRegistry implements ExpirableLockRegistry, RenewableLockReg
 					}
 					return renewed;
 				}
-				catch (TransientDataAccessException | TransactionTimedOutException e) {
+				catch (TransientDataAccessException | TransactionTimedOutException | TransactionSystemException e) {
 					// try again
 				}
 				catch (Exception e) {
