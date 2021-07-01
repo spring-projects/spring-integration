37e6457561675bc32fcfb307f9034fe23adfb811
Artem Bilan <abilan@vmware.com>
2021-07-01 15:51:50 -0400

    Remove unused imports
    **Cherry-pick to `5.4.x` & `5.3.x`**

--- spring-integration-core/src/main/java/org/springframework/integration/annotation/MessagingGateway.java
+++ spring-integration-core/src/main/java/org/springframework/integration/annotation/MessagingGateway.java
@@ -16,7 +16,6 @@
 
 package org.springframework.integration.annotation;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
