9ebe1b39133531a434c7f0841c26ccab7fc56f56
Artem Bilan <abilan@vmware.com>
2021-07-15 15:16:10 -0400

    Make MessageHistory JSON-serializable
    * Add `org.springframework.integration.history` to trusted default packaged of the `JacksonJsonUtils`
    * Add `@JsonCreator` to `MessageHistory` `private` ctor to let it to be created automatically by Jackson
    * Add `equals()` and `hashCode()` into `MessageHistory` for the proper `Message` comparison
    * Add  `MessageHistory` into headers for testing with Redis JSON (de)serialization
    
    **Cherry-pick to `5.4.x` & `5.3.x`**

--- spring-integration-core/src/main/java/org/springframework/integration/support/json/JacksonJsonUtils.java
+++ spring-integration-core/src/main/java/org/springframework/integration/support/json/JacksonJsonUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -157,7 +157,8 @@ public final class JacksonJsonUtils {
 						"org.springframework.messaging.support",
 						"org.springframework.integration.support",
 						"org.springframework.integration.message",
-						"org.springframework.integration.store"
+						"org.springframework.integration.store",
+						"org.springframework.integration.history"
 				);
 
 		private final TypeIdResolver delegate;
