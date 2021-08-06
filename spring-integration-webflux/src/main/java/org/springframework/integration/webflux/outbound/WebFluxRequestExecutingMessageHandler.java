e86fa8b409ec3ff1742d54f583d6a02925f8ed1f
David Graff <djgraff209@gmail.com>
2021-08-06 16:56:06 -0400

    Fix to address expectation of proper exception
    This fix changes the WebFluxRequestExecutingMessageHandler
    The change specifically changes the contruction of the
    WebClientResponseException to use the `create` factory method.
    
    This change addresses issue #3610.

--- spring-integration-webflux/src/main/java/org/springframework/integration/webflux/outbound/WebFluxRequestExecutingMessageHandler.java
+++ spring-integration-webflux/src/main/java/org/springframework/integration/webflux/outbound/WebFluxRequestExecutingMessageHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 the original author or authors.
+ * Copyright 2017-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -311,10 +311,7 @@ public class WebFluxRequestExecutingMessageHandler extends AbstractHttpRequestEx
 							})
 							.defaultIfEmpty(new byte[0])
 							.map(bodyBytes -> {
-										throw new WebClientResponseException(
-												"ClientResponse has erroneous status code: "
-														+ httpStatus.value() + " "
-														+ httpStatus.getReasonPhrase(),
+										throw WebClientResponseException.create(
 												httpStatus.value(),
 												httpStatus.getReasonPhrase(),
 												response.headers().asHttpHeaders(),
