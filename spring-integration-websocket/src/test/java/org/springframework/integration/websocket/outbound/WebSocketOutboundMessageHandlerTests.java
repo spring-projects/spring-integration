e7534fc49ff6a253f88ed0c585118ae46fa04993
Artem Bilan <abilan@vmware.com>
2021-11-15 13:10:25 -0500

    GH-3675: Restore nativeHeaders for WebSocketInChA
    Fixes https://github.com/spring-projects/spring-integration/issues/3675
    
    The `WebSocketInboundChannelAdapter` removes a `SimpMessageHeaderAccessor.NATIVE_HEADERS`
    from headers used to create an outbound message.
    Turns out not all native headers are mapped by the `StompHeaderAccessor`
    to the top-level header entries.
    
    **Cherry-pick to `5.4.x` & `5.3.x`**

--- spring-integration-websocket/src/test/java/org/springframework/integration/websocket/outbound/WebSocketOutboundMessageHandlerTests.java
+++ spring-integration-websocket/src/test/java/org/springframework/integration/websocket/outbound/WebSocketOutboundMessageHandlerTests.java
@@ -46,7 +46,6 @@ import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.StompSubProtocolHandler;
 import org.springframework.web.socket.messaging.SubProtocolHandler;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
-import org.springframework.web.socket.sockjs.client.Transport;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
 /**
@@ -101,7 +100,7 @@ public class WebSocketOutboundMessageHandlerTests {
 
 		@Bean
 		public WebSocketClient webSocketClient() {
-			return new SockJsClient(Collections.<Transport>singletonList(new WebSocketTransport(new StandardWebSocketClient())));
+			return new SockJsClient(Collections.singletonList(new WebSocketTransport(new StandardWebSocketClient())));
 		}
 
 		@Bean
