73ae2f1cb15b563c8119df7391801823e4667130
Daniel Frey <dmfrey@gmail.com>
2022-02-15 16:27:45 -0500

    make current with latest changes in 'spring-graphql'
    issue #3501

---
+++ spring-integration-graphql/src/main/java/org/springframework/integration/graphql/outbound/GraphQlMessageHandler.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2021-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.graphql.outbound;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.expression.Expression;
+import org.springframework.expression.common.LiteralExpression;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.graphql.GraphQlService;
+import org.springframework.graphql.RequestInput;
+import org.springframework.integration.expression.ExpressionUtils;
+import org.springframework.integration.expression.SupplierExpression;
+import org.springframework.integration.handler.AbstractReplyProducingMessageHandler;
+import org.springframework.messaging.Message;
+import org.springframework.util.Assert;
+
+/**
+ * A {@link org.springframework.messaging.MessageHandler} capable of fielding GraphQL Query, Mutation and Subscription requests.
+ *
+ * @author Daniel Frey
+ * @since 6.0
+ */
+public class GraphQlMessageHandler extends AbstractReplyProducingMessageHandler {
+
+	private final GraphQlService graphQlService;
+
+	private StandardEvaluationContext evaluationContext;
+
+	private Expression queryExpression;
+
+	private Expression operationNameExpression = new SupplierExpression<>(() -> null);
+
+	private Expression variablesExpression = new SupplierExpression<>(() -> Collections.emptyMap());
+
+	private Locale locale = null;
+
+	private String executionId = null;
+
+	public GraphQlMessageHandler(final GraphQlService graphQlService) {
+		Assert.notNull(graphQlService, "'graphQlService' must not be null");
+
+		this.graphQlService = graphQlService;
+		setAsync(true);
+	}
+
+	/**
+	 * Specify a GraphQL Query.
+	 * @param query the GraphQL query to use.
+	 */
+	public void setQuery(String query) {
+		Assert.hasText(query, "'query' must not be empty");
+		setQueryExpression(new LiteralExpression(query));
+	}
+
+	/**
+	 * Specify a SpEL expression to evaluate a GraphQL Query
+	 * @param queryExpression the expression to evaluate a GraphQL query.
+	 */
+	public void setQueryExpression(Expression queryExpression) {
+		Assert.notNull(queryExpression, "'queryExpression' must not be null");
+		this.queryExpression = queryExpression;
+	}
+
+	/**
+	 * Set a GraphQL Operation Name to execute.
+	 * @param operationName the GraphQL Operation Name to use.
+	 */
+	public void setOperationName(String operationName) {
+		setOperationNameExpression(new LiteralExpression(operationName));
+	}
+
+	/**
+	 * Set a SpEL expression to evaluate a GraphQL Operation Name to execute.
+	 * @param operationNameExpression the expression to use.
+	 */
+	public void setOperationNameExpression(Expression operationNameExpression) {
+		Assert.notNull(operationNameExpression, "'operationNameExpression' must not be null");
+		this.operationNameExpression = operationNameExpression;
+	}
+
+	/**
+	 * Set a SpEL expression to evaluate Variables for GraphQL Query to execute.
+	 * @param variablesExpression the expression to use.
+	 */
+	public void setVariablesExpression(Expression variablesExpression) {
+		Assert.notNull(variablesExpression, "'variablesExpression' must not be null");
+		this.variablesExpression = variablesExpression;
+	}
+
+	/**
+	 * Set a Locale for GraphQL Query to execute.
+	 * @param locale the locale to use.
+	 */
+	public void setLocale(@Nullable Locale locale) {
+		this.locale = locale;
+	}
+
+	/**
+	 * Set a Execution Id for GraphQL Query to execute.
+	 * @param executionId the executionId to use.
+	 */
+	public void setExecutionId(String executionId) {
+		Assert.hasText(executionId, "'executionId' must not be empty");
+		this.executionId = executionId;
+	}
+
+	@Override
+	protected final void doInit() {
+		BeanFactory beanFactory = getBeanFactory();
+		this.evaluationContext = ExpressionUtils.createStandardEvaluationContext(beanFactory);
+	}
+
+	@Override
+	protected Object handleRequestMessage(Message<?> requestMessage) {
+		RequestInput requestInput = null;
+
+		if (requestMessage.getPayload() instanceof RequestInput) {
+
+			requestInput = (RequestInput) requestMessage.getPayload();
+		}
+		else  {
+			Assert.notNull(this.queryExpression, "'queryExpression' must not be null");
+			Assert.hasText(this.executionId, "'executionId' must not be empty");
+			String query = evaluateQueryExpression(requestMessage);
+			String operationName = evaluateOperationNameExpression(requestMessage);
+			Map<String, Object> variables = evaluateVariablesExpression(requestMessage);
+			requestInput = new RequestInput(query, operationName, variables, this.locale, this.executionId);
+		}
+
+		return this.graphQlService
+				.execute(requestInput);
+
+	}
+
+	private String evaluateQueryExpression(Message<?> message) {
+		String query = this.queryExpression.getValue(this.evaluationContext, message, String.class);
+		Assert.notNull(query, "'queryExpression' must not evaluate to null");
+		return query;
+	}
+
+	private String evaluateOperationNameExpression(Message<?> message) {
+		return this.operationNameExpression.getValue(this.evaluationContext, message, String.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	private Map<String, Object> evaluateVariablesExpression(Message<?> message) {
+		return this.variablesExpression.getValue(this.evaluationContext, message, Map.class);
+	}
+
+}
---
+++ spring-integration-graphql/src/test/java/org/springframework/integration/graphql/outbound/GraphQlMessageHandlerTests.java
@@ -0,0 +1,481 @@
+/*
+ * Copyright 2021-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.graphql.outbound;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.graphql.GraphQlService;
+import org.springframework.graphql.RequestInput;
+import org.springframework.graphql.RequestOutput;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SubscriptionMapping;
+import org.springframework.graphql.data.method.annotation.support.AnnotatedControllerConfigurer;
+import org.springframework.graphql.execution.ExecutionGraphQlService;
+import org.springframework.graphql.execution.GraphQlSource;
+import org.springframework.integration.channel.FluxMessageChannel;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandlingException;
+import org.springframework.messaging.PollableChannel;
+import org.springframework.messaging.support.ErrorMessage;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.util.AlternativeJdkIdGenerator;
+import org.springframework.util.IdGenerator;
+
+import graphql.execution.reactive.SubscriptionPublisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+/**
+ *
+ * @author Daniel Frey
+ * @since 6.0
+ */
+@SpringJUnitConfig
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class GraphQlMessageHandlerTests {
+
+	@Autowired
+	private FluxMessageChannel inputChannel;
+
+	@Autowired
+	private FluxMessageChannel resultChannel;
+
+	@Autowired
+	private PollableChannel errorChannel;
+
+	@Autowired
+	private GraphQlMessageHandler graphQlMessageHandler;
+
+	@Autowired
+	private UpdateRepository updateRepository;
+
+	@Autowired
+	private IdGenerator idGenerator;
+
+
+	@Test
+	@SuppressWarnings("unchecked")
+	void testHandleMessageForQueryWithRequestInputProvided() {
+
+		Locale locale = Locale.getDefault();
+		String executionId = this.idGenerator.generateId().toString();
+		this.graphQlMessageHandler.setLocale(locale);
+		this.graphQlMessageHandler.setExecutionId(executionId);
+
+		StepVerifier verifier = StepVerifier.create(
+				Flux.from(this.resultChannel)
+						.map(Message::getPayload)
+						.cast(RequestOutput.class)
+				)
+				.consumeNextWith(result -> {
+					assertThat(result).isInstanceOf(RequestOutput.class);
+					Map<String, Object> data = result.getData();
+					Map<String, Object> testQuery = (Map<String, Object>) data.get("testQuery");
+					assertThat(testQuery.get("id")).isEqualTo("test-data");
+				})
+				.thenCancel()
+				.verifyLater();
+
+		this.inputChannel.send(
+				MessageBuilder
+						.withPayload(new RequestInput("{ testQuery { id } }", null, Collections.emptyMap(), locale, executionId))
+						.build()
+		);
+
+		verifier.verify(Duration.ofSeconds(10));
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	void testHandleMessageForQueryWithQueryProvided() {
+
+		String executionId = this.idGenerator.generateId().toString();
+		this.graphQlMessageHandler.setExecutionId(executionId);
+
+		String fakeQuery = "{ testQuery { id } }";
+		this.graphQlMessageHandler.setQuery(fakeQuery);
+
+		StepVerifier.create(
+				Mono.from((Mono<RequestOutput>) this.graphQlMessageHandler.handleRequestMessage(MessageBuilder.withPayload(fakeQuery).build()))
+		)
+				.consumeNextWith(result -> {
+					assertThat(result).isInstanceOf(RequestOutput.class);
+					Map<String, Object> data = result.getData();
+					Map<String, Object> testQuery = (Map<String, Object>) data.get("testQuery");
+					assertThat(testQuery.get("id")).isEqualTo("test-data");
+				})
+				.expectComplete()
+				.verify();
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	void testHandleMessageForMutationWithRequestInputProvided() {
+
+		String executionId = this.idGenerator.generateId().toString();
+		this.graphQlMessageHandler.setExecutionId(executionId);
+
+		String fakeId = UUID.randomUUID().toString();
+		Update expected = new Update(fakeId);
+
+		StepVerifier verifier = StepVerifier.create(
+						Flux.from(this.resultChannel)
+								.map(Message::getPayload)
+								.cast(RequestOutput.class)
+				)
+				.consumeNextWith(result -> {
+							assertThat(result).isInstanceOf(RequestOutput.class);
+							Map<String, Object> data = result.getData();
+							Map<String, Object> update = (Map<String, Object>) data.get("update");
+							assertThat(update.get("id")).isEqualTo(fakeId);
+
+							assertThat(this.updateRepository.current().block()).isEqualTo(expected);
+						}
+				)
+				.thenCancel()
+				.verifyLater();
+
+		this.inputChannel.send(
+				MessageBuilder
+						.withPayload(new RequestInput("mutation { update(id: \"" + fakeId + "\") { id } }", null, Collections.emptyMap(), null, executionId))
+						.build()
+		);
+
+		verifier.verify(Duration.ofSeconds(10));
+
+		StepVerifier.create(this.updateRepository.current())
+				.expectNext(expected)
+				.expectComplete()
+				.verify();
+
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	void testHandleMessageForSubscriptionWithRequestInputProvided() {
+
+		String executionId = this.idGenerator.generateId().toString();
+		this.graphQlMessageHandler.setExecutionId(executionId);
+
+		StepVerifier verifier = StepVerifier.create(
+						Flux.from(this.resultChannel)
+								.map(Message::getPayload)
+								.cast(RequestOutput.class)
+								.mapNotNull(RequestOutput::getData)
+								.cast(SubscriptionPublisher.class)
+								.map(Flux::from)
+								.flatMap(data -> data)
+				)
+				.consumeNextWith(requestOutput -> {
+					Map<String, Object> results = (Map<String, Object>) requestOutput.getData();
+					assertThat(results).containsKey("results");
+
+					Map<String, Object> queryResult = (Map<String, Object>) results.get("results");
+					assertThat(queryResult)
+							.containsKey("id")
+							.containsValue("test-data-01");
+
+				})
+				.expectNextCount(9)
+				.thenCancel()
+				.verifyLater();
+
+		this.inputChannel.send(
+				MessageBuilder
+						.withPayload(new RequestInput("subscription { results { id } }", null, Collections.emptyMap(), null, executionId))
+						.build()
+		);
+
+		verifier.verify(Duration.ofSeconds(10));
+	}
+
+	@Test
+	void testHandleMessageForQueryWithInvalidPayload() {
+
+		this.inputChannel.send(
+				MessageBuilder
+						.withPayload(new Object())
+						.build()
+		);
+
+		Message<?> errorMessage = errorChannel.receive(10_000);
+		assertThat(errorMessage).isNotNull()
+				.isInstanceOf(ErrorMessage.class)
+				.extracting(Message::getPayload)
+				.isInstanceOf(MessageHandlingException.class)
+				.satisfies((ex) -> assertThat((Exception) ex)
+						.hasMessageContaining(
+								"'queryExpression' must not be null"));
+
+	}
+
+	@Test
+	void testHandleMessageForMutationWithInvalidPayload() {
+
+		this.inputChannel.send(
+				MessageBuilder
+						.withPayload(new Object())
+						.build()
+		);
+
+		Message<?> errorMessage = errorChannel.receive(10_000);
+		assertThat(errorMessage).isNotNull()
+				.isInstanceOf(ErrorMessage.class)
+				.extracting(Message::getPayload)
+				.isInstanceOf(MessageHandlingException.class)
+				.satisfies((ex) -> assertThat((Exception) ex)
+						.hasMessageContaining(
+								"'queryExpression' must not be null"));
+
+	}
+
+	@Test
+	void testHandleMessageForSubscriptionWithInvalidPayload() {
+
+		this.inputChannel.send(
+				MessageBuilder
+						.withPayload(new Object())
+						.build()
+		);
+
+		Message<?> errorMessage = errorChannel.receive(10_000);
+		assertThat(errorMessage).isNotNull()
+				.isInstanceOf(ErrorMessage.class)
+				.extracting(Message::getPayload)
+				.isInstanceOf(MessageHandlingException.class)
+				.satisfies((ex) -> assertThat((Exception) ex)
+						.hasMessageContaining(
+								"'queryExpression' must not be null"));
+
+	}
+
+	@Controller
+	static class GraphQlController {
+
+		final UpdateRepository updateRepository;
+
+		GraphQlController(UpdateRepository updateRepository) {
+			this.updateRepository = updateRepository;
+		}
+
+		@QueryMapping
+		public Mono<QueryResult> testQuery() {
+			return Mono.just(new QueryResult("test-data"));
+		}
+
+		@QueryMapping
+		public Mono<QueryResult> testQueryById(@Argument String id) {
+			return Mono.just(new QueryResult("test-data"));
+		}
+
+		@MutationMapping
+		public Mono<Update> update(@Argument String id) {
+			return this.updateRepository.save(new Update(id));
+		}
+
+		@SubscriptionMapping
+		public Flux<QueryResult> results() {
+			return Flux.just(
+					new QueryResult("test-data-01"),
+					new QueryResult("test-data-02"),
+					new QueryResult("test-data-03"),
+					new QueryResult("test-data-04"),
+					new QueryResult("test-data-05"),
+					new QueryResult("test-data-06"),
+					new QueryResult("test-data-07"),
+					new QueryResult("test-data-08"),
+					new QueryResult("test-data-09"),
+					new QueryResult("test-data-10")
+			);
+		}
+
+	}
+
+	@Repository
+	static class UpdateRepository {
+
+		private Update current;
+
+		Mono<Update> save(Update update) {
+			this.current = update;
+			return Mono.justOrEmpty(this.current);
+		}
+
+		Mono<Update> current() {
+			return Mono.just(this.current);
+		}
+
+	}
+
+	@Configuration
+	@EnableIntegration
+	static class TestConfig {
+
+		@Bean
+		GraphQlMessageHandler handler(GraphQlService graphQlService) {
+
+			return new GraphQlMessageHandler(graphQlService);
+		}
+
+		@Bean
+		IntegrationFlow graphqlQueryMessageHandlerFlow(GraphQlMessageHandler handler) {
+
+			return IntegrationFlows.from(MessageChannels.flux("inputChannel"))
+					.handle(handler)
+					.channel(c -> c.flux("resultChannel"))
+					.get();
+		}
+
+		@Bean
+		PollableChannel errorChannel() {
+
+			return new QueueChannel();
+		}
+
+		@Bean
+		UpdateRepository updateRepository() {
+			return new UpdateRepository();
+		}
+
+		@Bean
+		GraphQlController graphqlQueryController(final UpdateRepository updateRepository) {
+
+			return new GraphQlController(updateRepository);
+		}
+
+		@Bean
+		GraphQlService graphQlService(GraphQlSource graphQlSource) {
+
+			return new ExecutionGraphQlService(graphQlSource);
+		}
+
+		@Bean
+		GraphQlSource graphQlSource(AnnotatedControllerConfigurer annotatedDataFetcherConfigurer) {
+
+			return GraphQlSource.builder()
+					.schemaResources(new ClassPathResource("graphql/test-schema.graphqls"))
+					.configureRuntimeWiring(annotatedDataFetcherConfigurer)
+					.build();
+		}
+
+		@Bean
+		AnnotatedControllerConfigurer annotatedDataFetcherConfigurer() {
+
+			return new AnnotatedControllerConfigurer();
+		}
+
+		@Bean
+		IdGenerator idGenerator() {
+
+			return new AlternativeJdkIdGenerator();
+		}
+
+	}
+
+	static class QueryResult {
+
+		private final String id;
+
+		QueryResult(final String id) {
+			this.id = id;
+		}
+
+		String getId() {
+			return this.id;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) {
+				return true;
+			}
+			if (!(o instanceof QueryResult)) {
+				return false;
+			}
+			QueryResult that = (QueryResult) o;
+			return getId().equals(that.getId());
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(getId());
+		}
+
+		@Override
+		public String toString() {
+			return "QueryResult{" +
+					"id='" + id + '\'' +
+					'}';
+		}
+	}
+
+	static class Update {
+
+		private final String id;
+
+		Update(final String id) {
+			this.id = id;
+		}
+
+		String getId() {
+			return this.id;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) {
+				return true;
+			}
+			if (!(o instanceof Update)) {
+				return false;
+			}
+			Update update = (Update) o;
+			return getId().equals(update.getId());
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(getId());
+		}
+	}
+
+}
