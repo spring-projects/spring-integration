5e0e62035158638432366b71e88fcc83749dc365
Artem Bilan <abilan@vmware.com>
2022-03-15 13:31:45 -0400

    Cancel subscription for MPS.subscribeToPublisher
    The `Flux.takeWhile()` only works if there is data in the `Publisher`
    to consume.
    We still need to be able to cancel subscription and stop producing even if
    there is no data at the moment.
    
    * Change `takeWhile()` to the `doOnSubscribe()` and store `subscription`
    in the `volatile` property of the `MessageProducerSupport`
    * Cancel such a subscription in the `doStop()` impl
    * Propagate `doStop()` to super in the `ZeroMqMessageProducer`
    which is only one reactive channel adapter overriding `doStop()`
    * Verify in the `ReactiveMessageProducerTests` that subscription is cancelled
    for delayed data in the `Publisher`
    
    **Cherry-pick to `5.5.x`**

--- spring-integration-zeromq/src/main/java/org/springframework/integration/zeromq/inbound/ZeroMqMessageProducer.java
+++ spring-integration-zeromq/src/main/java/org/springframework/integration/zeromq/inbound/ZeroMqMessageProducer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 the original author or authors.
+ * Copyright 2020-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -292,6 +292,7 @@ public class ZeroMqMessageProducer extends MessageProducerSupport {
 
 	@Override
 	protected void doStop() {
+		super.doStop();
 		this.socketMono.doOnNext(ZMQ.Socket::close).subscribe();
 	}
 
