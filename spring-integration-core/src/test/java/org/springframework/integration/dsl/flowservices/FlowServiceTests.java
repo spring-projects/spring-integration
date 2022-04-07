f7913c1380ebabfb2640f14b6de1ac211761680c
Artem Bilan <abilan@vmware.com>
2022-04-07 14:38:59 -0400

    GH-3615: Make log() as non-terminal in DSL
    Fixes https://github.com/spring-projects/spring-integration/issues/3615
    
    The `log()` operator for Java DSL in the end of flow causes a confusion
    for its different behavior and, therefore, inconsistency with expectations
    
    * Populate a `bridge()` in the end of flow instead of `nullChannel` when
    the current component is `WireTapSpec` (relevant to `wireTap()` and `log()`)
    * Deprecate a `logAndReply()` operator since its behavior is now included into the `log()` in the end if flow
    * Fix tests to use just `log()` in the end from now on
    * Fix `dsl.adoc` for the new `log()` behaviour in the end of flow

--- spring-integration-core/src/test/java/org/springframework/integration/dsl/flowservices/FlowServiceTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/dsl/flowservices/FlowServiceTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2020 the original author or authors.
+ * Copyright 2016-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -127,7 +127,7 @@ public class FlowServiceTests {
 		@Bean
 		public IntegrationFlow testGateway() {
 			return f -> f.gateway("processChannel", g -> g.replyChannel("replyChannel"))
-					.logAndReply();
+					.log();
 		}
 
 		@Bean
@@ -152,7 +152,8 @@ public class FlowServiceTests {
 					.log(LoggingHandler.Level.ERROR, m -> {
 						resultOverLoggingHandler.set(m.getPayload());
 						return m;
-					});
+					})
+					.nullChannel();
 		}
 
 		@Override
--- spring-integration-core/src/test/java/org/springframework/integration/dsl/transformers/TransformerTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/dsl/transformers/TransformerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2021 the original author or authors.
+ * Copyright 2017-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -297,7 +297,8 @@ public class TransformerTests {
 							.propertyFunction("date", m -> new Date())
 							.headerExpression("foo", "payload['name']")
 					)
-					.logAndReply();
+					.log()
+					.get();
 		}
 
 		@Bean
@@ -463,7 +464,7 @@ public class TransformerTests {
 								throw new RuntimeException("intentional");
 							},
 							e -> e.advice(expressionAdvice()))
-					.logAndReply();
+					.log();
 		}
 
 	}
