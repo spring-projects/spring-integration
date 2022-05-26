97ecf31c7f525086f82d36f69ca8c10e0a80316c
Gregory Bragg <gbragg@thecommonwell.ca>
2022-05-26 07:56:37 -0400

    Clean test directory of mistakenly migrated SMB extension code

--- spring-integration-smb/src/test/java/org/springframework/integration/smb/inbound/SmbInboundRemoteFileSystemSynchronizerTests.java
+++ spring-integration-smb/src/test/java/org/springframework/integration/smb/inbound/SmbInboundRemoteFileSystemSynchronizerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,7 +39,6 @@ import jcifs.smb.SmbFile;
 /**
  * @author Markus Spann
  * @author Gunnar Hillert
- * @since 1.0
  */
 public class SmbInboundRemoteFileSystemSynchronizerTests extends AbstractBaseTests {
 
