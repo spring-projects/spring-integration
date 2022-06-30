dd5f17502bf7e0609c7f2d8bbf73f11fd88c04a8
Zahid Khan <63726443+ZFK07@users.noreply.github.com>
2022-07-01 01:15:28 +0530

    Add Exception Message for TestingUtilities.java
    Add exception message "Class Instantiation not allowed." for the TestingUtilities

--- spring-integration-ip/src/main/java/org/springframework/integration/ip/util/TestingUtilities.java
+++ spring-integration-ip/src/main/java/org/springframework/integration/ip/util/TestingUtilities.java
@@ -35,7 +35,7 @@ public final class TestingUtilities {
 
 	// Non-instantiable utility class
 	private TestingUtilities() {
-		throw new AssertionError();
+		throw new AssertionError(""Class Instantiation not allowed."");
 	}
 
 	/**
