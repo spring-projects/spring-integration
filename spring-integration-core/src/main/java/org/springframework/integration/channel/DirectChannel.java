12263d305144c9b71fa082138c330ce1e111f09c
Zahid Khan <63726443+ZFK07@users.noreply.github.com>
2022-07-06 21:45:23 +0530

    Double check for null before assign to setMaxSubscribers(max) in the DirectChannel

--- spring-integration-core/src/main/java/org/springframework/integration/channel/DirectChannel.java
+++ spring-integration-core/src/main/java/org/springframework/integration/channel/DirectChannel.java
@@ -22,6 +22,8 @@ import org.springframework.integration.dispatcher.RoundRobinLoadBalancingStrateg
 import org.springframework.integration.dispatcher.UnicastingDispatcher;
 import org.springframework.lang.Nullable;
 
+import java.util.Objects;
+
 /**
  * A channel that invokes a single subscriber for each sent Message.
  * The invocation will occur in the sender's thread.
@@ -50,6 +52,7 @@ public class DirectChannel extends AbstractSubscribableChannel {
 	 * Create a DirectChannel with a {@link LoadBalancingStrategy}.
 	 * Can be {@code null} meaning that no balancing is applied;
 	 * every message is always going to be handled by the first subscriber.
+	 *
 	 * @param loadBalancingStrategy The load balancing strategy implementation.
 	 * @see #setFailover(boolean)
 	 */
@@ -61,6 +64,7 @@ public class DirectChannel extends AbstractSubscribableChannel {
 	/**
 	 * Specify whether the channel's dispatcher should have failover enabled.
 	 * By default, it will. Set this value to 'false' to disable it.
+	 *
 	 * @param failover The failover boolean.
 	 */
 	public void setFailover(boolean failover) {
@@ -70,9 +74,10 @@ public class DirectChannel extends AbstractSubscribableChannel {
 	/**
 	 * Specify the maximum number of subscribers supported by the
 	 * channel's dispatcher.
+	 *
 	 * @param maxSubscribers The maximum number of subscribers allowed.
 	 */
-	public void setMaxSubscribers(Integer maxSubscribers) {
+	public void setMaxSubscribers(int maxSubscribers) {
 		this.maxSubscribers = maxSubscribers;
 		this.dispatcher.setMaxSubscribers(maxSubscribers);
 	}
@@ -87,7 +92,9 @@ public class DirectChannel extends AbstractSubscribableChannel {
 		super.onInit();
 		if (this.maxSubscribers == null) {
 			Integer max = getIntegrationProperty(IntegrationProperties.CHANNELS_MAX_UNICAST_SUBSCRIBERS, Integer.class);
-			setMaxSubscribers(max);
+			if (!Objects.isNull(max)) {
+				setMaxSubscribers(max);
+			}
 		}
 	}
 
