38bb304218fd5f7667f8da8e16fb5089cfb7232d
Artem Bilan <abilan@vmware.com>
2022-09-12 09:53:23 -0400

    Add Apache Camel support
    * Implement `CamelMessageHandler` to perform send and send-n-reply
    operations to the Apache Camel routes
    * Fix `AbstractMessageProducingHandler` to handle async errors even if reply is
    not expected

---
+++ spring-integration-camel/src/main/java/org/springframework/integration/camel/support/CamelHeaderMapper.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.camel.support;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.camel.Message;
+
+import org.springframework.core.log.LogAccessor;
+import org.springframework.core.log.LogMessage;
+import org.springframework.integration.mapping.HeaderMapper;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.util.Assert;
+import org.springframework.util.PatternMatchUtils;
+
+/**
+ * A {@link HeaderMapper} for mapping headers from Spring Integration message
+ * to Apache Camel message and back.
+ *
+ * @author Artem Bilan
+ *
+ * @since 6.0
+ */
+public class CamelHeaderMapper implements HeaderMapper<org.apache.camel.Message> {
+
+
+	private static final LogAccessor LOGGER = new LogAccessor(CamelHeaderMapper.class);
+
+	private String[] inboundHeaderNames = { "*" };
+
+	private String[] outboundHeaderNames = { "*" };
+
+	/**
+	 * Provide a list of patterns to map Apache Camel message headers into Spring Integration message.
+	 * By default, it maps all.
+	 * @param inboundHeaderNames the Apache Camel message headers patterns to map.
+	 */
+	public void setInboundHeaderNames(String... inboundHeaderNames) {
+		Assert.notNull(inboundHeaderNames, "'inboundHeaderNames' must not be null");
+		String[] copy = Arrays.copyOf(inboundHeaderNames, inboundHeaderNames.length);
+		Arrays.sort(copy);
+		this.inboundHeaderNames = copy;
+	}
+
+	/**
+	 * Provide a list of patterns to map Spring Integration message headers into an Apache Camel message.
+	 * By default, it maps all.
+	 * @param outboundHeaderNames the header patterns to map.
+	 */
+	public void setOutboundHeaderNames(String... outboundHeaderNames) {
+		Assert.notNull(outboundHeaderNames, "'outboundHeaderNames' must not be null");
+		String[] copy = Arrays.copyOf(outboundHeaderNames, outboundHeaderNames.length);
+		Arrays.sort(copy);
+		this.outboundHeaderNames = copy;
+	}
+
+	@Override
+	public void fromHeaders(MessageHeaders headers, Message target) {
+		for (Map.Entry<String, Object> entry : headers.entrySet()) {
+			String name = entry.getKey();
+			if (shouldMapHeader(name, this.outboundHeaderNames)) {
+				Object value = entry.getValue();
+				if (value != null) {
+					target.setHeader(name, value);
+				}
+			}
+		}
+	}
+
+	@Override
+	public Map<String, Object> toHeaders(Message source) {
+		Map<String, Object> headers = new HashMap<>();
+		for (Map.Entry<String, Object> entry : source.getHeaders().entrySet()) {
+			String name = entry.getKey();
+			if (shouldMapHeader(name, this.inboundHeaderNames)) {
+				Object value = entry.getValue();
+				if (value != null) {
+					headers.put(name, value);
+				}
+			}
+		}
+		return headers;
+	}
+
+	private static boolean shouldMapHeader(String headerName, String[] patterns) {
+		if (patterns.length > 0) {
+			for (String pattern : patterns) {
+				if (PatternMatchUtils.simpleMatch(pattern, headerName)) {
+					LOGGER.debug(LogMessage.format("headerName=[{0}] WILL be mapped, matched pattern={1}",
+							headerName, pattern));
+					return true;
+				}
+			}
+		}
+		LOGGER.debug(LogMessage.format("headerName=[{0}] WILL NOT be mapped", headerName));
+		return false;
+	}
+
+}
---
+++ spring-integration-camel/src/test/java/org/springframework/integration/camel/outbound/CamelMessageHandlerTests.java
@@ -0,0 +1,189 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.camel.outbound;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.throwable;
+import static org.mockito.Mockito.mock;
+
+import org.apache.camel.CamelExecutionException;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.integration.camel.support.CamelHeaderMapper;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.expression.FunctionExpression;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandlingException;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.GenericMessage;
+
+/**
+ * @author Artem Bilan
+ *
+ * @since 6.0
+ */
+public class CamelMessageHandlerTests extends CamelTestSupport {
+
+	@Test
+	void inOnlyPatternSyncMessageHandler() throws InterruptedException {
+		Message<String> messageUnderTest = new GenericMessage<>("Hello Camel!");
+		Message<String> messageUnderTest2 = new GenericMessage<>("Hello Camel again!");
+
+		MockEndpoint mockEndpoint = getMockEndpoint("mock:result");
+		mockEndpoint.message(0).body().isEqualTo(messageUnderTest.getPayload());
+		mockEndpoint.message(0).header(MessageHeaders.ID).isEqualTo(messageUnderTest.getHeaders().getId());
+		mockEndpoint.message(1).body().isEqualTo(messageUnderTest2.getPayload());
+		mockEndpoint.message(1).header(MessageHeaders.ID).isEqualTo(messageUnderTest2.getHeaders().getId());
+
+		CamelMessageHandler camelMessageHandler = new CamelMessageHandler(template());
+		camelMessageHandler.setEndpointUri("direct:simple");
+		camelMessageHandler.setBeanFactory(mock(BeanFactory.class));
+		camelMessageHandler.afterPropertiesSet();
+
+		camelMessageHandler.handleMessage(messageUnderTest);
+		camelMessageHandler.handleMessage(messageUnderTest2);
+
+		assertMockEndpointsSatisfied();
+	}
+
+	@Test
+	void inOutPatternSyncMessageHandlerWithNoRequestHeadersButReplyHeaders() throws InterruptedException {
+		SpelExpressionParser spelExpressionParser = new SpelExpressionParser();
+		QueueChannel replyChannel = new QueueChannel();
+		Message<String> messageUnderTest =
+				MessageBuilder.withPayload("test data")
+						.setHeader("exchangePattern", "InOptionalOut")
+						.setHeader(MessageHeaders.REPLY_CHANNEL, replyChannel)
+						.build();
+
+		MockEndpoint mockEndpoint = getMockEndpoint("mock:result");
+		mockEndpoint.expectedHeaderReceived(MessageHeaders.ID, null);
+		mockEndpoint.expectedHeaderReceived(MessageHeaders.TIMESTAMP, null);
+		mockEndpoint.whenAnyExchangeReceived(exchange -> {
+			org.apache.camel.Message out = exchange.getMessage();
+			out.setBody("Reply for: " + exchange.getIn().getBody());
+			out.setHeader("testHeader", "testHeaderValue");
+			out.setHeader("notMappedHeader", "someValue");
+		});
+
+		CamelHeaderMapper headerMapper = new CamelHeaderMapper();
+		headerMapper.setOutboundHeaderNames("");
+		headerMapper.setInboundHeaderNames("testHeader");
+
+		CamelMessageHandler camelMessageHandler = new CamelMessageHandler(template());
+		camelMessageHandler.setEndpointUriExpression(new FunctionExpression<>(m -> "direct:simple"));
+		camelMessageHandler.setExchangePatternExpression(spelExpressionParser.parseExpression("headers.exchangePattern"));
+		camelMessageHandler.setHeaderMapper(headerMapper);
+		camelMessageHandler.setBeanFactory(mock(BeanFactory.class));
+		camelMessageHandler.afterPropertiesSet();
+
+		camelMessageHandler.handleMessage(messageUnderTest);
+
+		Message<?> receive = replyChannel.receive(10_000);
+
+		assertMockEndpointsSatisfied();
+
+		assertThat(receive).isNotNull();
+		assertThat(receive.getPayload()).isEqualTo("Reply for: test data");
+		assertThat(receive.getHeaders())
+				.containsEntry("testHeader", "testHeaderValue")
+				.doesNotContainKey("notMappedHeader");
+	}
+
+	@Test
+	void inOnlyPatternAsyncMessageHandlerWithException() throws InterruptedException {
+		QueueChannel errorChannel = new QueueChannel();
+		Message<String> messageUnderTest =
+				MessageBuilder.withPayload("test data")
+						.setHeader(MessageHeaders.ERROR_CHANNEL, errorChannel)
+						.build();
+
+		getMockEndpoint("mock:result")
+				.whenAnyExchangeReceived(exchange -> {
+					throw new RuntimeException("intentional");
+				});
+
+		CamelMessageHandler camelMessageHandler = new CamelMessageHandler(template());
+		camelMessageHandler.setEndpointUri("direct:simple");
+		camelMessageHandler.setBeanFactory(mock(BeanFactory.class));
+		camelMessageHandler.setAsync(true);
+		camelMessageHandler.afterPropertiesSet();
+
+		camelMessageHandler.handleMessage(messageUnderTest);
+		Message<?> receive = errorChannel.receive(10_000);
+
+		assertMockEndpointsSatisfied();
+
+		assertThat(receive).isNotNull();
+		assertThat(receive.getPayload())
+				.asInstanceOf(throwable(MessageHandlingException.class))
+				.hasCauseInstanceOf(CamelExecutionException.class)
+				.hasRootCauseExactlyInstanceOf(RuntimeException.class)
+				.hasStackTraceContaining("intentional");
+	}
+
+	@Test
+	void inOutPatternAsyncMessageHandler() throws InterruptedException {
+		QueueChannel replyChannel = new QueueChannel();
+		Message<String> messageUnderTest =
+				MessageBuilder.withPayload("test async data")
+						.setHeader(MessageHeaders.REPLY_CHANNEL, replyChannel)
+						.build();
+
+		MockEndpoint mockEndpoint = getMockEndpoint("mock:result");
+		mockEndpoint.whenAnyExchangeReceived(exchange ->
+				exchange.getMessage().setBody("Async reply for: " + exchange.getIn().getBody()));
+
+		ProducerTemplate producerTemplate = template();
+		producerTemplate.setDefaultEndpointUri("direct:simple");
+		CamelMessageHandler camelMessageHandler = new CamelMessageHandler(producerTemplate);
+		camelMessageHandler.setExchangePattern(ExchangePattern.InOut);
+		camelMessageHandler.setBeanFactory(mock(BeanFactory.class));
+		camelMessageHandler.setAsync(true);
+		camelMessageHandler.afterPropertiesSet();
+
+		camelMessageHandler.handleMessage(messageUnderTest);
+
+		Message<?> receive = replyChannel.receive(10_000);
+
+		assertMockEndpointsSatisfied();
+
+		assertThat(receive).isNotNull();
+		assertThat(receive.getPayload()).isEqualTo("Async reply for: test async data");
+	}
+
+	@Override
+	protected RoutesBuilder createRouteBuilder() {
+		return new RouteBuilder() {
+
+			@Override
+			public void configure() {
+				from("direct:simple").to("mock:result");
+			}
+		};
+	}
+
+}
