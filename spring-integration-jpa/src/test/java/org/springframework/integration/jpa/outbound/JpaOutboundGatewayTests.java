f7dd876be2946c54a77eee8fa37912cda67814b9
Artem Bilan <abilan@vmware.com>
2022-10-11 14:39:29 -0400

    INT-3333: Return empty list as is from DB gateway (#3907)
    * INT-3333: Return empty list as is from DB gateway
    
    Fixes https://jira.spring.io/browse/INT-3333
    
    In `JdbcOutboundGateway` and `JpaOutboundGateway` the empty result list
    is treated as "no reply" and therefore `null` is returned cause
    the flow to stop at this point.
    It is better to return such a result as is and the target application to
    decided what to do with it, e.g. a `discardChannel` on a downstream splitter
    configuration.
    
    NOTE: the `MongoDbOutboundGateway` doesn't treat an empty result as a `null`
    
    * * Fix language in docs
    
    Co-authored-by: Gary Russell <grussell@vmware.com>
    
    Co-authored-by: Gary Russell <grussell@vmware.com>

--- spring-integration-jpa/src/test/java/org/springframework/integration/jpa/outbound/JpaOutboundGatewayTests.java
+++ spring-integration-jpa/src/test/java/org/springframework/integration/jpa/outbound/JpaOutboundGatewayTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -160,7 +160,7 @@ public class JpaOutboundGatewayTests {
 		List<StudentDomain> allStudents = this.studentService.getAllStudents();
 		assertThat(allStudents).hasSize(3);
 		this.studentService.deleteStudents(allStudents);
-		assertThat(this.studentService.getAllStudents()).isNull();
+		assertThat(this.studentService.getAllStudents()).isEmpty();
 	}
 
 }
