4a6e52f3f00c879f12b6bf9a2c1ececf7bf0fe65
abilan <abilan@vmware.com>
2023-01-09 16:27:06 -0500

    Some tweaks for SimpleMessageGroupTests
    **Cherry-pick to `5.5.x`**

--- spring-integration-core/src/test/java/org/springframework/integration/store/SimpleMessageGroupTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/store/SimpleMessageGroupTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2019 the original author or authors.
+ * Copyright 2009-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
