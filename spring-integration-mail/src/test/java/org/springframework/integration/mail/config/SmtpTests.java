4be99de1f4bef3af2667f1300763adb8b2818992
abilan <abilan@vmware.com>
2023-01-18 11:03:10 -0500

    Upgrade dependencies; prepare for release
    * Fix mail tests for the latest Greenmail changes

--- spring-integration-mail/src/test/java/org/springframework/integration/mail/config/SmtpTests.java
+++ spring-integration-mail/src/test/java/org/springframework/integration/mail/config/SmtpTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2022 the original author or authors.
+ * Copyright 2016-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -81,7 +81,7 @@ public class SmtpTests {
 		assertThat(message.getFrom()).containsOnly(new InternetAddress("foo@bar"));
 		assertThat(message.getRecipients(RecipientType.TO)).containsOnly(new InternetAddress("bar@baz"));
 		assertThat(message.getSubject()).isEqualTo("foo");
-		assertThat(message.getContent()).asString().isEqualTo("foo\r\n");
+		assertThat(message.getContent()).asString().isEqualTo("foo");
 	}
 
 }
--- spring-integration-mail/src/test/java/org/springframework/integration/mail/dsl/MailTests.java
+++ spring-integration-mail/src/test/java/org/springframework/integration/mail/dsl/MailTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2022 the original author or authors.
+ * Copyright 2014-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -129,7 +129,7 @@ public class MailTests {
 		assertThat(message.getFrom()).containsOnly(new InternetAddress("foo@bar"));
 		assertThat(message.getRecipients(RecipientType.TO)).containsOnly(new InternetAddress("bar@baz"));
 		assertThat(message.getSubject()).isEqualTo("foo");
-		assertThat(message.getContent()).asString().isEqualTo("foo\r\n");
+		assertThat(message.getContent()).asString().isEqualTo("foo");
 
 	}
 
