b0093ef161493a6918062494640ced23783297d8
abilan <abilan@vmware.com>
2023-03-30 10:47:46 -0400

    TestXmlAppCtxHelper: Fix trailing white space

--- spring-integration-xml/src/test/java/org/springframework/integration/xml/config/TestXmlApplicationContextHelper.java
+++ spring-integration-xml/src/test/java/org/springframework/integration/xml/config/TestXmlApplicationContextHelper.java
@@ -25,28 +25,27 @@ package org.springframework.integration.xml.config;
  */
 public class TestXmlApplicationContextHelper {
 
-	private static final String header = """
-			<?xml version='1.0' encoding='UTF-8'?>
-			<beans xmlns='http://www.springframework.org/schema/beans'
-					xmlns:si-xml='http://www.springframework.org/schema/integration/xml'
-					xmlns:si='http://www.springframework.org/schema/integration'
-					xmlns:util='http://www.springframework.org/schema/util'
-					xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
-					xmlns:context='http://www.springframework.org/schema/context'
-					xsi:schemaLocation='
-						http://www.springframework.org/schema/beans
-						https://www.springframework.org/schema/beans/spring-beans.xsd
-						http://www.springframework.org/schema/integration
-						https://www.springframework.org/schema/integration/spring-integration.xsd
-						http://www.springframework.org/schema/integration/xml
-						https://www.springframework.org/schema/integration/xml/spring-integration-xml.xsd
-						http://www.springframework.org/schema/util
-						https://www.springframework.org/schema/util/spring-util.xsd
-						http://www.springframework.org/schema/context
-						https://www.springframework.org/schema/context/spring-context.xsd'>
-
-				<context:annotation-config/>
-				""";
+	private static final String header =
+			"""
+					<?xml version='1.0' encoding='UTF-8'?>
+					<beans xmlns='http://www.springframework.org/schema/beans'
+							xmlns:si-xml='http://www.springframework.org/schema/integration/xml'
+							xmlns:si='http://www.springframework.org/schema/integration'
+							xmlns:util='http://www.springframework.org/schema/util'
+							xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
+							xmlns:context='http://www.springframework.org/schema/context'
+							xsi:schemaLocation='http://www.springframework.org/schema/beans
+								https://www.springframework.org/schema/beans/spring-beans.xsd
+								http://www.springframework.org/schema/integration
+								https://www.springframework.org/schema/integration/spring-integration.xsd
+								http://www.springframework.org/schema/integration/xml
+								https://www.springframework.org/schema/integration/xml/spring-integration-xml.xsd
+								http://www.springframework.org/schema/util
+								https://www.springframework.org/schema/util/spring-util.xsd
+								http://www.springframework.org/schema/context
+								https://www.springframework.org/schema/context/spring-context.xsd'>
+						<context:annotation-config/>
+					""";
 
 	private static final String footer = "</beans>";
 
