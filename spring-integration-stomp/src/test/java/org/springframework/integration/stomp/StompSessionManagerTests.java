edbaf6d5904ffeb89bd2dff4b36846feed5153a8
abilan <abilan@vmware.com>
2023-05-16 20:40:11 -0400

    Start version 6.2.0
    * Add Christian Tzolov to author name in the doc
    * Move `whats-new.adoc` content to new `changes-6.0-6.1.adoc`
    * Upgrade to Gradle `8.1.1`
    * Upgrade to Spring Framework `6.1`
    * Fix `StompSessionManagerTests` for deprecated `ConcurrentTaskScheduler` ctor
    * Fix XML configs for Kafka to use types for ctor args instead of their names.
    Apparently SF doesn't do a discovery by names anymore.

--- spring-integration-stomp/src/test/java/org/springframework/integration/stomp/StompSessionManagerTests.java
+++ spring-integration-stomp/src/test/java/org/springframework/integration/stomp/StompSessionManagerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2022 the original author or authors.
+ * Copyright 2016-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 package org.springframework.integration.stomp;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -42,7 +43,7 @@ public class StompSessionManagerTests {
 	@Test
 	public void testDoConnectFailure() throws Exception {
 		StompClientSupport stompClient = mock(StompClientSupport.class);
-		stompClient.setTaskScheduler(new ConcurrentTaskScheduler());
+		stompClient.setTaskScheduler(new ConcurrentTaskScheduler(Executors.newSingleThreadScheduledExecutor()));
 		AbstractStompSessionManager sessionManager = new AbstractStompSessionManager(stompClient) {
 
 			private final AtomicBoolean thrown = new AtomicBoolean();
