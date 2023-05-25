7917f47a64f139ab6b615d30caf5e60c6c331dae
Christian Tzolov <christian.tzolov@gmail.com>
2023-05-25 15:16:53 +0200

    Debezium DSL initial support

---
+++ spring-integration-debezium/src/main/java/org/springframework/integration/debezium/dsl/DebeziumMessageProducerSpec.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2023-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.debezium.dsl;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ThreadFactory;
+
+import io.debezium.engine.ChangeEvent;
+import io.debezium.engine.DebeziumEngine;
+import io.debezium.engine.DebeziumEngine.Builder;
+import io.debezium.engine.Header;
+import io.debezium.engine.format.SerializationFormat;
+
+import org.springframework.integration.debezium.inbound.DebeziumMessageProducer;
+import org.springframework.integration.debezium.support.DefaultDebeziumHeaderMapper;
+import org.springframework.integration.dsl.MessageProducerSpec;
+import org.springframework.messaging.support.HeaderMapper;
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
+import org.springframework.util.Assert;
+
+/**
+ *
+ * @author Christian Tzolov
+ *
+ * @since 6.2
+ */
+public class DebeziumMessageProducerSpec
+		extends MessageProducerSpec<DebeziumMessageProducerSpec, DebeziumMessageProducer> {
+
+	private String[] inboundHeaderNames = { "*" };
+
+	public DebeziumMessageProducerSpec(Builder<ChangeEvent<byte[], byte[]>> debeziumEngineBuilder) {
+		super(new DebeziumMessageProducer(debeziumEngineBuilder));
+	}
+
+	/**
+	 * Enable the {@link ChangeEvent} batch mode handling. When enabled the channel adapter will send a {@link List} of
+	 * {@link ChangeEvent}s as a payload in a single downstream {@link Message}. Such a batch payload is not
+	 * serializable. By default, the batch mode is disabled, e.g. every input {@link ChangeEvent} is converted into a
+	 * single downstream {@link Message}.
+	 * @param enable set to true to enable the batch mode. Disabled by default.
+	 * @return the spec.
+	 */
+	public DebeziumMessageProducerSpec enableBatch(boolean enable) {
+		this.target.setEnableBatch(enable);
+		return this;
+	}
+
+	/**
+	 * Enable support for tombstone (aka delete) messages. On a database row delete, Debezium can send a tombstone
+	 * change event that has the same key as the deleted row and a value of {@link Optional#empty()}. This record is a
+	 * marker for downstream processors. It indicates that log compaction can remove all records that have this key.
+	 * When the tombstone functionality is enabled in the Debezium connector configuration you should enable the empty
+	 * payload as well.
+	 * @param enabled set true to enable the empty payload. Disabled by default.
+	 * @return the spec.
+	 */
+	public DebeziumMessageProducerSpec enableEmptyPayload(boolean enabled) {
+		this.target.setEnableEmptyPayload(enabled);
+		return this;
+	}
+
+	/**
+	 * Set a {@link ThreadFactory} for the Debezium executor. Defaults to the {@link CustomizableThreadFactory} with a
+	 * {@code debezium:inbound-channel-adapter-thread-} prefix.
+	 * @param threadFactory the {@link ThreadFactory} instance to use.
+	 * @return the spec.
+	 */
+	public DebeziumMessageProducerSpec threadFactory(ThreadFactory threadFactory) {
+		this.target.setThreadFactory(threadFactory);
+		return this;
+	}
+
+	/**
+	 * Set the outbound message content type. Must be aligned with the {@link SerializationFormat} configuration used by
+	 * the provided {@link DebeziumEngine}.
+	 * @param contentType payload content type.
+	 * @return the spec.
+	 */
+	public DebeziumMessageProducerSpec contentType(String contentType) {
+		this.target.setContentType(contentType);
+		return this;
+	}
+
+	/**
+	 * Comma-separated list of names of {@link ChangeEvent} headers to be mapped into outbound Message headers.
+	 * Debezium's NewRecordStateExtraction 'add.headers' property configures the metadata to be used as
+	 * {@link ChangeEvent} headers.
+	 * <p>
+	 * You should prefix the names passed to the 'headerNames' with the prefix configured by the Debezium
+	 * 'add.headers.prefix' property. Later defaults to '__'. For example for 'add.headers=op,name' and
+	 * 'add.headers.prefix=__' you should use header hames like: '__op', '__name'.
+	 * @param headerNames The values in this list can be a simple patterns to be matched against the header names.
+	 * @return the spec.
+	 */
+	public DebeziumMessageProducerSpec headerNames(String... headerNames) {
+		Assert.notEmpty(headerNames, "'inboundHeaderNames' must not be empty");
+		this.inboundHeaderNames = Arrays.copyOf(headerNames, headerNames.length);
+		return addDebeziumHeaderMapper();
+	}
+
+	/**
+	 * Set a {@link HeaderMapper} to convert the {@link ChangeEvent} headers into {@link Message} headers.
+	 * @param headerMapper {@link HeaderMapper} implementation to use. Defaults to {@link DefaultDebeziumHeaderMapper}.
+	 * @return the spec.
+	 */
+	public DebeziumMessageProducerSpec headerMapper(HeaderMapper<List<Header<Object>>> headerMapper) {
+		this.target.setHeaderMapper(headerMapper);
+		return this;
+	}
+
+	private DebeziumMessageProducerSpec addDebeziumHeaderMapper() {
+		DefaultDebeziumHeaderMapper headerMapper = new DefaultDebeziumHeaderMapper();
+		headerMapper.setHeaderNamesToMap(this.inboundHeaderNames);
+		return headerMapper(headerMapper);
+	}
+
+}
---
+++ spring-integration-debezium/src/main/java/org/springframework/integration/debezium/dsl/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides classes for supporting Debezium component via Java DSL.
+ */
+@org.springframework.lang.NonNullApi
+@org.springframework.lang.NonNullFields
+package org.springframework.integration.debezium.dsl;
