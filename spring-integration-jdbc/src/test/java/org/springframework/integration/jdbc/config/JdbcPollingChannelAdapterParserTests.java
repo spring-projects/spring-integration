7655d97ceebf984973e5e8187f6ceef636078bc6
abilan <abilan@vmware.com>
2023-06-07 15:19:03 -0400

    Fix code typos in filter.adoc & router.adoc
    * Fix race condition in the `JdbcPollingChannelAdapterParserTests`
    making SELECT and UPDATE as a part of the same transaction
    * Remove `inProcess = JAVA_EXEC` from ASCIIDoc Gradle tasks in attempt
    to make them working in parallel

--- spring-integration-jdbc/src/test/java/org/springframework/integration/jdbc/config/JdbcPollingChannelAdapterParserTests.java
+++ spring-integration-jdbc/src/test/java/org/springframework/integration/jdbc/config/JdbcPollingChannelAdapterParserTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,8 +22,8 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.junit.After;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -177,7 +177,7 @@ public class JdbcPollingChannelAdapterParserTests {
 		assertThat(TestUtils.getPropertyValue(autoChannelAdapter, "outputChannel")).isSameAs(autoChannel);
 	}
 
-	@After
+	@AfterEach
 	public void tearDown() {
 		if (appCtx != null) {
 			appCtx.close();
