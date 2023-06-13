a862692546f7eff88f187d38c25c0292d6b9b54f
Artem Bilan <abilan@vmware.com>
2023-06-13 16:06:37 -0400

    Executor instead of ThreadFactory for DebeziumMP
    Related to https://github.com/spring-projects/spring-integration/issues/8642
    
    For consistency with other Spring requirements and realignment with virtual threads,
    it is better to require a `TaskExecutor` injection instead of `ThreadFactory`
    
    * Fix `DebeziumMessageProducer` to rely on a `TaskExecutor` API instead of `ThreadFactory`
    and `ExecutorService`
    
    * * Remove unused import from the `DebeziumMessageProducerSpec`

--- spring-integration-debezium/src/main/java/org/springframework/integration/debezium/dsl/DebeziumMessageProducerSpec.java
+++ spring-integration-debezium/src/main/java/org/springframework/integration/debezium/dsl/DebeziumMessageProducerSpec.java
@@ -18,23 +18,23 @@ package org.springframework.integration.debezium.dsl;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ThreadFactory;
 
 import io.debezium.engine.ChangeEvent;
 import io.debezium.engine.DebeziumEngine;
 import io.debezium.engine.Header;
 import io.debezium.engine.format.SerializationFormat;
 
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.integration.debezium.inbound.DebeziumMessageProducer;
 import org.springframework.integration.debezium.support.DefaultDebeziumHeaderMapper;
 import org.springframework.integration.dsl.MessageProducerSpec;
 import org.springframework.messaging.support.HeaderMapper;
-import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
 
 /**
  * A {@link org.springframework.integration.dsl.MessageProducerSpec} for {@link DebeziumMessageProducer}.
  *
  * @author Christian Tzolov
+ * @author Artem Bilan
  *
  * @since 6.2
  */
@@ -74,19 +74,18 @@ public class DebeziumMessageProducerSpec
 	}
 
 	/**
-	 * Set a {@link ThreadFactory} for the Debezium executor. Defaults to the {@link CustomizableThreadFactory} with a
-	 * {@code debezium:inbound-channel-adapter-thread-} prefix.
-	 * @param threadFactory the {@link ThreadFactory} instance to use.
+	 * Set a {@link TaskExecutor} for the Debezium engine.
+	 * @param taskExecutor the {@link TaskExecutor} to use.
 	 * @return the spec.
 	 */
-	public DebeziumMessageProducerSpec threadFactory(ThreadFactory threadFactory) {
-		this.target.setThreadFactory(threadFactory);
+	public DebeziumMessageProducerSpec taskExecutor(TaskExecutor taskExecutor) {
+		this.target.setTaskExecutor(taskExecutor);
 		return this;
 	}
 
 	/**
-	 * Set the outbound message content type. Must be aligned with the {@link SerializationFormat} configuration used by
-	 * the provided {@link DebeziumEngine}.
+	 * Set the outbound message content type.
+	 * Must be aligned with the {@link SerializationFormat} configuration used by the provided {@link DebeziumEngine}.
 	 * @param contentType payload content type.
 	 * @return the spec.
 	 */
