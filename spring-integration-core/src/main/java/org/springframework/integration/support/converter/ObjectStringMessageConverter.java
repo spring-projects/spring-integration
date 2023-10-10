348fc40752d33180bec0b29a0ff81774748a0427
Falk Hanisch <falk.hanisch@outlook.com>
2023-10-10 09:50:08 +0200

    add ctor ObjectStringMessageConverter(Charset)
    * create tests for ObjectStringMessageConverter

--- spring-integration-core/src/main/java/org/springframework/integration/support/converter/ObjectStringMessageConverter.java
+++ spring-integration-core/src/main/java/org/springframework/integration/support/converter/ObjectStringMessageConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 the original author or authors.
+ * Copyright 2017-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,8 @@
 
 package org.springframework.integration.support.converter;
 
+import java.nio.charset.Charset;
+
 import org.springframework.messaging.Message;
 import org.springframework.messaging.converter.StringMessageConverter;
 
@@ -24,13 +26,29 @@ import org.springframework.messaging.converter.StringMessageConverter;
  * <p>
  * Delegates to super when payload is {@code byte[]} or {@code String}.
  * Performs {@link Object#toString()} in other cases.
+ * <p>
+ * This class meant to be used as a fallback converter internally when deserializing messages. Therefore, only
+ * {@link org.springframework.messaging.converter.AbstractMessageConverter#fromMessage(Message, Class) fromMessage}
+ * method should be called with {@code String.class} as {@code targetClass}, obviously.To be explicit, using method
+ * {@link org.springframework.messaging.converter.AbstractMessageConverter#toMessage(Object, org.springframework.messaging.MessageHeaders) toMessage}
+ * with anything else than {@code String payload} will return {@code null}.
  *
  * @author Marius Bogoevici
  * @author Artem Bilan
+ * @author Falk Hanisch
  *
  * @since 5.0
  */
 public class ObjectStringMessageConverter extends StringMessageConverter {
+
+	public ObjectStringMessageConverter(Charset defaultCharset) {
+		super(defaultCharset);
+	}
+
+	public ObjectStringMessageConverter() {
+		super();
+	}
+
 
 	@Override
 	protected Object convertFromInternal(Message<?> message, Class<?> targetClass, Object conversionHint) {
---
+++ spring-integration-core/src/test/java/org/springframework/integration/support/converter/ObjectStringMessageConverterTests.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2002-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.support.converter;
+
+
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * @author Falk Hanisch
+ * @since 6.2
+ *
+ */
+class ObjectStringMessageConverterTests {
+	private static final ArbitraryPayload PAYLOAD = new ArbitraryPayload(1234, "ÄÖÜ");
+
+	@Test
+	void testFromMessage() {
+		Message<ArbitraryPayload> message = MessageBuilder.withPayload(PAYLOAD).build();
+		ObjectStringMessageConverter converter = new ObjectStringMessageConverter();
+		Object result = converter.fromMessage(message, String.class);
+
+		assertThat(message.getPayload()).isEqualTo(PAYLOAD);
+		assertThat(result).isEqualTo(PAYLOAD.toString());
+
+		result = converter.fromMessage(message, Object.class);
+		assertThat(result).isNull();
+	}
+
+	@Test
+	void testToMessage() {
+		String text = PAYLOAD.text();
+		ObjectStringMessageConverter converter = new ObjectStringMessageConverter(StandardCharsets.ISO_8859_1);
+		Message<?> converted = converter.toMessage(text, null);
+
+		assertThat(converted).isNotNull();
+		assertThat(converted.getPayload()).isEqualTo(text.getBytes(StandardCharsets.ISO_8859_1));
+
+		converter.setSerializedPayloadClass(String.class);
+		converted = converter.toMessage(text, null);
+		assertThat(converted).isNotNull();
+		assertThat(converted.getPayload()).isEqualTo(text);
+
+		converted = converter.toMessage(PAYLOAD, null);
+		assertThat(converted).isNull();
+	}
+
+	private record ArbitraryPayload(Integer id, String text) { }
+}
