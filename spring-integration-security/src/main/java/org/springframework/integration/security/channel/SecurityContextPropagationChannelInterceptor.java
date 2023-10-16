b147e295ee40c94898b3675986b45df2c4ee4a00
Artem Bilan <abilan@vmware.com>
2023-10-16 17:28:50 -0400

    Deprecate `spring-integration-security` module
    The `SecurityContextPropagationChannelInterceptor` has been migrated to `spring-security-messaging`.
    Since it was only the class in the `spring-integration-security`, it is now fully considered
    as deprecated
    * Remove all the tests from `spring-integration-security`
    * Modify `HttpDslTests` to demonstrate the `spring-security-messaging` in action
    which has been replaced with whatever there was in `spring-integration-security`
    * Remove redundant `exclude group: 'org.springframework'` for security
    dependencies in `build.gradle` since all of them rely on the same SF deps
    as SI

--- spring-integration-security/src/main/java/org/springframework/integration/security/channel/SecurityContextPropagationChannelInterceptor.java
+++ spring-integration-security/src/main/java/org/springframework/integration/security/channel/SecurityContextPropagationChannelInterceptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2019 the original author or authors.
+ * Copyright 2015-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,7 +43,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
  * @since 4.2
  *
  * @see ThreadStatePropagationChannelInterceptor
+ *
+ * @deprecated since 6.2 in favor of {@link org.springframework.security.messaging.context.SecurityContextPropagationChannelInterceptor}
  */
+@Deprecated(since = "6.2", forRemoval = true)
 public class SecurityContextPropagationChannelInterceptor
 		extends ThreadStatePropagationChannelInterceptor<Authentication> {
 
