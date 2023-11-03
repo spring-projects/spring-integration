d95bc681ddd854cac4796da71da836d7e52eeefb
Artem Bilan <abilan@vmware.com>
2023-11-03 11:10:06 -0400

    GH-8786: Make `FtpSession.finalizeRaw()` robust
    Fixes https://github.com/spring-projects/spring-integration/issues/8786
    
    If `FtpSession.readRaw()` fails, the next `FtpSession.finalizeRaw()` call
    would lead to `FTPClient.completePendingCommand()` failure since
    there is no command to finish.
    
    * Fix `FtpSession.finalizeRaw()` to exit earlier positively in case of
    `FTPReply.isNegativePermanent()` for the current reply code set by the failure
    from a previous `FtpSession.readRaw()`
    
    **Cherry-pick to `6.1.x`, `6.0.x` & `5.5.x`**

--- spring-integration-ftp/src/main/java/org/springframework/integration/ftp/session/FtpSession.java
+++ spring-integration-ftp/src/main/java/org/springframework/integration/ftp/session/FtpSession.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -110,6 +110,10 @@ public class FtpSession implements Session<FTPFile> {
 	public boolean finalizeRaw() throws IOException {
 		if (!this.readingRaw.compareAndSet(true, false)) {
 			throw new IOException("Raw read is not in process");
+		}
+		if (FTPReply.isNegativePermanent(this.client.getReplyCode())) {
+			// The 'readRaw()' has failed - nothing to complete.
+			return true;
 		}
 		if (this.client.completePendingCommand()) {
 			int replyCode = this.client.getReplyCode();
