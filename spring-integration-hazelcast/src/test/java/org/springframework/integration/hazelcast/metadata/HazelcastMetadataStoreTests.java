87a070598532fd8b47bc0d732346d584bf52f0d0
Artem Bilan <abilan@vmware.com>
2023-11-13 15:25:13 -0500

    Fix race condition in HazelcastMetadataStoreTests
    The listener on `IMap` is called in async manner.
    
    * Add `await().untilAsserted()` for the first `verify(listener)`
    * Migrate this `HazelcastMetadataStoreTests` to JUnit 5

--- spring-integration-hazelcast/src/test/java/org/springframework/integration/hazelcast/metadata/HazelcastMetadataStoreTests.java
+++ spring-integration-hazelcast/src/test/java/org/springframework/integration/hazelcast/metadata/HazelcastMetadataStoreTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2022 the original author or authors.
+ * Copyright 2017-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,21 +19,23 @@ package org.springframework.integration.hazelcast.metadata;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import org.springframework.integration.metadata.MetadataStoreListener;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 /**
  * @author Vinicius Carvalho
+ * @author Artem Bilan
  */
 public class HazelcastMetadataStoreTests {
 
@@ -43,24 +45,24 @@ public class HazelcastMetadataStoreTests {
 
 	HazelcastMetadataStore metadataStore;
 
-	@BeforeClass
+	@BeforeAll
 	public static void init() {
 		instance = Hazelcast.newHazelcastInstance();
 		map = instance.getMap("customTestsMetadataStore");
 	}
 
-	@AfterClass
+	@AfterAll
 	public static void destroy() {
 		instance.shutdown();
 	}
 
-	@Before
-	public void setup() throws Exception {
+	@BeforeEach
+	public void setup() {
 		this.metadataStore = new HazelcastMetadataStore(map);
 		this.metadataStore.afterPropertiesSet();
 	}
 
-	@After
+	@AfterEach
 	public void clean() {
 		map.clear();
 	}
@@ -80,62 +82,43 @@ public class HazelcastMetadataStoreTests {
 	@Test
 	public void testGetValueFromMetadataStore() {
 		this.metadataStore.put("HazelcastMetadataStoreTests-GetValue", "Hello Hazelcast");
-		String retrievedValue = this.metadataStore
-				.get("HazelcastMetadataStoreTests-GetValue");
+		String retrievedValue = this.metadataStore.get("HazelcastMetadataStoreTests-GetValue");
 		assertThat(retrievedValue).isEqualTo("Hello Hazelcast");
 	}
 
 	@Test
 	public void testPersistEmptyStringToMetadataStore() {
 		this.metadataStore.put("HazelcastMetadataStoreTests-PersistEmpty", "");
-
-		String retrievedValue = this.metadataStore
-				.get("HazelcastMetadataStoreTests-PersistEmpty");
+		String retrievedValue = this.metadataStore.get("HazelcastMetadataStoreTests-PersistEmpty");
 		assertThat(retrievedValue).isEqualTo("");
 	}
 
 	@Test
 	public void testPersistNullStringToMetadataStore() {
-		try {
-			this.metadataStore.put("HazelcastMetadataStoreTests-PersistEmpty", null);
-			fail("Expected an IllegalArgumentException to be thrown.");
-		}
-		catch (IllegalArgumentException e) {
-			assertThat(e.getMessage()).isEqualTo("'value' must not be null.");
-		}
+		assertThatIllegalArgumentException()
+				.isThrownBy(() -> this.metadataStore.put("HazelcastMetadataStoreTests-PersistEmpty", null))
+				.withMessage("'value' must not be null.");
 	}
 
 	@Test
 	public void testPersistWithEmptyKeyToMetadataStore() {
 		this.metadataStore.put("", "PersistWithEmptyKey");
-
 		String retrievedValue = this.metadataStore.get("");
 		assertThat(retrievedValue).isEqualTo("PersistWithEmptyKey");
 	}
 
 	@Test
 	public void testPersistWithNullKeyToMetadataStore() {
-		try {
-			this.metadataStore.put(null, "something");
-			fail("Expected an IllegalArgumentException to be thrown.");
-
-		}
-		catch (IllegalArgumentException e) {
-			assertThat(e.getMessage()).isEqualTo("'key' must not be null.");
-		}
+		assertThatIllegalArgumentException()
+				.isThrownBy(() -> this.metadataStore.put(null, "something"))
+				.withMessage("'key' must not be null.");
 	}
 
 	@Test
 	public void testGetValueWithNullKeyFromMetadataStore() {
-		try {
-			this.metadataStore.get(null);
-		}
-		catch (IllegalArgumentException e) {
-			assertThat(e.getMessage()).isEqualTo("'key' must not be null.");
-			return;
-		}
-
-		fail("Expected an IllegalArgumentException to be thrown.");
+		assertThatIllegalArgumentException()
+				.isThrownBy(() -> this.metadataStore.get(null))
+				.withMessage("'key' must not be null.");
 	}
 
 	@Test
@@ -151,8 +134,7 @@ public class HazelcastMetadataStoreTests {
 
 	@Test
 	public void testPersistKeyValueIfAbsent() {
-		this.metadataStore.putIfAbsent("HazelcastMetadataStoreTests-Spring",
-				"Integration");
+		this.metadataStore.putIfAbsent("HazelcastMetadataStoreTests-Spring", "Integration");
 		assertThat(map.get("HazelcastMetadataStoreTests-Spring")).isEqualTo("Integration");
 	}
 
@@ -172,7 +154,8 @@ public class HazelcastMetadataStoreTests {
 		this.metadataStore.put("foo", "bar");
 		this.metadataStore.replace("foo", "bar", "baz");
 		this.metadataStore.remove("foo");
-		verify(listener).onAdd("foo", "bar");
+
+		await().untilAsserted(() -> verify(listener).onAdd("foo", "bar"));
 		verify(listener).onUpdate("foo", "baz");
 		verify(listener).onRemove("foo", "baz");
 	}
