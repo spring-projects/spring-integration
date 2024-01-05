ab3516cb8611d3f7e8b62e795caa40339bc7dd48
Artem Bilan <artem.bilan@broadcom.com>
2024-01-05 15:53:37 -0500

    GH-8852: Fix TcpNetServerConnectionFactory.run loop
    Fixes: #8852
    
    The `while (true) {` causes the thread to be blocked forever.
    The `stop()` waits on the `if (!executorService.awaitTermination(10, TimeUnit.SECONDS)) {` the whole time
    
    * Fix with the `while (isActive()) {`
    * Remove the `ConnectionFactoryTests.testEarlyCloseNet()` since we cannot reach expectations because of
    race condition between `start()` and `stop()`
    
    **Cherry-pick to `6.2.x` & `6.1.x`**
    
    (cherry picked from commit 0b60dfc9d13c43e3d592f05678a04c1071ea1d45)

--- spring-integration-ip/src/test/java/org/springframework/integration/ip/tcp/connection/ConnectionFactoryTests.java
+++ spring-integration-ip/src/test/java/org/springframework/integration/ip/tcp/connection/ConnectionFactoryTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -220,12 +220,6 @@ public class ConnectionFactoryTests {
 		}
 		serverFactory.stop();
 		scheduler.shutdown();
-	}
-
-	@Test
-	public void testEarlyCloseNet() throws Exception {
-		AbstractServerConnectionFactory factory = new TcpNetServerConnectionFactory(0);
-		testEarlyClose(factory, "serverSocket", " stopped before accept");
 	}
 
 	@Test
