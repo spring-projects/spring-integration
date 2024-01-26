00bd3d86aa38b68715760b9d46215a3146648e47
Neville Bonavia <neville.bonavia@gamesysgroup.com>
2024-01-26 02:31:21 -0500

    GH-8869: Fix NPE in RedisLockRegistry.setExecutor
    Fixes: #8869
    
    Do not set the `redisMessageListenerContainer`'s executor in the setter method
    since the Redis Message Listener container is not initialised during configuration due to lazy loading
    
    **Cherry-pick to `6.2.x` & `6.0.x`**
    
    (cherry picked from commit 62fd3e6c20de8a892620edaa0d0943b57e74c7fa)

--- spring-integration-redis/src/main/java/org/springframework/integration/redis/util/RedisLockRegistry.java
+++ spring-integration-redis/src/main/java/org/springframework/integration/redis/util/RedisLockRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2023 the original author or authors.
+ * Copyright 2014-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -195,8 +195,6 @@ public final class RedisLockRegistry implements ExpirableLockRegistry, Disposabl
 	public void setExecutor(Executor executor) {
 		this.executor = executor;
 		this.executorExplicitlySet = true;
-		this.redisMessageListenerContainer.setTaskExecutor(this.executor);
-		this.redisMessageListenerContainer.setSubscriptionExecutor(this.executor);
 	}
 
 	/**
