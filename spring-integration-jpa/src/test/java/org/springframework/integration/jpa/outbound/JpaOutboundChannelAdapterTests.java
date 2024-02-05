372d9266787f9633241e09a34f7224edd51d2ec9
Artem Bilan <artem.bilan@broadcom.com>
2024-02-05 12:51:45 -0500

    Upgrade some dev dependencies
    * Fix JPA test for the latest Hibernate

--- spring-integration-jpa/src/test/java/org/springframework/integration/jpa/outbound/JpaOutboundChannelAdapterTests.java
+++ spring-integration-jpa/src/test/java/org/springframework/integration/jpa/outbound/JpaOutboundChannelAdapterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2023 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -102,7 +102,7 @@ public class JpaOutboundChannelAdapterTests {
 		List<?> results2 = this.jdbcTemplate.queryForList("Select * from Student");
 		assertThat(results2).hasSize(4);
 
-		assertThat(testStudent.getRollNumber()).isNotNull();
+		assertThat(results2.get(0)).extracting("rollNumber").isNotNull();
 	}
 
 	@Test
@@ -134,7 +134,7 @@ public class JpaOutboundChannelAdapterTests {
 		List<?> results2 = this.jdbcTemplate.queryForList("Select * from Student");
 		assertThat(results2).hasSize(4);
 
-		assertThat(testStudent.getRollNumber()).isNotNull();
+		assertThat(results2.get(0)).extracting("rollNumber").isNotNull();
 	}
 
 	@Test
