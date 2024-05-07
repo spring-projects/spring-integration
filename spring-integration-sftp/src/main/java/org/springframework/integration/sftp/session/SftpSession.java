a7a9d47850eed37b4b90abc3404511ea0e9e0c15
Artem Bilan <artem.bilan@broadcom.com>
2024-05-07 14:23:17 -0400

    GH-9114: SFTP: Use `canonicalPath` `exists` operation
    Fixes: #9114
    
    If path is not in normalized presentation, the SFTP operation might fail like:
    ```
    Caused by: SFTP error (SSH_FX_NO_SUCH_PATH): The file path does not exist or is invalid.
    at org.apache.sshd.sftp.client.impl.AbstractSftpClient.throwStatusException(AbstractSftpClient.java:277)
    at org.apache.sshd.sftp.client.impl.AbstractSftpClient.checkAttributesResponse(AbstractSftpClient.java:333)
    at org.apache.sshd.sftp.client.impl.AbstractSftpClient.checkAttributes(AbstractSftpClient.java:325)
    at org.apache.sshd.sftp.client.impl.AbstractSftpClient.lstat(AbstractSftpClient.java:1010)
    at org.springframework.integration.sftp.session.SftpSession.exists(SftpSession.java:191)
    ```
    
    * Use it now like this `this.sftpClient.lstat(normalizePath(path))`
    
    (cherry picked from commit 8b88668fbeb842ec9003ebb1363981bd04948404)

--- spring-integration-sftp/src/main/java/org/springframework/integration/sftp/session/SftpSession.java
+++ spring-integration-sftp/src/main/java/org/springframework/integration/sftp/session/SftpSession.java
@@ -188,7 +188,7 @@ public class SftpSession implements Session<SftpClient.DirEntry> {
 	@Override
 	public boolean exists(String path) {
 		try {
-			this.sftpClient.lstat(path);
+			this.sftpClient.lstat(normalizePath(path));
 			return true;
 		}
 		catch (SftpException ex) {
