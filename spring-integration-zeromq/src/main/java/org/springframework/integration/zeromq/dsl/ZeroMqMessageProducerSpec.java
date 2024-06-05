1cd15dc2462efc6488c91dfbad4f2c4d16755ecf
Alessio Matricardi <alessiomatricardi@yahoo.com>
2024-06-05 19:41:11 +0200

    add wrapTopic function also in DSL specs

--- spring-integration-zeromq/src/main/java/org/springframework/integration/zeromq/dsl/ZeroMqMessageProducerSpec.java
+++ spring-integration-zeromq/src/main/java/org/springframework/integration/zeromq/dsl/ZeroMqMessageProducerSpec.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 the original author or authors.
+ * Copyright 2020-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,6 +30,7 @@ import org.springframework.messaging.converter.MessageConverter;
 
 /**
  * @author Artem Bilan
+ * @author Alessio Matricardi
  *
  * @since 5.4
  */
@@ -105,6 +106,19 @@ public class ZeroMqMessageProducerSpec
 	 */
 	public ZeroMqMessageProducerSpec topics(String... topics) {
 		this.target.setTopics(topics);
+		return this;
+	}
+
+	/**
+	 * Specify if the topic
+	 * that {@link SocketType#SUB} socket is going to receive is wrapped with an additional empty frame.
+	 * It is ignored for all other {@link SocketType}s supported.
+	 * @param wrapTopic true iff the received topic is wrapped with an additional empty frame.
+	 * @return the spec
+	 * @since 6.2.6
+	 */
+	public ZeroMqMessageProducerSpec wrapTopic(boolean wrapTopic) {
+		this.target.wrapTopic(wrapTopic);
 		return this;
 	}
 
