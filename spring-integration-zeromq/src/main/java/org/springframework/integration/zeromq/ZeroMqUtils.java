6e06a0912efed9f01e70ec93e7178e859a13b365
Alessio Matricardi <alessiomatricardi@yahoo.com>
2024-06-17 22:44:03 +0200

    introduce ZeroMqUtils, for common Zero MQ utilities functions

---
+++ spring-integration-zeromq/src/main/java/org/springframework/integration/zeromq/ZeroMqUtils.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.zeromq;
+
+import org.zeromq.ZMQ;
+
+/**
+ * Module that wraps common methods of ZeroMq integration classes
+ *
+ * @author Alessio Matricardi
+ *
+ * @since 6.4
+ */
+public final class ZeroMqUtils {
+
+	/**
+	 * Binds the ZeroMq socket to the given port when different from 0
+	 * or to a random port if not.
+	 * @param socket the ZeroMq socket
+	 * @param port the port to bind ZeroMq socket to over TCP.
+	 * @return the effectively bound port
+	 * @since 6.4
+	 */
+	public static int bindSocket(ZMQ.Socket socket, int port) {
+		if (port == 0) {
+			return socket.bindToRandomPort("tcp://*");
+		}
+		else {
+			boolean bound = socket.bind("tcp://*:" + port);
+			if (!bound) {
+				throw new IllegalArgumentException("Cannot bind ZeroMQ socket to port: " + port);
+			}
+			return port;
+		}
+	}
+
+	private ZeroMqUtils() {
+	}
+
+}
