feef055a53ee090a23b8381209312d38e4fe3cb2
Artem Bilan <artem.bilan@broadcom.com>
2024-08-20 11:10:52 -0400

    Use `withAssignmentDisabled()` for IntSimpEvalCtxFactoryBean
    **Auto-cherry-pick to `6.3.x` & `6.2.x`**

--- spring-integration-core/src/main/java/org/springframework/integration/config/IntegrationSimpleEvaluationContextFactoryBean.java
+++ spring-integration-core/src/main/java/org/springframework/integration/config/IntegrationSimpleEvaluationContextFactoryBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2022 the original author or authors.
+ * Copyright 2018-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -82,6 +82,7 @@ public class IntegrationSimpleEvaluationContextFactoryBean extends AbstractEvalu
 				SimpleEvaluationContext.forPropertyAccessors(accessorArray)
 						.withTypeConverter(getTypeConverter())
 						.withInstanceMethods()
+						.withAssignmentDisabled()
 						.build();
 		for (Entry<String, Method> functionEntry : getFunctions().entrySet()) {
 			evaluationContext.setVariable(functionEntry.getKey(), functionEntry.getValue());
