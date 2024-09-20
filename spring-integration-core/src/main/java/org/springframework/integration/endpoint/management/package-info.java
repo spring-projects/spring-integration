2c2ed25d6594c3c95330661ebcea2aeacc08cbbe
Artem Bilan <artem.bilan@broadcom.com>
2024-09-20 16:53:35 -0400

    GH-9455: Introduce `IntegrationKeepAlive`
    Fixes: #9455
    Issue link: https://github.com/spring-projects/spring-integration/issues/9455
    
    * Add an `IntegrationKeepAlive` infrastructure bean to initiate a long-lived non-daemon thread
    to keep application alive when it cannot be kept like that for various reason, but has to.
    * Expose `spring.integration.keepAlive` global property to disable an `IntegrationKeepAlive` auto-startup
    * Test and document the feature

--- spring-integration-core/src/main/java/org/springframework/integration/endpoint/management/package-info.java
+++ spring-integration-core/src/main/java/org/springframework/integration/endpoint/management/package-info.java
@@ -1,4 +1,6 @@
 /**
  * Provides classes related to endpoint management.
  */
+@org.springframework.lang.NonNullApi
+@org.springframework.lang.NonNullFields
 package org.springframework.integration.endpoint.management;
