3a8e3abc7d2f25fe39290a9758e9d556cc0faf22
Martin Tomik <mtomik@live.com>
2024-09-23 20:04:00 -0400

    GH-9492: Honor `@Nested` in `@SpringIntegrationTest`
    Fixes: #9492
    Issue link: https://github.com/spring-projects/spring-integration/issues/9492
    
    Switches `MockIntegrationContextCustomizerFactory` & `SpringIntegrationTestExecutionListener`
    to use `TestContextAnnotationUtils` in order to properly support
    `@NestedTestConfiguration` semantics.
    
    Adds an integration test with a base class and several
    `@Nested` inner test classes to verify the various
    permutations of `@SpringIntegrationTest` inherit/override
    behavior when used w/ `@NestedTestConfiguration`.
    
    **Auto-cherry-pick to `6.3.x` & `6.2.x`**

--- spring-integration-test/src/main/java/org/springframework/integration/test/context/SpringIntegrationTest.java
+++ spring-integration-test/src/main/java/org/springframework/integration/test/context/SpringIntegrationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2022 the original author or authors.
+ * Copyright 2017-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,6 +51,8 @@ import org.springframework.test.context.TestExecutionListeners;
  *
  * }
  * </pre>
+ * <p>
+ * Honors the {@link org.springframework.test.context.NestedTestConfiguration} semantics.
  *
  * @author Artem Bilan
  *
