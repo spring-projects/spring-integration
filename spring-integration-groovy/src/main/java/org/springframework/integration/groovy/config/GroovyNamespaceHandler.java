54fbce4b988fd1f6f6a9b39fe62bc3630e9f5f3b
Artem Bilan <artem.bilan@broadcom.com>
2025-01-02 14:14:15 -0500

    GH-9683: Remove deprecated Groovy components for Control Bus
    Related to: https://github.com/spring-projects/spring-integration/issues/9683
    
    The functionality is now fully covered by the `ControlBusCommandRegistry` API

--- spring-integration-groovy/src/main/java/org/springframework/integration/groovy/config/GroovyNamespaceHandler.java
+++ spring-integration-groovy/src/main/java/org/springframework/integration/groovy/config/GroovyNamespaceHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,10 +26,8 @@ import org.springframework.integration.config.xml.AbstractIntegrationNamespaceHa
  */
 public class GroovyNamespaceHandler extends AbstractIntegrationNamespaceHandler {
 
-	@SuppressWarnings("removal")
 	public void init() {
 		registerBeanDefinitionParser("script", new GroovyScriptParser());
-		registerBeanDefinitionParser("control-bus", new GroovyControlBusParser());
 	}
 
 }
