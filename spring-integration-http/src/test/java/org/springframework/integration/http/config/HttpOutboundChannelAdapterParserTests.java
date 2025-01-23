a0a2e76ee2448b6bfe70cfb76bab9f82cc9d903a
Artem Bilan <artem.bilan@broadcom.com>
2025-01-23 10:29:08 -0500

    Fix deprecation in the `HttpOutboundChannelAdapterParserTests`
    The SF `6.2.2` has a `ResponseErrorHandler.handleError(ClientHttpResponse)` deprecated
    
    **Auto-cherry-pick to `6.4.x`**

--- spring-integration-http/src/test/java/org/springframework/integration/http/config/HttpOutboundChannelAdapterParserTests.java
+++ spring-integration-http/src/test/java/org/springframework/integration/http/config/HttpOutboundChannelAdapterParserTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 package org.springframework.integration.http.config;
 
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
@@ -311,7 +312,7 @@ public class HttpOutboundChannelAdapterParserTests {
 		}
 
 		@Override
-		public void handleError(ClientHttpResponse response) {
+		public void handleError(URI url, HttpMethod method, ClientHttpResponse response) {
 		}
 
 	}
