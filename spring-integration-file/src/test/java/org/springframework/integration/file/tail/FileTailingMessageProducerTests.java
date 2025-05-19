6f9130ab68fed479be0e52c1dd32312140708ca7
Jiandong Ma <jiandong.ma.cn@gmail.com>
2025-05-19 22:32:31 +0800

    Fix `FileTailingMessageProducerTests.testOS` for MacOS
    Signed-off-by: Jiandong Ma <jiandong.ma.cn@gmail.com>

--- spring-integration-file/src/test/java/org/springframework/integration/file/tail/FileTailingMessageProducerTests.java
+++ spring-integration-file/src/test/java/org/springframework/integration/file/tail/FileTailingMessageProducerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -87,6 +87,7 @@ public class FileTailingMessageProducerTests {
 	public void testOS() throws Exception {
 		OSDelegatingFileTailingMessageProducer adapter = new OSDelegatingFileTailingMessageProducer();
 		adapter.setOptions(TAIL_OPTIONS_FOLLOW_NAME_ALL_LINES);
+		adapter.setIdleEventInterval(1000);
 		testGuts(adapter, "stdOutReader");
 	}
 
