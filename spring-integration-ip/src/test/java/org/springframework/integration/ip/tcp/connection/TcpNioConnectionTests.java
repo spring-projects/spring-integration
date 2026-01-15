6b78830ebec6a6300b4afb371bd0907ce59d6251
Artem Bilan <artem.bilan@broadcom.com>
2026-01-15 10:07:52 -0500

    Fix Mockito race in the `TcpNioConnectionTests`
    The Mockito complains that stub is not fully initialized:
    ```
    org.mockito.exceptions.misusing.UnfinishedStubbingException:
    Unfinished stubbing detected here:
    -> at org.springframework.integration.ip.tcp.connection.TcpNioConnectionTests.int3453RaceTest(TcpNioConnectionTests.java:741)
    ```
    So, move stubbing a bit closed to the `spy()` before it is assigned to the target object
    
    (cherry picked from commit c19b192eb41768bdf9313c6c5e9269944d7d7fe9)

--- spring-integration-ip/src/test/java/org/springframework/integration/ip/tcp/connection/TcpNioConnectionTests.java
+++ spring-integration-ip/src/test/java/org/springframework/integration/ip/tcp/connection/TcpNioConnectionTests.java
@@ -64,7 +64,6 @@ import org.mockito.stubbing.Answer;
 
 import org.springframework.beans.DirectFieldAccessor;
 import org.springframework.beans.factory.DisposableBean;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
@@ -675,24 +674,14 @@ public class TcpNioConnectionTests implements TestApplicationContextAware {
 	}
 
 	@Test
-	public void int3453RaceTest() throws Exception {
+	public void nioAssemblerThreadIsReleased() throws Exception {
 		TcpNioServerConnectionFactory factory = new TcpNioServerConnectionFactory(0);
 		factory.setTaskScheduler(mock());
 		final CountDownLatch connectionLatch = new CountDownLatch(1);
-		factory.setApplicationEventPublisher(new ApplicationEventPublisher() {
-
-			@Override
-			public void publishEvent(ApplicationEvent event) {
-				if (event instanceof TcpConnectionOpenEvent) {
-					connectionLatch.countDown();
-				}
+		factory.setApplicationEventPublisher(event -> {
+			if (event instanceof TcpConnectionOpenEvent) {
+				connectionLatch.countDown();
 			}
-
-			@Override
-			public void publishEvent(Object event) {
-
-			}
-
 		});
 		final CountDownLatch assemblerLatch = new CountDownLatch(1);
 		final AtomicReference<Thread> assembler = new AtomicReference<>();
@@ -716,27 +705,11 @@ public class TcpNioConnectionTests implements TestApplicationContextAware {
 		Socket socket = SocketFactory.getDefault().createSocket("localhost", port);
 		assertThat(connectionLatch.await(10, TimeUnit.SECONDS)).isTrue();
 
+		final CountDownLatch readerLatch = new CountDownLatch(4); // 3 dataAvailable, 1 continuing
+
 		TcpNioConnection connection = (TcpNioConnection) TestUtils.getPropertyValue(factory, "connections", Map.class)
 				.values().iterator().next();
 		Log logger = spy(TestUtils.getPropertyValue(connection, "logger", Log.class));
-		DirectFieldAccessor dfa = new DirectFieldAccessor(connection);
-		dfa.setPropertyValue("logger", logger);
-
-		ChannelInputStream cis =
-				spy(TestUtils.getPropertyValue(connection, "channelInputStream", ChannelInputStream.class));
-		dfa.setPropertyValue("channelInputStream", cis);
-
-		final CountDownLatch readerLatch = new CountDownLatch(4); // 3 dataAvailable, 1 continuing
-		final CountDownLatch readerFinishedLatch = new CountDownLatch(1);
-		doAnswer(invocation -> {
-			invocation.callRealMethod();
-			// delay the reader thread resetting writingToPipe
-			readerLatch.await(10, TimeUnit.SECONDS);
-			Thread.sleep(100);
-			readerFinishedLatch.countDown();
-			return null;
-		}).when(cis).write(any(ByteBuffer.class));
-
 		doReturn(true).when(logger).isTraceEnabled();
 		doAnswer(invocation -> {
 			invocation.callRealMethod();
@@ -749,6 +722,21 @@ public class TcpNioConnectionTests implements TestApplicationContextAware {
 			readerLatch.countDown();
 			return null;
 		}).when(logger).trace(contains("Nio assembler continuing"));
+		DirectFieldAccessor dfa = new DirectFieldAccessor(connection);
+		dfa.setPropertyValue("logger", logger);
+
+		final CountDownLatch readerFinishedLatch = new CountDownLatch(1);
+		ChannelInputStream cis =
+				spy(TestUtils.getPropertyValue(connection, "channelInputStream", ChannelInputStream.class));
+		doAnswer(invocation -> {
+			invocation.callRealMethod();
+			// delay the reader thread resetting writingToPipe
+			readerLatch.await(10, TimeUnit.SECONDS);
+			Thread.sleep(100);
+			readerFinishedLatch.countDown();
+			return null;
+		}).when(cis).write(any(ByteBuffer.class));
+		dfa.setPropertyValue("channelInputStream", cis);
 
 		socket.getOutputStream().write("foo\r\n".getBytes());
 
