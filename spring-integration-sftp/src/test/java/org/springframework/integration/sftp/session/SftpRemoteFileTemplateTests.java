cccb75799437325459fd5fb288f8c5d92641958b
Adam Yang <yangmm.adam@gmail.com>
2026-06-16 21:56:28 +0800

    GH-11121: Fix SftpSession.listNames() for root directory wildcard paths
    Change `lastIndex > 0` to `lastIndex >= 0` in `SftpSession.doList()` so
    that root-level paths like `/*` are correctly split into directory and
    file components. Previously, `lastIndexOf('/')` returning `0` was not
    handled, causing the entire path to be treated as a directory name.
    
    Fixes: gh-11121
    
    Signed-off-by: Adam Yang <yangmm.adam@gmail.com>

--- spring-integration-sftp/src/test/java/org/springframework/integration/sftp/session/SftpRemoteFileTemplateTests.java
+++ spring-integration-sftp/src/test/java/org/springframework/integration/sftp/session/SftpRemoteFileTemplateTests.java
@@ -55,6 +55,7 @@ import static org.assertj.core.api.Assertions.assertThatNoException;
  * @author Artem Bilan
  * @author Darryl Smith
  * @author Glenn Renfro
+ * @author Adam Yang
  *
  * @since 4.1
  */
@@ -135,6 +136,22 @@ public class SftpRemoteFileTemplateTests extends SftpTestSupport implements Test
 	public void lsUserHome() throws IOException {
 		try (Session<SftpClient.DirEntry> session = this.sessionFactory.getSession()) {
 			String[] entries = session.listNames("");
+			assertThat(entries).contains(".", "sftpSource", "sftpTarget");
+		}
+	}
+
+	@Test
+	public void lsRootWildcard() throws IOException {
+		try (Session<SftpClient.DirEntry> session = this.sessionFactory.getSession()) {
+			String[] entries = session.listNames("/*");
+			assertThat(entries).contains(".", "sftpSource", "sftpTarget");
+		}
+	}
+
+	@Test
+	public void lsRoot() throws IOException {
+		try (Session<SftpClient.DirEntry> session = this.sessionFactory.getSession()) {
+			String[] entries = session.listNames("/");
 			assertThat(entries).contains(".", "sftpSource", "sftpTarget");
 		}
 	}
