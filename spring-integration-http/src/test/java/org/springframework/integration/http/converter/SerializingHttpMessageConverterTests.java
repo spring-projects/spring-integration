33f0432906ddae0e13568e8e07d05f00a3e1d98b
Artem Bilan <artem.bilan@broadcom.com>
2026-06-29 17:16:10 -0400

    Fix Javadoc tags order in the SerializingHttpMessageConverter
    (cherry picked from commit b3894d08ff7007be37a8d36cf366e71ec0274de5)

--- spring-integration-http/src/test/java/org/springframework/integration/http/converter/SerializingHttpMessageConverterTests.java
+++ spring-integration-http/src/test/java/org/springframework/integration/http/converter/SerializingHttpMessageConverterTests.java
@@ -12,7 +12,6 @@ import java.io.InputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.HashMap;
-import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
