7eb1509b1d730e36b51a57d72d5499cff51c318d
Jiandong Ma <jiandong.ma.cn@gmail.com>
2026-09-16 19:16:02 +0800

    Fix deprecation warning in `IntegrationGraphServerTests` (#11428)
    Signed-off-by: Jiandong Ma <jiandong.ma.cn@gmail.com>
    (cherry picked from commit 8058986dd086ee284fc88accf67c09eca77e0c47)

--- spring-integration-core/src/test/java/org/springframework/integration/graph/IntegrationGraphServerTests.java
+++ spring-integration-core/src/test/java/org/springframework/integration/graph/IntegrationGraphServerTests.java
@@ -313,7 +313,7 @@ public class IntegrationGraphServerTests {
 		@Bean
 		public ObservationRegistry observationRegistry(MeterRegistry meterRegistry) {
 			ObservationRegistry registry = ObservationRegistry.create();
-			registry.observationConfig().observationHandler(new DefaultMeterObservationHandler(meterRegistry));
+			registry.observationConfig().observationHandler(DefaultMeterObservationHandler.builder(meterRegistry).build());
 			return registry;
 		}
 
